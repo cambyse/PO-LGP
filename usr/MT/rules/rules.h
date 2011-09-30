9ab6acab5b6741419dd8d2e329831205eaae4c02
Marc Toussaint <marc.toussaint@fu-berlin.de>
2011-09-30 09:34:25 +0200

    rules to dot works

--- usr/MT/rules/rules.h
+++ usr/MT/rules/rules.h
@@ -1,27 +1,28 @@
-struct Variable;
+struct StateVariable;
 struct Rule;
-typedef MT::Array<Variable*> VariableList;
+typedef MT::Array<StateVariable*> StateVariableList;
 typedef MT::Array<Rule*> RuleList;
 
-VariableList globalVars;
+StateVariableList globalVars;
 
-struct Variable {
+struct StateVariable {
   uint id;          //!< unique identifyer
   uint dim;         //!< cardinality of domain
   MT::String name;  //!< up to you...
   MT::Array<MT::String> valueNames;
   
-  Variable();
-  Variable(uint _dim, const char *_name);
-  Variable(uint _dim, const char *_name, uint _id);
-  ~Variable();
+  StateVariable();
+  StateVariable(uint _dim, const char *_name);
+  StateVariable(uint _dim, const char *_name, uint _id);
+  ~StateVariable();
   operator uint() const { return id; }
   void write(std::ostream& os = std::cout) const;
   void read(std::istream& is);
 };
+stdPipes(StateVariable);
 
 struct Rule {
-  VariableList vars;
+  StateVariableList vars;
   boolA negation;
   uintA values;
   uint arrow; //preconditionSize
@@ -30,6 +31,7 @@ struct Rule {
   void write(std::ostream& os = std::cout) const;
   void read(std::istream& is);
 };
+stdPipes(Rule);
 
 #if 1
 #  include "rules.cpp"
