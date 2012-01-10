8e82f592d30c02b1c59e9ec2efff057bf89768df
Marc Toussaint <marc.toussaint@fu-berlin.de>
2012-01-10 11:17:41 +0100

    qt with opengl works nicely again

--- share/src/MT/opengl.h
+++ share/src/MT/opengl.h
@@ -199,7 +199,8 @@ struct OpenGL {
   
   //!@name constructors & destructors
   OpenGL(const char* title="MT::OpenGL", int w=400, int h=400, int posx=-1, int posy=-1);
-  OpenGL(void *parent, const char* title="MT::OpenGL", int w=400, int h=400, int posx=-1, int posy=-1);
+  //OpenGL(void *parent, const char* title="MT::OpenGL", int w=400, int h=400, int posx=-1, int posy=-1);
+  OpenGL(sOpenGL *_s); //special constructor: used when the underlying system-dependent class exists already
   
   OpenGL *newClone() const;
   ~OpenGL();
--- share/src/MT/opengl_qt.cxx
+++ share/src/MT/opengl_qt.cxx
@@ -1,101 +1,39 @@
-#include <GL/glut.h>
-#undef scroll
-#undef border
-// #  define QT3_SUPPORT
-// #  include <Qt/Qt3Support>
-// #  include <Qt/qtimer.h>
-#include <QtGui/QApplication>
-#include <QtOpenGL/QGLWidget>
-// #  include <Qt/qobject.h>
-// #  include <Qt/qevent.h>
-#include <QtOpenGL/QtOpenGL>
-#if defined MT_Cygwin //|| defined MT_Linux
-#    define GLformat QGL::DirectRendering | QGL::DepthBuffer | QGL::Rgba
-#    define GLosformat QGL::DirectRendering | QGL::DepthBuffer | QGL::Rgba
-#  else
-#    define GLformat QGL::DirectRendering | QGL::DepthBuffer | QGL::Rgba
-#    define GLosformat QGL::DirectRendering | QGL::DepthBuffer | QGL::Rgba
-#endif
-#define MT_GLUT
+#include "opengl_qt.h"
 
-#ifdef MT_MSVC
-#  include<windows.h>
-#  undef min //I hate it that windows defines these macros!
-#  undef max
-#endif
-
-
-struct sOpenGL:public QGLWidget{
-  Q_OBJECT
-public:
-  sOpenGL(const char* title="MT::OpenGL(Qt)",int w=400,int h=400,int posx=-1,int posy=-1);
-  sOpenGL(QWidget *parent,const char* title,int width=400,int height=400,int posx=-1,int posy=-1);
-  sOpenGL(OpenGL *_gl,const char* title,int w,int h,int posx,int posy){
-    gl=_gl;
-  };
-  ~sOpenGL();
-
-  void init(){
-    osPixmap=0;
-    osContext=0;
-    quitLoopOnTimer=gl->reportEvents=false;
-  }
-
-  static uint nrWins;
-  OpenGL *gl;
-  ors::Vector downVec,downPos,downFoc;
-  ors::Quaternion downRot;
-
-  bool quitLoopOnTimer;
-  QPixmap *osPixmap;      // the paint device for off-screen rendering
-  QGLContext *osContext;  //the GL context for off-screen rendering
-
-  void createOffscreen(int width,int height);
-  void offscreenGrab(byteA& image);
-  void offscreenGrab(byteA& image,byteA& depth);
-  void offscreenGrabDepth(byteA& depth);
-  void offscreenGrabDepth(floatA& depth);
-
-
-  //hooks for Qt (overloading virtuals)
-    void paintGL(){ gl->Draw(width(),height()); }
-    void initializeGL(){ }
-    void resizeGL(int w,int h){ gl->Reshape(w,h); }
-    void keyPressEvent(QKeyEvent *e){ gl->pressedkey=e->text().toAscii()[0]; gl->Key(gl->pressedkey,gl->mouseposx,gl->mouseposy); }
-    void timerEvent(QTimerEvent*){ if(quitLoopOnTimer) gl->exitEventLoop(); }
-    void mouseMoveEvent(QMouseEvent* e){
-      if(!gl->mouseIsDown) gl->PassiveMotion(e->x(),e->y()); else gl->Motion(e->x(),e->y());
-    }
-    void mousePressEvent(QMouseEvent* e){
-      if(e->button()==Qt::LeftButton) { gl->Mouse(0,0,e->x(),e->y()); }
-      if(e->button()==Qt::MidButton)  { gl->Mouse(1,0,e->x(),e->y()); }
-      if(e->button()==Qt::RightButton){ gl->Mouse(2,0,e->x(),e->y()); }
-    }
-    void mouseReleaseEvent(QMouseEvent* e){
-      if(e->button()==Qt::LeftButton) { gl->Mouse(0,1,e->x(),e->y()); }
-      if(e->button()==Qt::MidButton)  { gl->Mouse(1,1,e->x(),e->y()); }
-      if(e->button()==Qt::RightButton){ gl->Mouse(2,1,e->x(),e->y()); }
-    }
-
-private:
-  void setOffscreen(int width,int height);
-
-};
+//===========================================================================
+//
+// sOpenGL implementations
+//
 
 uint sOpenGL::nrWins=0;
 
-sOpenGL::sOpenGL(const char* title,int width,int height,int posx,int posy)
+sOpenGL::sOpenGL(QDialog*& parent):QGLWidget(QGLFormat(GLformat),parent){
+  gl = new OpenGL(this);
+  QWidget::setMouseTracking(true);
+  init();
+}
+
+sOpenGL::sOpenGL(OpenGL *_gl, const char* title,int width,int height,int posx,int posy)
   :QGLWidget(QGLFormat(GLformat)){
+  gl = _gl;
   QGLWidget::move(posx,posy);
   QGLWidget::resize(width,height);
   QWidget::setMouseTracking(true);
   QWidget::setWindowTitle(title);
+  init();
+}
+
+void sOpenGL::init(){
+  //osPixmap=0;
+  //osContext=0;
+  quitLoopOnTimer=gl->reportEvents=false;
 }
 
 sOpenGL::~sOpenGL(){
-  if(osContext) delete osContext;
-  if(osPixmap) delete osPixmap;
+  //if(osContext) delete osContext;
+  //if(osPixmap) delete osPixmap;
 };
+
 
 //===========================================================================
 //
@@ -113,21 +51,20 @@ OpenGL::OpenGL(const char* title,int width,int height,int posx,int posy){
   s->nrWins++;
 
   init();
-  //windowID=(int)winId();
 
   s->QGLWidget::show();
 }
 
-// //! Qt constructor when window is parent of another window
-// OpenGL::OpenGL(QWidget *parent,const char* title,int width,int height,int posx,int posy)
-//   :QGLWidget(QGLFormat(GLformat),parent){
-//   QGLWidget::move(posx,posy);
-//   QGLWidget::resize(width,height);
-//   QWidget::setMouseTracking(true);
-//   QWidget::setWindowTitle(title);
-//   init();
-//   windowID=(int)winId();
-// }
+OpenGL::OpenGL(sOpenGL *_s){
+  s=_s;
+  if(!s->nrWins){
+    int argc=1;
+    char *argv[1]={(char*)"x"};
+    glutInit(&argc, argv);
+  }
+  s->nrWins++;
+  init();
+}
 
 //! destructor
 OpenGL::~OpenGL(){
@@ -151,6 +88,41 @@ void OpenGL::resize(int w,int h){
   processEvents();
 }
 
+void OpenGL::about(std::ostream& os){
+  os <<"Widget's OpenGL capabilities:\n";
+  QGLFormat f=s->format();
+  os <<"direct rendering: " <<f.directRendering() <<"\n"
+  <<"double buffering: " <<f.doubleBuffer()  <<"\n"
+  <<"depth:            " <<f.depth() <<"\n"
+  <<"rgba:             " <<f.rgba() <<"\n"
+  <<"alpha:            " <<f.alpha() <<"\n"
+  <<"accum:            " <<f.accum() <<"\n"
+  <<"stencil:          " <<f.stencil() <<"\n"
+  <<"stereo:           " <<f.stereo() <<"\n"
+  <<"overlay:          " <<f.hasOverlay() <<"\n"
+  <<"plane:            " <<f.plane() <<std::endl;
+  
+#if 0
+  if(!s->osContext){
+    os <<"no off-screen context created yet" <<std::endl;
+  }else{
+    os <<"Off-screen pixmaps's OpenGL capabilities:\n";
+    f=s->osContext->format();
+    os <<"direct rendering: " <<f.directRendering() <<"\n"
+    <<"double buffering: " <<f.doubleBuffer()  <<"\n"
+    <<"depth:            " <<f.depth() <<"\n"
+    <<"rgba:             " <<f.rgba() <<"\n"
+    <<"alpha:            " <<f.alpha() <<"\n"
+    <<"accum:            " <<f.accum() <<"\n"
+    <<"stencil:          " <<f.stencil() <<"\n"
+    <<"stereo:           " <<f.stereo() <<"\n"
+    <<"overlay:          " <<f.hasOverlay() <<"\n"
+    <<"plane:            " <<f.plane() <<std::endl;
+  }
+#endif
+}
+
+#if 0 //OLD offscrean code
 /*!\brief creates a off-screen rendering context for future backround
     rendering routines -- the off-screen context cannot be
     resized... */
@@ -214,35 +186,4 @@ void OpenGL::setOffscreen(int width, int height){
   s->osContext->makeCurrent();
   //if(initRoutine) (*initRoutine)();
 }
-
-void OpenGL::about(std::ostream& os){
-  os <<"Widget's OpenGL capabilities:\n";
-  QGLFormat f=s->format();
-  os <<"direct rendering: " <<f.directRendering() <<"\n"
-  <<"double buffering: " <<f.doubleBuffer()  <<"\n"
-  <<"depth:            " <<f.depth() <<"\n"
-  <<"rgba:             " <<f.rgba() <<"\n"
-  <<"alpha:            " <<f.alpha() <<"\n"
-  <<"accum:            " <<f.accum() <<"\n"
-  <<"stencil:          " <<f.stencil() <<"\n"
-  <<"stereo:           " <<f.stereo() <<"\n"
-  <<"overlay:          " <<f.hasOverlay() <<"\n"
-  <<"plane:            " <<f.plane() <<std::endl;
-  
-  if(!s->osContext){
-    os <<"no off-screen context created yet" <<std::endl;
-  }else{
-    os <<"Off-screen pixmaps's OpenGL capabilities:\n";
-    f=s->osContext->format();
-    os <<"direct rendering: " <<f.directRendering() <<"\n"
-    <<"double buffering: " <<f.doubleBuffer()  <<"\n"
-    <<"depth:            " <<f.depth() <<"\n"
-    <<"rgba:             " <<f.rgba() <<"\n"
-    <<"alpha:            " <<f.alpha() <<"\n"
-    <<"accum:            " <<f.accum() <<"\n"
-    <<"stencil:          " <<f.stencil() <<"\n"
-    <<"stereo:           " <<f.stereo() <<"\n"
-    <<"overlay:          " <<f.hasOverlay() <<"\n"
-    <<"plane:            " <<f.plane() <<std::endl;
-  }
-}
+#endif
