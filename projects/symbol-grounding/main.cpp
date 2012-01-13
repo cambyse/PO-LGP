d6ed814d13ce754c9ee172b14d92665e0286b855
Johannes Kulick <kulick@hildensia.de>
2012-01-13 17:42:00 +0100

    move perception of data and test to other files

--- projects/symbol-grounding/main.cpp
+++ projects/symbol-grounding/main.cpp
@@ -1,107 +1,12 @@
-#include <vision/visionModule.h>
-#include <MT/perceptionModule.h>
-#include <MT/robot_variables.h>
+#include <MT/opengl.h>
 
-#include "ObjectFileCreator.h"
 #include "naiveBayesClassificator.h"
 #include "DataReader.h"
 
 #include <MT/array_t.cpp>
+#include <MT/ors.h>
 
-void testNaiveBayesClassificator() {
-  MT::Array<arr> traindata;
-  traindata.append(ARR(0.61, 0.));
-  traindata.append(ARR(0.44, 0.));
-  traindata.append(ARR(0., 0.67));
-  traindata.append(ARR(0., 0.76));
-
-  MT::Array<int> classes;
-  classes.resize(4);
-  classes(0) = 0;
-  classes(1) = 0;
-  classes(2) = 1;
-  classes(3) = 1;
-  traindata.resize(4,1);
-
-  NaiveBayesClassificator c;
-  c.setTrainingsData(traindata, classes);
-
-  MT::Array<arr> testdata;
-  testdata.append(ARR(0., 0.7));
-  testdata.reshape(1,1);
-  int cl = c.classify(testdata);
-  std::cout << "class of (0, 0.7): " << cl << std::endl;
-
-  testdata(0,0) = ARR(0.5, 0.);
-  cl = c.classify(testdata);
-  std::cout << "class of (0.5, 0): " << cl << std::endl;
-  
-}
-
-void testDataReader() {
-  DataReader d; 
-	d.readDataFile("s111230-1356-0.dat", "s111230-1356-0.dat.rel");
-  d.readDataFile("s111230-1356-1.dat", "s111230-1356-1.dat.rel");
-	d.readDataFile("s111230-1436-1.dat", "s111230-1436-1.dat.rel");
-	d.readDataFile("s111230-1439-0.dat", "s111230-1439-0.dat.rel");
-	d.readDataFile("s111230-1439-1.dat", "s111230-1439-1.dat.rel");
-  d.readDataFile("s111230-1442-0.dat", "s111230-1442-0.dat.rel");
-  d.readDataFile("s111230-1442-1.dat", "s111230-1442-1.dat.rel");
-  d.readDataFile("s111230-1442-2.dat", "s111230-1442-2.dat.rel");
-
-  std::cout << d.getData() << std::endl;
-  std::cout << d.getClasses() << std::endl;
-}
-
-void testBayesOnRealData() {
-    
-  DataReader d;
-
-	d.readDataFile("s111230-1356-0.dat", "s111230-1356-0.dat.rel");
-  d.readDataFile("s111230-1356-1.dat", "s111230-1356-1.dat.rel");
-	d.readDataFile("s111230-1436-1.dat", "s111230-1436-1.dat.rel");
-	d.readDataFile("s111230-1439-0.dat", "s111230-1439-0.dat.rel");
-	d.readDataFile("s111230-1439-1.dat", "s111230-1439-1.dat.rel");
-  d.readDataFile("s111230-1442-0.dat", "s111230-1442-0.dat.rel");
-
-  NaiveBayesClassificator c;
-  c.setTrainingsData(d.getData(), d.getClasses());
-
-  DataReader testdata;
-  testdata.readDataFile("s111230-1442-1.dat", "s111230-1442-1.dat.rel");
-
-  std::cout << "Classified " << testdata.getData() << " as " << c.classify(testdata.getData()) << " (Should be " << testdata.getClasses() << ")" << std::endl;
-
-  DataReader testdata2;
-  testdata2.readDataFile("s111230-1442-2.dat", "s111230-1442-2.dat.rel");
-
-  std::cout << "Classified " << testdata2.getData() << " as " << c.classify(testdata2.getData()) << " (Should be " << testdata2.getClasses() << ")" << std::endl;
-}
-
-void testSampling() {
-  MT::Array<arr> traindata;
-  traindata.append(ARR(0.61, 0.));
-  traindata.append(ARR(0.44, 0.));
-  traindata.append(ARR(0., 0.67));
-  traindata.append(ARR(0., 0.76));
-
-  MT::Array<int> classes;
-  classes.resize(4);
-  classes(0) = 0;
-  classes(1) = 0;
-  classes(2) = 1;
-  classes(3) = 1;
-  traindata.resize(4,1);
-
-  NaiveBayesClassificator c;
-  c.setTrainingsData(traindata, classes);
-
-  MT::Array<arr> test;
-  c.nextSample(test);
-  std::cout << test << std::endl;
-
-  test.clear();
-
+void sample(MT::Array<arr>& sample) {
   DataReader d;
 
 	d.readDataFile("s111230-1356-0.dat", "s111230-1356-0.dat.rel");
@@ -116,28 +21,98 @@ void testSampling() {
   NaiveBayesClassificator cl;
   cl.setTrainingsData(d.getData(), d.getClasses());
 
-  cl.nextSample(test);
-  std::cout << "test: " << test << std::endl;
+  cl.nextSample(sample);
 
+}
+
+void drawEnv(void*){  glStandardLight(NULL); }
+
+void init(ors::Graph& G,OpenGL& gl,const char* orsFile){
+  G.init(orsFile);
+  //G.makeLinkTree();
+  gl.add(drawEnv,0);
+  gl.add(ors::glDrawGraph,&G);
+  gl.setClearColors(1.,1.,1.,1.);
+  gl.camera.setPosition(10.,-15.,8.);
+  gl.camera.focus(0,0,1.);
+  gl.camera.upright();
+  gl.update();
+}
+
+void createCylinder(ors::Body& cyl, const ors::Vector& pos, const int color) {
+    
+  ors::Transformation t;
+  t.pos = pos;
+  ors::Shape* s = new ors::Shape();
+  s->size[0] = 0.1;
+  s->size[1] = 0.1;
+  s->size[2] = 0.108;
+  s->size[3] = 0.0375;
+
+  s->type = ors::cylinderST;
+  switch (color) {
+    case 0:  
+      s->color[0] = 1;
+      s->color[1] = 0;
+      s->color[2] = 0;
+      break;
+    case 1:
+      s->color[0] = 0;
+      s->color[1] = 1;
+      s->color[2] = 0;
+      break;
+  }
+  s->body = &cyl;
+  cyl.shapes.append(s);
+  cyl.X = t;
 }
 
 int main(int argn, char** argv) {
   MT::initCmdLine(argn,argv);
 
-	//VisionModule vision;
-  //ObjectFileCreator fileCreator;
-  //PerceptionOutput percout;
 
-	//vision.output = &percout;
-  //fileCreator.input = &percout;
+  ors::Graph G;
+  OpenGL gl;
+  init(G,gl,"schunk_no.ors");
 
-	//vision.open();
-  //fileCreator.threadOpen();
-  //fileCreator.threadLoop();
-    
-  //testNaiveBayesClassificator();
-	//testDataReader();
-  testSampling();
+  MT::Array<arr> s;
+  sample(s);
+
+  cout << "Sample: " << s << endl;
+
+  ors::Body fst;
+  ors::Body snd;
+  createCylinder(fst, s(0), 0);
+  createCylinder(snd, s(1), 1);
+ 
+  G.bodies.append(&fst);
+  G.bodies.append(&snd);
+
+  DataReader d;
+
+	d.readDataFile("s111230-1356-0.dat", "s111230-1356-0.dat.rel");
+  d.readDataFile("s111230-1356-1.dat", "s111230-1356-1.dat.rel");
+	d.readDataFile("s111230-1436-1.dat", "s111230-1436-1.dat.rel");
+	d.readDataFile("s111230-1439-0.dat", "s111230-1439-0.dat.rel");
+	d.readDataFile("s111230-1439-1.dat", "s111230-1439-1.dat.rel");
+  d.readDataFile("s111230-1442-0.dat", "s111230-1442-0.dat.rel");
+  d.readDataFile("s111230-1442-1.dat", "s111230-1442-1.dat.rel");
+  d.readDataFile("s111230-1442-2.dat", "s111230-1442-2.dat.rel");
 
 
+  int steps=0;
+  int i = 0;
+  while (true) { 
+    steps ++;
+    if (steps % 200 == 0 && i < d.getData().d0) {
+      fst.X.pos = d.getData()(i,0);
+      snd.X.pos = d.getData()(i,1);
+      i++;
+    }
+    else if (i > d.getData().d0) {
+      fst.X.pos = s(0); 
+      snd.X.pos = s(1); 
+    }
+    gl.timedupdate(0.01); 
+  }
 }
