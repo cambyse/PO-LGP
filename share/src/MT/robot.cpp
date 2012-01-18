23e3bf2a37fdb31011f6fd35428769a441ec0d73
Michael <michael.borst@fu-berlin.de>
2012-01-18 12:43:56 +0100

    Magic bug is now fixed, seems like some files weren't commited yet.

--- share/src/MT/robot.cpp
+++ share/src/MT/robot.cpp
@@ -271,6 +271,8 @@ void RobotProcessGroup::open(){
   
   if(openEarlyVision){
     evis.input = &currentCameraImages;
+		std::cout << evisOutput.hsvThetaL << std::endl;
+		evis.output = &evisOutput;
     evis.threadOpen(MT::getParameter<int>("evisThreadNice", 0));
     evis.threadLoop();
   }
