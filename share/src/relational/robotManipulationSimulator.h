035a681de74c1c036c2377d9c3ac9d72fe4504e4
Tobias Lang <tobias.lang@fu-berlin.de>
2012-05-09 15:44:39 +0200

    blocksWorld files removed

--- share/src/relational/robotManipulationSimulator.h
+++ share/src/relational/robotManipulationSimulator.h
@@ -194,8 +194,18 @@ public:
 //   void controlledStep(arr &q,arr &W,const char* text);
 };
 
-// #ifdef MT_IMPLEMENTATION
-// #  include "robotManipulationSimulatorInterface.cpp"
-// #endif
+
+namespace relational {
+void generateOrsBlocksSample(ors::Graph& ors, const uint numOfBlocks);
+void generateOrsFromSample(ors::Graph& ors, const MT::Array<arr>& sample);
+void generateBlocksSample(MT::Array<arr>& sample, uint numOfBlocks);
+void createCylinder(ors::Body& cyl, const ors::Vector& pos, const arr& color, const arr& size);
+void createCylinder(ors::Body& cyl, const ors::Vector& pos, const arr& color);
+}
+
+
+
+
+
 
 #endif  // TL_robot_manipulation_simulator
