4dc452ba4e0f19f2b035de70a036933088420e14
Johannes Kulick <kulick@hildensia.de>
2012-05-24 18:40:40 +0200

    use kinect for perception (testcase)

--- share/test/perception_pointcloud/main.cpp
+++ share/test/perception_pointcloud/main.cpp
@@ -1,20 +1,30 @@
 #include <perception/pointcloud.h>
+#include <JK/utils/util.h>
 
 #include <pcl/visualization/pcl_visualizer.h>
+
+#include <queue>
 
 int main(int argc, char **argv) {
 
   // Variables
   PointCloudVar kinectData3d("KinectData3D");
   PointCloudSet objectClusters("ObjectClusters");
+  ObjectSet objects("Objects");
 
   // Processes
   KinectInterface kinect("Kinect");
   ObjectClusterer clusterer;
+  ObjectFitterIntegrator integrator;
 
+  ObjectFitterWorkerFactory factory;
+  ObjectFitter fitter(&factory, &integrator, 10); //no process but a master!
 
   kinect.threadLoop();
   clusterer.threadLoop();
+
+  MT::wait(2.);
+
 
   // TODO: put into Viewer<PointCloudVar>
   boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer (new pcl::visualization::PCLVisualizer ("3D Viewer"));
@@ -24,12 +34,30 @@ int main(int argc, char **argv) {
   //viewer->addPointCloud(kinectData3d.get_point_cloud_copy(NULL), "cloud");
   while(objectClusters.get_point_clouds(NULL).N == 0) MT::wait(0.1);
   
-  viewer->addPointCloud(objectClusters.get_point_clouds(NULL)(0)->makeShared(), "cluster");
+  viewer->addPointCloud(kinectData3d.get_point_cloud(NULL), "cluster");
+
+  PointCloudL plist = objectClusters.get_point_clouds(NULL);
 
   for(int i = 0; i < 1000; ++i) {
+    //if(i % 50 == 0 ) {
+      objectClusters.readAccess(NULL);
+      plist = objectClusters.point_clouds;
+      std::queue<FittingJob> jobs;
+      for(int i = 0; i < plist.N; ++i) {
+        jobs.push(plist(i));
+      }
+      fitter.pause();
+      fitter.restart(jobs, NULL);
+      objectClusters.deAccess(NULL);
+    //}
     MT::wait(.1);
-    //viewer->updatePointCloud(kinectData3d.get_point_cloud_copy(NULL), "cloud");
-    viewer->updatePointCloud(objectClusters.get_point_clouds(NULL)(0)->makeShared(), "cluster");
+    for(int i = 0; i < objects.get_objects(NULL).N; ++i) {
+      std::stringstream name;
+      name << "cyl_" << i;
+      viewer->removeShape(name.str());  
+      viewer->addCylinder(*objects.get_objects(NULL)(i), name.str());
+    }
+    viewer->updatePointCloud(kinectData3d.get_point_cloud(NULL), "cluster");
     viewer->spinOnce();
   }
 
