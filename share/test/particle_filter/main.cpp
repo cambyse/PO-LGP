2767c65cd02ba832c3e1a9f99d59b2bbb9c5ff3e
Johannes Kulick <kulick@hildensia.de>
2012-06-01 12:28:59 +0200

    use particle filter as process

--- share/test/particle_filter/main.cpp
+++ share/test/particle_filter/main.cpp
@@ -11,10 +11,11 @@ arr z1;
 arr z2;
 
 void measure(arr& r) {
+  dir += 0.002*randn(2,1);
   pos += dir;
   if(pos(0,0) > 1 || pos(0,0) < 0) dir(0,0) *= -1;
   if(pos(0,1) > 1 || pos(0,1) < 0) dir(0,1) *= -1;
-  r = pos + 0.00001*randn(2,1);
+  r = pos + 0.001*randn(2,1);
   z2 = z1;
   z1 = r;
 }
@@ -22,13 +23,12 @@ void measure(arr& r) {
 double lin_weight(const arr &particle, const arr &measurement) {
   Gaussian g;
   g.c = measurement;
-  g.C = 0.00001*eye(2);
+  g.C = 0.001*eye(2);
   return g.evaluate(particle);
 }
 
 void gaussian_control(arr &after, const arr &before) {
-  JK_DEBUG(z2 - z1);
-  after = before + (dir)+0.005*randn(before.d0,1); 
+  after = before + (dir)+0.009*randn(before.d0,1); 
 }
 
 int main(int argc, char **argv) {
@@ -37,18 +37,33 @@ int main(int argc, char **argv) {
   dir = ARR(.004, .005);
   pos.reshape(1,2);
   dir.reshape(1,2);
-  ParticleFilter f(1000, 2);
+
+  Measurement m("M");
+  Particles p("P");
+
+  ParticleFilter f;
+  f.measurement = &m;
+  f.particles = &p;
+  arr mean = ARR(0.5, 0.4);
+  mean.reshape(1,2);
+  f.init(mean, 1000);
+  f.add_measurement(z1);
+
   f.weight = &lin_weight;
   f.control = &gaussian_control;
+
   gnuplot("set xrange [0:1.5]; set yrange [0:1.5]");
   for(int i=0; i<10000; ++i) {
     arr z;
     measure(z);
     f.add_measurement(z);
-    f.step();
-    arr mean = sum(f.particles, 0)*(1./f.particles.d0);
+    f.threadStep();
+    MT::wait(0.1);
+    p.readAccess(NULL);
+    arr mean = sum(p.particles, 0)*(1./p.particles.d0);
     MT::IOraw=true;
-    MT::save(f.particles, "z.pltX");
+    MT::save(p.particles, "z.pltX");
+    p.deAccess(NULL);
     MT::save(mean, "sum");
     MT::save(pos, "pos");
     MT::save(z, "z");
