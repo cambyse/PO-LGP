6b800a05f3d9b74d761004b66f85e59be789978a
Johannes Kulick <kulick@hildensia.de>
2012-06-01 11:04:58 +0200

    make particle filter a process

--- share/src/JK/particle_filter/particle_filter.cpp
+++ share/src/JK/particle_filter/particle_filter.cpp
@@ -5,56 +5,80 @@ class sParticleFilter {
   public:
     ParticleFilter *p;
     sParticleFilter(ParticleFilter *p) : p(p) {};
-    arr measurement;
 
     double cur_sum, cur_particle_sum, step;
     int cur_particle;
 
-    void reset_drawing() {
+    void reset_drawing(const arr &particles, const arr& measurement) {
       cur_particle = 0;
       double sum = 0;
-      for(int i=0; i<p->particles.d0; ++i) {
-         sum += p->weight(p->particles[i], measurement);
+      for(int i=0; i<particles.d0; ++i) {
+         sum += p->weight(particles[i], measurement);
       }
-      step = sum / (p->particles.d0 + 1);
-      cur_particle_sum = p->weight(p->particles[0], measurement);
+      step = sum / (particles.d0 + 1);
+      cur_particle_sum = p->weight(particles[0], measurement);
       cur_sum = step / 2.;
     }
 
-    arr draw() {
+    arr draw(const arr &particles, const arr& measurement) {
       cur_sum += step;
       if (cur_sum > cur_particle_sum) {
         cur_particle++;
-        cur_particle_sum += p->weight(p->particles[cur_particle], measurement);
+        cur_particle_sum += p->weight(particles[cur_particle], measurement);
       }
-      return p->particles[cur_particle];
+      return particles[cur_particle];
     }
 };
 
 ParticleFilter::ParticleFilter(int num_of_particles, int dim) : s(new sParticleFilter(this)) {
+  particles.writeAccess(this);
   for (int i=0; i< num_of_particles; ++i) {
-    particles.append(rand(dim,1));
+    particles.particles.append(rand(dim,1));
   }
-  particles.reshape(num_of_particles, dim);
+  particles.particles.reshape(num_of_particles, dim);
+  particles.deAccess(this);
 }
 
 void ParticleFilter::step() {
-  for (int i=0; i<particles.d0; ++i) {
+  //get current measurement
+  arr measurement_;
+  measurement.writeAccess(this);
+  //if(measurement.measurements.isEmpty() {
+    //measurement.deAccess(this);
+    //return;
+  //}
+  copy(measurement_, measurement.measurement())
+  measurement.deAccess();
+
+  // get copy of particles
+  arr particles_;
+  particles.readAccess(this);
+  copy(particles_, particles.particles);
+  particles.deAccess(this);
+
+  // compute particle filter
+  for (int i=0; i<particles_.d0; ++i) {
     arr x;
-    control(x, particles[i]);
-    particles[i] = x;
+    control(x, particles_[i]);
+    particles_[i] = x;
   }
-  s->reset_drawing();
+  s->reset_drawing(particles_, measurement_));
   arr next;
-  for (int i=0; i<particles.d0; ++i) {
-    next.append(s->draw());
+  for (int i=0; i<particles_.d0; ++i) {
+    next.append(s->draw(particles_, measurement_));
   }
-  next.reshape(particles.d0, particles.d1);
-  copy(particles, next);
+  next.reshape(particles_.d0, particles_.d1);
+
+  // write particles back to Variable
+  particles.writeAccess(this); 
+  copy(particles.particles, next);
+  particles.deAccess(this);
 }
 
 void ParticleFilter::add_measurement(const arr &measurement) {
-  copy(s->measurement, measurement);
+  measurement.writeAccess(this);
+  copy(measurement.measurement, measurement);
+  measurement.deAccess(this;
 }
 ParticleFilter::~ParticleFilter() {
   delete s;  
--- share/src/JK/particle_filter/particle_filter.h
+++ share/src/JK/particle_filter/particle_filter.h
@@ -1,15 +1,29 @@
 #ifndef PARTICLE_FILTER_H_
 #define PARTICLE_FILTER_H_
 #include <MT/array.h>
+#include <biros/biros.h>
 
-class ParticleFilter {
+class Particles : public Variable {
+  public:
+    Particles(const char *name) : Variable(name) {reg_particles();}
+    FIELD(arr, particles);  
+}
+
+class Measurement : public Variable {
+  public:
+    Measurement(const char *name) : Variable(name) {reg_measurement();}
+    FIELD(arr, measurement);  
+}
+
+class ParticleFilter :public Process {
   public:
     class sParticleFilter *s;
     
     double (*weight)(const arr &particle, const arr &measurement);
     void (*control)(arr &after, const arr &before);
     
-    arr particles;
+    Particles particles;
+    Measurement measurement;
 
     ParticleFilter(int num_of_particles, int dim);
     ~ParticleFilter();
