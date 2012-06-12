5e00c874ea7b1f0c93463e5f4688e167368fa8ba
Johannes Kulick <kulick@hildensia.de>
2012-06-12 12:12:52 +0200

    minor changes due to refactoring of jk lib

--- projects/active-learning/main.cpp
+++ projects/active-learning/main.cpp
@@ -1,16 +1,30 @@
+#define LOG_STRING (level != ERROR ? std::cout : std::cerr ) << "[@" << file << ":" << line <<  " | "  << name << " (" << level_str[0] << ") | " << current_time << " | " << msg << " ]" <<  std::endl; 
 #include <MT/opengl.h>
 
-#include "logisticRegression.h"
-#include "naiveBayesClassificator.h"
-#include "gaussProcClassificator.h"
-#include "dataReader.h"
-#include "activeLearningProcess.h"
-#include "oracle.h"
-#include "tester.h"
-#include "sampler.h"
-#include "gui.h"
+#include <JK/active_learning/al_logistic_regression.h>
+#include <JK/active_learning/al_gaussian_process.h>
+#include <JK/active_learning/al_grounded_symbol.h>
+#include <JK/active_learning/al_tester.h>
+#include <JK/active_learning/al_gui.h>
+#include <JK/active_learning/al_process.h>
+#include <JK/active_learning/al_tester.h>
 
-#include <MT/array_t.cpp>
+#include <JK/utils/oracle.h>
+#include <JK/utils/sampler.h>
+#include <biros/logging.h>
+
+//#include <relational/robotManipulationSimulator.h>
+//#include <JK/al/al_logistic_regression.h>
+//#include "naiveBayesClassificator.h"
+//#include "gaussProcClassificator.h"
+//#include "dataReader.h"
+//#include "activeLearningProcess.h"
+//#include "oracle.h"
+//#include "tester.h"
+//#include "sampler.h"
+//#include "gui.h"
+
+#include <MT/array_t.cxx>
 #include <MT/ors.h>
 
 #include <csignal>
@@ -20,6 +34,7 @@ void shutdown(int) {
   exit(0);
 }
 int main(int argc, char** argv) {
+  ClassifyData d;
   MT::initCmdLine(argc,argv);
   signal(SIGINT,shutdown);
 
@@ -51,7 +66,10 @@ int main(int argc, char** argv) {
   else
     cl.classificator = new LogisticRegression(new BlocksWorldSampler);
   cl.oracle = new OnOracle();
-  cl.tester = new Tester(50000, filename);
+  cl.tester = new Tester(5000, filename);
+
+  DEBUG_VAR(classify, d.get_numOfWorkingJobs(NULL));
+  DEBUG_VAR(classify, &d);
 
   ActiveLearningP alp;
   alp.traindata = &train;
@@ -59,7 +77,11 @@ int main(int argc, char** argv) {
   //alp.guiData = &guiData;
  
   alp.threadOpen();
-  alp.threadSteps(n_steps);
+  alp.threadLoop();
+
+  while(true) {
+    MT::wait(.1);
+  }
 
   //gui.threadOpen();
   //gui.threadLoop();
