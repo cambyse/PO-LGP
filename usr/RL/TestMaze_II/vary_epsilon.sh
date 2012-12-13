e10a20082713b98917187fa8f046e267dea051aa
Robert Lieck <robert.lieck@gmail.com>
2012-12-13 19:20:27 +0100

    new data

--- usr/RL/TestMaze_II/vary_epsilon.sh
+++ usr/RL/TestMaze_II/vary_epsilon.sh
@@ -1,7 +1,14 @@
 epsilon=.01
+discount=.01
 
-while [ "$epsilon" != "1.00" ]; do
-    ./TestMaze_II optimal $epsilon 0.9 &
+while [ "$discount" != "1.00" ]; do
+
+    ./TestMaze_II optimal $epsilon $discount &
+
     epsilon=`echo $epsilon+.01 | bc`
-    sleep 1
+    if [ "$epsilon" == "1.00" ]; then
+	epsilon=.01
+	discount=`echo $discount+.01 | bc`
+    fi
+
 done
