cf5b2f02b9bd55ca81e754ecd156e9882130664f
Robert Lieck <robert.lieck@gmail.com>
2013-02-01 11:41:23 +0100

    add min/max function to util

--- usr/RL/TestMaze_II/util.h
+++ usr/RL/TestMaze_II/util.h
@@ -15,6 +15,10 @@ namespace util {
 bool arg_int(const QString& string, const int& n, int& i);
 bool arg_double(const QString& string, const int& n, double& d);
 bool arg_string(const QString& string, const int& n, QString& s);
+template < class C >
+C min(const C& c1, const C& c2) { return c1<c2 ? c1 : c2; }
+template < class C >
+C max(const C& c1, const C& c2) { return c1>c2 ? c1 : c2; }
 
 } // end namespace util
 
