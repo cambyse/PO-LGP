7aa2b2069052f8fcee86de3ddb2b2f99b751ea21
Marc Toussaint <marc.toussaint@fu-berlin.de>
2013-02-07 16:00:31 +0100

    ximea :-(

---
+++ usr/MT/ximea/main.cpp
@@ -0,0 +1,93 @@
+// Sample for XIMEA Software Package V2.57
+#include <xiApi.h>
+#include <xiExt.h>
+
+// docu:
+// http://www.ximea.com/support/wiki/apis/XiApi_Manual
+
+#include <MT/util.h>
+#include <MT/array.h>
+#include <MT/opengl.h>
+
+#define HandleResult(res,place) if (res!=XI_OK) {printf("Error after %s (%d)",place,res);goto finish;}
+
+int main(int argc, char** argv){
+  // image buffer
+  XI_IMG image;
+  image.size = sizeof(XI_IMG);
+  image.bp = NULL;
+  image.bp_size = 0;
+
+  byteA img;
+  OpenGL gl;
+  gl.img = &img;
+  ofstream file("video.raw", std::ios::out | std::ios::binary);
+
+  // Sample for XIMEA API V2.10
+  HANDLE xiH = NULL;
+  XI_RETURN stat = XI_OK;
+
+  // Get number of camera devices
+  DWORD dwNumberOfDevices = 0;
+  stat = xiGetNumberDevices(&dwNumberOfDevices);
+  HandleResult(stat,"xiGetNumberDevices (no camera found)");
+
+  if (!dwNumberOfDevices)
+  {
+    printf("No camera found\n");
+    goto finish;
+  }
+
+  // Retrieving a handle to the camera device
+  stat = xiOpenDevice(0, &xiH);
+  HandleResult(stat,"xiOpenDevice");
+
+  // Setting "exposure" parameter (10ms=10000us)
+  stat = xiSetParamInt(xiH, XI_PRM_EXPOSURE, 10000);
+  HandleResult(stat,"xiSetParam (exposure set)");
+
+  stat = xiSetParamInt(xiH, XI_PRM_IMAGE_DATA_FORMAT, XI_MONO8);
+  HandleResult(stat,"xiSetParam (format)");
+
+  stat = xiSetParamInt(xiH, XI_PRM_DOWNSAMPLING, 2);
+  HandleResult(stat,"xiSetParam (downsampling)");
+
+  float min_fps,max_fps;
+  xiGetParamFloat(xiH, XI_PRM_FRAMERATE XI_PRM_INFO_MIN, &min_fps);
+  xiGetParamFloat(xiH, XI_PRM_FRAMERATE XI_PRM_INFO_MAX, &max_fps);
+  cout <<"available frame rates:" <<min_fps <<'-' <<max_fps <<endl;
+
+  int cfa;
+  xiGetParamInt(xiH, XI_PRM_COLOR_FILTER_ARRAY, &cfa);
+  cout <<"buffer format=" <<cfa <<endl;
+
+  int iscolor;
+  xiGetParamInt(xiH, XI_PRM_IMAGE_IS_COLOR, &iscolor);
+  cout <<"color=" <<iscolor <<endl;
+
+  // Start acquisition
+  stat = xiStartAcquisition(xiH);
+  HandleResult(stat,"xiStartAcquisition");
+
+  for (int images=0; images<100 ;images++){
+    // getting image from camera
+    stat = xiGetImage(xiH, 5000, &image);
+    HandleResult(stat,"xiGetImage");
+    cout <<"image received: height=" <<image.height <<" width=" <<image.width <<" bp=" <<image.bp <<" bp_size=" <<image.bp_size <<" nframe=" <<image.nframe <<" format=" <<image.frm <<endl;
+    if(image.frm==XI_MONO8) file.write((char*)image.bp, image.height*image.width);
+    if(image.frm==XI_RAW8)  file.write((char*)image.bp, image.height*image.width);
+    if(image.frm==XI_RGB24)  file.write((char*)image.bp, 3*image.height*image.width);
+//      img.referTo((byte*)image.bp,image.height*image.width);
+//      img.reshape(image.height,image.width);
+//      gl.update();
+//    }
+  }
+
+finish:
+
+  file.close();
+  // Close device
+  if(xiH) xiCloseDevice(xiH);
+  printf("Done\n");
+  return 0;
+}
