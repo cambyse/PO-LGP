97c1f5015cfdce31445dc383ae9d824f338fd562
Stefan Otte <stefan.otte@gmail.com>
2013-04-05 15:05:34 +0200

    orspy: extend orspy test: set jointState manually.

--- share/test/orspy/ors_test.py
+++ share/test/orspy/ors_test.py
@@ -16,13 +16,20 @@ if __name__ == '__main__':
     gl = orspy.OpenGL()
     orspy.bindOrsToOpenGL(graph, gl)
 
+    # follow random trajectory
     # data
     X = orspy.ArrayDouble()
     V = orspy.ArrayDouble()
     orspy.generateSequence(X, V, graph.getJointStateDimension())
-
-    # follow random trajectory
     for t in range(X.d0):
         graph.setJointState(X[t])
         graph.calcBodyFramesFromJoints()
         gl.timedupdate(0.01)
+
+    # set joint state directly (with a python list)
+    jsd = graph.getJointStateDimension()
+    for i in range(700):
+        jointState = [0.01 * i] * jsd
+        graph.setJointStateList(jointState)
+        graph.calcBodyFramesFromJoints()
+        gl.update()
