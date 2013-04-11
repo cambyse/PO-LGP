583e10af26ed1cd6f3cf99e2edda875e22809cbe
Stefan Otte <stefan.otte@gmail.com>
2013-04-11 17:18:19 +0200

    orspy: add and test matrix addition.

--- share/src/MT/ors.i
+++ share/src/MT/ors.i
@@ -242,7 +242,7 @@ struct Matrix {
   Matrix(const arr& m);
   double *p();
 
-  void set(double* m);
+  // void set(double* m);
   void setZero();
   void setRandom(double range=1.);
   void setId();
@@ -256,6 +256,22 @@ struct Matrix {
 
   void write(std::ostream&) const;
   void read(std::istream&);
+%extend {
+  Matrix __add__(const Matrix& other) { return *$self + other; };
+}
+%pythoncode %{
+def set(self, lst):
+    assert(len(lst) >= 9)
+    self.m00 = lst[0]
+    self.m01 = lst[1]
+    self.m02 = lst[2]
+    self.m10 = lst[3]
+    self.m11 = lst[4]
+    self.m12 = lst[5]
+    self.m20 = lst[6]
+    self.m21 = lst[7]
+    self.m22 = lst[8]
+%} // end of %pythoncode
 };
 
 
--- share/unittest/orspy/test_ors_datastructures.py
+++ share/unittest/orspy/test_ors_datastructures.py
@@ -25,6 +25,27 @@ class Test_VectorOperatorOverloading(unittest.TestCase):
         self.assertAlmostEqual(v3.z, -1.)
 
 
+class Test_MatrixOperatorOverloading(unittest.TestCase):
+    def test_add(self):
+        m1, m2 = orspy.Matrix(), orspy.Matrix()
+        m1.set([1.] * 9)
+        m2.set([2.] * 9)
+        m3 = m1 + m2
+
+        # check each value
+        self.assertAlmostEqual(m3.m00, 3.)
+        self.assertAlmostEqual(m3.m01, 3.)
+        self.assertAlmostEqual(m3.m02, 3.)
+
+        self.assertAlmostEqual(m3.m10, 3.)
+        self.assertAlmostEqual(m3.m11, 3.)
+        self.assertAlmostEqual(m3.m12, 3.)
+
+        self.assertAlmostEqual(m3.m20, 3.)
+        self.assertAlmostEqual(m3.m21, 3.)
+        self.assertAlmostEqual(m3.m22, 3.)
+
+
 # run tests
 if __name__ == '__main__':
     unittest.main()
