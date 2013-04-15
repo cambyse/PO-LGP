a90c84fc4bf42884343df147056caa3ce2f4eca1
Robert Lieck (Thinkpad) <robert.lieck@gmail.com>
2013-04-15 18:33:16 +0200

    fix indentation

--- usr/RL/TestMaze_II/util.cpp
+++ usr/RL/TestMaze_II/util.cpp
@@ -11,26 +11,27 @@
 
 namespace util {
 
-bool arg_int(const QString& string_in, const int& n, int& i_arg_out) {
-    QString arg = string_in.section(QRegExp("\\s+"),n,n);
-    bool ok;
-    i_arg_out = arg.toInt(&ok);
-    return ok;
-}
+    bool arg_int(const QString& string_in, const int& n, int& i_arg_out) {
+        QString arg = string_in.section(QRegExp("\\s+"),n,n);
+        bool ok;
+        i_arg_out = arg.toInt(&ok);
+        return ok;
+    }
 
-bool arg_double(const QString& string_in, const int& n, double& d_arg_out) {
-    QString arg = string_in.section(QRegExp("\\s+"),n,n);
-    bool ok;
-    d_arg_out = arg.toDouble(&ok);
-    return ok;
-}
+    bool arg_double(const QString& string_in, const int& n, double& d_arg_out) {
+        QString arg = string_in.section(QRegExp("\\s+"),n,n);
+        bool ok;
+        d_arg_out = arg.toDouble(&ok);
+        return ok;
+    }
 
-bool arg_string(const QString& string_in, const int& n, QString& s_arg_out) {
-    s_arg_out = string_in.section(QRegExp("\\s+"),n,n);
-    return true;
-}
+    bool arg_string(const QString& string_in, const int& n, QString& s_arg_out) {
+        s_arg_out = string_in.section(QRegExp("\\s+"),n,n);
+        return true;
+    }
 
-double approx_equal_tolerance() { return 1e-10; }
+    double approx_equal_tolerance() { return 1e-10; }
+
     const InvalidBase INVALID = InvalidBase(true);
 
 } // end namespace util
