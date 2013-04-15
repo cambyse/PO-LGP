aadd5d3883fddea21bd0f450a60ea600f84e15cf
Stefan Otte <stefan.otte@gmail.com>
2013-04-15 17:02:35 +0200

    pep8ify controller.py

--- share/projects/the_curious_robot/src/controller.py
+++ share/projects/the_curious_robot/src/controller.py
@@ -10,7 +10,7 @@ import roslib
 roslib.load_manifest('the_curious_robot')
 import rospy
 import the_curious_robot.msg as msgs
-import numpy as np
+# import numpy as np
 import orspy as ors
 
 
@@ -29,7 +29,7 @@ class FakeController():
         ors.bindOrsToOpenGL(self.world, self.gl)
 
         self.pub = rospy.Publisher('geometric_state', msgs.ors)
-        self.traj_sub = rospy.Subscriber(name='control', 
+        self.traj_sub = rospy.Subscriber(name='control',
                                          data_class=msgs.control,
                                          callback=self.control_cb)
 
@@ -57,12 +57,13 @@ class FakeController():
     def control_cb(self, data):
         print "Got control message."
         if not self.goal:
-            self.goal = ors.Transformation();
-        
+            self.goal = ors.Transformation()
+
         self.goal.pos.x = data.pose.position.x
         self.goal.pos.y = data.pose.position.y
         self.goal.pos.z = data.pose.position.z
         #self.goal.rot = data.pose.orientation
+
 
 if __name__ == '__main__':
     controller = FakeController()
