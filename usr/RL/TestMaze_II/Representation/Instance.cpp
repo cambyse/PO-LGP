3eb4086e14b455aecca684b2eb1a802690d14ead
Robert Lieck (Thinkpad) <robert.lieck@gmail.com>
2013-04-17 20:09:01 +0200

    Instance and InstanceIt classes.

--- usr/RL/TestMaze_II/Representation/Instance.cpp
+++ usr/RL/TestMaze_II/Representation/Instance.cpp
@@ -1,3 +1,180 @@
 #include "Instance.h"
 
-Instance::Instance() {}
+using util::INVALID;
+
+Instance::Instance(
+    const Action& a,
+    const State& s,
+    const Reward& r,
+    Instance * prev,
+    Instance * next
+    ):
+    util::InvalidAdapter<Instance>(false),
+    action(a),
+    state(s),
+    reward(r),
+    previous_instance(prev),
+    next_instance(next)
+{}
+
+Instance::~Instance() {
+    // delete instance and also fix previous and next
+}
+
+Instance & Instance::operator++() {
+    if(next_instance==nullptr) {
+        this->invalidate();
+    } else {
+        (*this) = (*next_instance);
+    }
+    return *this;
+}
+
+Instance & Instance::operator--() {
+    if(previous_instance==nullptr) {
+        this->invalidate();
+    } else {
+        (*this) = (*previous_instance);
+    }
+    return *this;
+}
+
+Instance & Instance::insert_instance_after(
+    const Action& a,
+    const State& s,
+    const Reward& r
+    ) {
+    Instance * new_instance = new Instance(a,s,r);
+
+    new_instance->next_instance = this->next_instance;
+    if(this->next_instance!=nullptr) {
+        this->next_instance->previous_instance = new_instance;
+    }
+
+    new_instance->previous_instance = this;
+    this->next_instance = new_instance;
+
+    return *new_instance;
+}
+
+Instance & Instance::insert_instance_before(
+    const Action& a,
+    const State& s,
+    const Reward& r
+    ) {
+    Instance * new_instance = new Instance(a,s,r);
+
+    new_instance->previous_instance = this->previous_instance;
+    if(this->previous_instance!=nullptr) {
+        this->previous_instance->next_instance = new_instance;
+    }
+
+    new_instance->next_instance = this;
+    this->previous_instance = new_instance;
+
+    return *new_instance;
+}
+
+Instance & Instance::append_instance(
+    const Action& a,
+    const State& s,
+    const Reward& r
+    ) {
+    Instance * current_instance = this;
+    while(current_instance->next_instance!=nullptr) {
+        current_instance = current_instance->next_instance;
+    }
+    return current_instance->insert_instance_after(a,s,r);
+}
+
+Instance & Instance::prepend_instance(
+    const Action& a,
+    const State& s,
+    const Reward& r
+    ) {
+    Instance * current_instance = this;
+    while(current_instance->previous_instance!=nullptr) {
+        current_instance = current_instance->previous_instance;
+    }
+    return current_instance->insert_instance_before(a,s,r);
+}
+
+Instance Instance::first() const {
+    const Instance * current_instance = this;
+    while(current_instance->previous_instance!=nullptr) {
+        current_instance = current_instance->previous_instance;
+    }
+    return *current_instance;
+}
+
+Instance Instance::last() const {
+    const Instance * current_instance = this;
+    while(current_instance->next_instance!=nullptr) {
+        current_instance = current_instance->next_instance;
+    }
+    return *current_instance;
+}
+
+std::ostream& operator<<(std::ostream &out, const Instance& i) {
+    out << "(" <<
+        i.action << ", " <<
+        i.state << ", " <<
+        i.reward << ")";
+    return out;
+}
+
+InstanceIt::InstanceIt() {
+    *this = first();
+}
+
+InstanceIt::InstanceIt( const ActionIt& a, const StateIt& s, const RewardIt& r ):
+    util::InvalidAdapter<InstanceIt>(false),
+    action(a), state(s), reward(r)
+{}
+
+InstanceIt & InstanceIt::operator++() {
+    ++reward;
+    if(reward==INVALID) {
+        reward = RewardIt::first();
+        ++state;
+        if(state==INVALID) {
+            state = StateIt::first();
+            ++action;
+            if(action==INVALID) {
+                action = ActionIt::first();
+                this->invalidate();
+            }
+        }
+    }
+    return *this;
+}
+
+InstanceIt & InstanceIt::operator--() {
+    --reward;
+    if(reward==INVALID) {
+        reward = RewardIt::last();
+        --state;
+        if(state==INVALID) {
+            state = StateIt::last();
+            --action;
+            if(action==INVALID) {
+                action = ActionIt::last();
+                this->invalidate();
+            }
+        }
+    }
+    return *this;
+}
+
+const InstanceIt InstanceIt::first() {
+    return InstanceIt(ActionIt::first(), StateIt::first(), RewardIt::first());
+}
+
+const InstanceIt InstanceIt::last() {
+    return InstanceIt(ActionIt::last(), StateIt::last(), RewardIt::last());
+}
+
+std::ostream& operator<<(std::ostream &out, const InstanceIt& i) {
+    out << (Instance)i;
+    return out;
+}
--- usr/RL/TestMaze_II/Representation/Instance.h
+++ usr/RL/TestMaze_II/Representation/Instance.h
@@ -3,27 +3,65 @@
 #ifndef INSTANCE_H_
 #define INSTANCE_H_
 
+#include "../util.h"
 #include "Action.h"
 #include "State.h"
 #include "Reward.h"
 
-#include <vector>
+#include <ostream>
 
-/** \brief Instance object */
-class Instance {
-protected:
-    typedef std::vector<Instance*> container_t;
+/** \brief Instance object.
+ *
+ * Instance objects are essentially triples of (Action, State, Reward)
+ * representing all information associated with one time step in a reinforcement
+ * learning process. Additionally every Instance stores pointers to the Instance
+ * before and after, so one can iterate through time. */
+class Instance: public util::InvalidAdapter<Instance> {
 public:
     Action action;
     State state;
     Reward reward;
-    Instance();
+    Instance(
+        const Action& a = Action(),
+        const State& s = State(),
+        const Reward& r = Reward(),
+        Instance * prev = nullptr,
+        Instance * next = nullptr
+        );
+    ~Instance();
     Instance & operator++();
     Instance & operator--();
-    bool is_valid() const;
+    Instance & insert_instance_after  (const Action& a, const State& s, const Reward& r);
+    Instance & insert_instance_before (const Action& a, const State& s, const Reward& r);
+    Instance & append_instance        (const Action& a, const State& s, const Reward& r);
+    Instance & prepend_instance       (const Action& a, const State& s, const Reward& r);
+    Instance first() const;
+    Instance last() const;
+    friend std::ostream& operator<<(std::ostream &out, const Instance& i);
 protected:
     Instance * previous_instance, * next_instance;
-    container_t * container;
+};
+
+/** \brief Iterator over Instances.
+ *
+ * InstanceIt iterate over all possible Instance object by iterating their
+ * Action, State, and Reward component. They are much like Instances and can be
+ * implicitely converted to Instance objects. As opposed to what happens when
+ * iterating Instance objects themselves, InstanceIt objects traverses the space
+ * of possible instances, not the sequence of all instances occurred in time. */
+class InstanceIt: public util::InvalidAdapter<InstanceIt> {
+public:
+    ActionIt action;
+    StateIt state;
+    RewardIt reward;
+    InstanceIt();
+    InstanceIt(const ActionIt& a, const StateIt& s, const RewardIt& r);
+    InstanceIt & operator++();
+    InstanceIt & operator--();
+    static const InstanceIt first();
+    static const InstanceIt last();
+    operator Instance() const { return Instance(action,state,reward); }
+    friend std::ostream& operator<<(std::ostream &out, const InstanceIt& i);
 };
 
 #endif // INSTANCE_H_
