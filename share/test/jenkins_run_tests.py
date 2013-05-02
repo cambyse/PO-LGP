ec2db46702bfe62901485757a53ad42e33d3d143
Stefan Otte <stefan.otte@gmail.com>
2013-05-02 15:01:00 +0200

    Only compile when Makefile exists & time test.

--- share/test/jenkins_run_tests.py
+++ share/test/jenkins_run_tests.py
@@ -21,6 +21,7 @@ from xml.etree.ElementTree import SubElement
 from xml.etree.ElementTree import tostring
 from xml.dom import minidom
 
+import datetime
 import os
 import envoy
 
@@ -35,16 +36,27 @@ def prettyprintXML(elem):
 def main():
     root = Element("testsuite")
     for filename in os.listdir('.'):
-        if os.path.isdir(filename):
-            print filename
-            r = envoy.run("make -C %s" % filename)
-            testcase = SubElement(root, "testcase",
-                                  {"classname": "test", "name": filename})
-            if r.status_code != 0:
-                failure = SubElement(testcase, "failure", {"type": "error"})
-                print '\t', "status_code:", r.status_code
-                # failure.text =  r.std_err
-                failure.text = "TODO add compile error"
+        if not os.path.isdir(filename):
+            continue
+        if not os.path.exists(os.path.join(filename, "Makefile")):
+            continue
+
+        print filename
+        t_start = datetime.datetime.now()
+        r = envoy.run("make -C %s" % filename)
+        t_end = datetime.datetime.now()
+        t_diff = str((t_end - t_start).total_seconds() / 1000.)
+
+        testcase = SubElement(root, "testcase",
+                              {"classname": "test",
+                               "name": filename,
+                               "time": t_diff
+                               })
+        if r.status_code != 0:
+            failure = SubElement(testcase, "failure", {"type": "error"})
+            print '\t', "status_code:", r.status_code
+            # failure.text =  r.std_err
+            failure.text = "Did not compile!"
 
     s = prettyprintXML(root)
     open("test_run.xml", "w").write(s)
