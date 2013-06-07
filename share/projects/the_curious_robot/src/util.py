e79957f241fd4aec8491b366b53954397084d361
Johannes Kulick <kulick@hildensia.de>
2013-06-07 13:46:35 +0200

    parsing of different msgs

--- share/projects/the_curious_robot/src/util.py
+++ share/projects/the_curious_robot/src/util.py
@@ -23,6 +23,7 @@ class Properties():
                 if not (attr.startswith("__") or
                         callable(getattr(self, attr)))]
 
+# msg parser/builder
 
 def parse_body_msg(msg):
     body = ors.Body()
@@ -30,3 +31,20 @@ def parse_body_msg(msg):
     body.name = body_list[0]
     body.read(body_list[1])
     return body
+
+def parse_property_msg(msg):
+    properties = Properties()
+    for p in msg:
+        setattr(properties, p.name, Gaussian(p.values[0], p.values[1]))
+    return properties
+
+def parse_ooi_msg(msg):
+    body = parse_body_msg(msg.body)
+    properties = parse_property_msg(msg.properties)
+    return (body, properties)
+
+def parse_oois_msg(msg):
+    objects = []
+    for obj in msg.objects:
+        objects.append(parse_ooi_msg(obj))
+    return objects
