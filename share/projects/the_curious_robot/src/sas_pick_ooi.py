c51c4120e29e2c04088e8fe6a826021c59e665be
Johannes Kulick <kulick@hildensia.de>
2013-06-07 13:46:56 +0200

    pick an object of interest at random (so far)

--- share/projects/the_curious_robot/src/sas_pick_ooi.py
+++ share/projects/the_curious_robot/src/sas_pick_ooi.py
@@ -5,21 +5,44 @@ roslib.load_manifest('the_curious_robot')
 roslib.load_manifest('actionlib')
 import rospy
 from actionlib import SimpleActionServer
-from the_curious_robot.msg import PickOOIAction
+import the_curious_robot.msg as msgs
+import random
+import util
 
 class PickOOIActionServer:
     def __init__(self, name):
-        self.server = SimpleActionServer(name, PickOOIAction,
+        self.oois = None
+
+        # Subscriber
+        self.oois_sub = rospy.Subscriber('oois', msgs.Objects, self.oois_cb)
+        # right now we don't need the world_belief, that might change
+        # self.world_belief_sub = rospy.Subscriber ...
+
+        # Publisher
+        self.ooi_id_pub = rospy.Publisher('ooi_id', msgs.ObjectID)
+
+        # Actionlib Server
+        self.server = SimpleActionServer(name, msgs.PickOOIAction,
                 execute_cb=self.execute)
         self.server.start()
 
     def execute(self, msg):
-        rospy.sleep(.33);
-        rospy.logdebug('Pick object of interest')
+        if self.oois is None:
+            self.server.set_aborted()
+            return
+
+        ooi = random.choice(self.oois)
+        ooi_id_msg = msgs.ObjectID()
+        ooi_id_msg.id = ooi[0].name
+        self.ooi_id_pub.publish(ooi_id_msg)
         self.server.set_succeeded()
 
+    def oois_cb(self, msg):
+        rospy.logdebug("callback")
+        self.oois = util.parse_oois_msg(msg)
+
 def main():
-    rospy.init_node('tcr_sas_pick_ooi')
+    rospy.init_node('tcr_sas_pick_ooi', log_level=rospy.DEBUG)
     server = PickOOIActionServer('pick_ooi')
 
 if __name__ == '__main__':
