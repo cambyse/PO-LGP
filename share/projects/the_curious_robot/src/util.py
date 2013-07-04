54b04102b9078973a0ae5d553848833e4c50e806
Johannes Kulick <kulick@hildensia.de>
2013-07-04 17:29:55 +0200

    add msg parser/creater

--- share/projects/the_curious_robot/src/util.py
+++ share/projects/the_curious_robot/src/util.py
@@ -1,4 +1,10 @@
 import orspy as ors
+
+import roslib
+import rospy
+import the_curious_robot.msg as msgs
+import geometry_msgs.msg
+
 class Gaussian():
     """
     Represent Gaussians with this class.
@@ -35,16 +41,37 @@ def parse_body_msg(msg):
     body.read(body_list[1])
     return body
 
+def create_body_msg(body):
+    return body.name + ' ' + str(body)
+
 def parse_property_msg(msg):
     properties = Properties()
     for p in msg:
         setattr(properties, p.name, Gaussian(p.values[0], p.values[1]))
     return properties
 
+def create_properties_msg(properties):
+    result = []
+    for prop_name in properties.property_names():
+        msg = msgs.Property()
+        msg.name = prop_name
+        msg.values = [ getattr(ooi[1], prop_name).mu,
+                       getattr(ooi[1], prop_name),sigma ]
+        result.append(msg)
+    return result
+
 def parse_ooi_msg(msg):
     body = parse_body_msg(msg.body)
     properties = parse_property_msg(msg.properties)
     return {"body": body, "properties": properties}
+
+def create_ooi_msg(body, properties):
+    body_msg = create_body_msg(body)
+    properties_msg = create_properties_msg(properties)
+    msg = msgs.Object()
+    msg.body = body_msg
+    msg.properties = properties_msg
+    return msg
 
 def parse_oois_msg(msg):
     objects = []
@@ -52,4 +79,19 @@ def parse_oois_msg(msg):
         objects.append(parse_ooi_msg(obj))
     return objects
 
+def create_oois_msg(objects):
+    msg = msgs.Objects()
+    for obj in objects:
+        msg.objects.append(create_ooi_msg(obj))
+    return msg
 
+def create_trajectory_msg(obj_id, pos):
+    msg = msgs.Trajectory()
+    for p in pos:
+        pose = geometry_msgs.msg.Pose()
+        pose.position.x = p.x
+        pose.position.y = p.y
+        pose.position.z = p.z
+        msg.pos.append(pose)
+    msg.object = obj_id
+    return msg
