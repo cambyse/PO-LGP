b8111347f3170064d0a25e1c732ddac4029b108e
Marc Toussaint <marc.toussaint@fu-berlin.de>
2013-09-17 10:13:02 +0200

    src compiles complete - old code retired; JK retired (didnt compile)

--- share/src/relational/decisionMakingModule.h
+++ share/src/relational/decisionMakingModule.h
@@ -1,8 +1,8 @@
 #include <Core/array.h>
 #include <Ors/ors.h>
-#include <System/biros.h>
+#include <Core/module.h>
 
-struct DecisionMakingModule:public Process{
+struct DecisionMakingModule:Module {
   
   const static uint SYMBOLIC_ACTION__NO_ACTION_FOUND = 0;
   const static uint SYMBOLIC_ACTION__FINISHED = 1;
