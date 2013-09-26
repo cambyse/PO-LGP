f372ab086f5c2eeb9d41a16c8b463526202bd671
Robert Lieck (Kermit) <science@robert-lieck.com>
2013-09-26 23:12:17 +0200

    add simple startup script

---
+++ usr/RL/TestMaze_II/start.sh
@@ -0,0 +1,35 @@
+## print kind of help if no arguments are given
+if [ $# -lt 1 ]; then
+    echo "expecting at least one argument"
+    echo "    [ verbose | quiet [ OMP_NUM_THREADS ] ]"
+    exit 0
+fi
+
+## print error message if first argument doesn't match
+if [ "$1" != "verbose" -a "$1" != "quiet" ]; then
+    echo "first argument must be 'verbose' or 'quiet'"
+    exit 0
+fi
+
+## print error message if second argument doesn't match
+## set number of threads otherwise
+if [ $# -gt 1 ]; then
+    if [ $2 -eq $2 2> /dev/null ]; then
+	export OMP_NUM_THREADS=$2
+    else
+	echo "second argument must be a number"
+	exit 0
+    fi
+else
+    unset OMP_NUM_THREADS
+fi
+
+## define string for calling the program
+start_string="./TestMaze_II -mode SPARSE -sample UNIFORM -nEp 100 -maxTran 22 -minTrain 5000 -maxTrain 5000 -maxTree 50000 -l1 0.0003 -incr 0 -fincr 50 -dl 0.0001"
+
+## call the program
+if [ "$1" == "verbose" ]; then
+    eval "$start_string"
+else
+    eval "nohup $start_string &"
+fi
