4494f7ee80992f4345498b50d7ba0842af1056eb
Andrea Baisero <andrea.baisero@ipvs.uni-stuttgart.de>
2013-09-27 13:58:50 +0200

    timestamp in visualizer

--- share/examples/Hardware/g4_ors/main.cpp
+++ share/examples/Hardware/g4_ors/main.cpp
@@ -26,12 +26,15 @@ void threadedRun(){
   poseStr << "z." << nowStr << ".g4_pose.dat";
   FILE *fil = fopen((const char*)timesStr,"w");
   ofstream file_poses((const char*)poseStr);
+  char ts[25];
 
   engine().open(S);
   uint t;
   for(t=0;/*t<100*/;t++){
     if(engine().shutdown) break;
+    cout << "waiting" << endl;
     S.currentPoses.var->waitForNextWriteAccess();
+    cout << "done" << endl;
     gettimeofday(&time, 0);
     poses = S.currentPoses.get();
     poses.reshape(poses.N/7,7);
@@ -40,13 +43,16 @@ void threadedRun(){
       ors.bodies(b+1)->X.pos.set(poses(b,0), poses(b,1), poses(b,2));
       ors.bodies(b+1)->X.rot.set(poses(b,3), poses(b,4), poses(b,5), poses(b,6));
     }
+    sprintf(ts, "%4i %8li.%06li", t, time.tv_sec&0xffffff, time.tv_usec);
+    gl.text.clear() << ts;
+
     ors.calcShapeFramesFromBodies();
     gl.update();
 
-    fprintf(fil, "%4i %8li.%06li\n", t, time.tv_sec&0xffffff, time.tv_usec);
+    fprintf(fil, "%s\n", ts);
     fflush(fil);
 
-    file_poses <<poses <<endl; //-- just for clarity, change it to binary later
+    file_poses <<poses <<endl; //-- TODO just for clarity, change it to binary later
     //file_poses.write((char*)poses.p, poses.sizeT*poses.N);
     //file_poses.flush();
   }
@@ -54,7 +60,7 @@ void threadedRun(){
 
   fclose(fil);
   file_poses.close();
-  cout <<"bye bye" <<endl;
+  cout << "bye bye" << endl;
   engine().close(S);
 }
 
