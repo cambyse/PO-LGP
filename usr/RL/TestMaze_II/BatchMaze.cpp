255e1c8ab21d02f121798705d1802ebec8b8b264
Robert Lieck (snuff) <science@robert-lieck.com>
2013-09-29 22:56:14 +0200

    Fix BatchMaze

--- usr/RL/TestMaze_II/BatchMaze.cpp
+++ usr/RL/TestMaze_II/BatchMaze.cpp
@@ -642,7 +642,7 @@ int BatchMaze::run_active() {
             // extra info for some methods
             QString extra_info("");
             if(mode=="SPARSE") {
-                extra_info = QString("data_likelihood: %1	l1: ").arg(likelihood).arg(switch_double("-l1"));
+                extra_info = QString("data_likelihood: %1	l1: %2").arg(likelihood).arg(switch_double("-l1"));
             } else if(mode=="LINEAR_Q") {
                 extra_info = QString("TD loss: %1").arg(loss);
             } else if(mode=="UTREE_PROB"  ||
