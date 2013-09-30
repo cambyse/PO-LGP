d2c79bff9cd72293f09fd0e631ced83d452e5003
Robert Lieck (snuff) <science@robert-lieck.com>
2013-09-30 14:51:34 +0200

    things

--- usr/RL/TestMaze_II/start.sh
+++ usr/RL/TestMaze_II/start.sh
@@ -1,5 +1,9 @@
 ## define string for calling the program
-start_string="-mode UTREE_VALUE -sample UNIFORM -nEp 1000 -maxTran 12 -minTrain 100 -maxTrain 1000 -maxTree 10000 -f 2 -l1 0.001 -incr 100"
+
+## 2x2
+start_string="-mode UTREE_PROB -sample UNIFORM -nEp 1000 -maxTran 12 -minTrain 10 -maxTrain 100 -maxTree 10000 -f 2 -l1 0.0002 -incr 10"
+
+## 4x4
 #start_string="-mode SPARSE -sample UNIFORM -nEp 1000 -maxTran 22 -minTrain 100 -maxTrain 1000 -maxTree 50000 -l1 0.001 -incr 100 -fincr 50 -dl 0.0001"
 #start_string="-mode UTREE_PROB -sample UNIFORM -nEp 2000 -maxTran 22 -minTrain 500 -maxTrain 10000 -maxTree 50000 -incr 500"
 
