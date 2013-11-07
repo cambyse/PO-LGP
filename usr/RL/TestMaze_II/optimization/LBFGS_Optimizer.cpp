08b2f5cf6d786e07b0ea901897c4e1b0ec3f49d2
Robert Lieck (Thinkpad) <robert.lieck@gmail.com>
2013-11-07 17:55:00 +0100

    new base class for using L-BFGS optimizer

---
+++ usr/RL/TestMaze_II/optimization/LBFGS_Optimizer.cpp
@@ -0,0 +1,242 @@
+#include "LBFGS_Optimizer.h"
+#include "../lbfgs_codes.h"
+#include "../util.h"
+
+#include <math.h> // for fabs()
+#include <algorithm> // for min, max
+#include <iomanip> // for std::setw(n)
+
+#ifdef BATCH_MODE_QUIET
+#define DEBUG_LEVEL 0
+#else
+#define DEBUG_LEVEL 1
+#endif
+#define DEBUG_STRING "LBFGS: "
+#include "debug.h"
+
+using std::min;
+using std::max;
+
+using util::Range;
+
+LBFGS_Optimizer::LBFGS_Optimizer() {}
+
+LBFGS_Optimizer::~LBFGS_Optimizer() {
+    lbfgs_free(lbfgs_variables);
+}
+
+lbfgsfloatval_t LBFGS_Optimizer::static_objective(
+    void *instance,
+    const lbfgsfloatval_t *x,
+    lbfgsfloatval_t *g,
+    const int n,
+    const lbfgsfloatval_t /*step*/
+) {
+    return ((LBFGS_Optimizer*)instance)->objective(x,g,n);
+}
+
+lbfgsfloatval_t LBFGS_Optimizer::objective(
+    const lbfgsfloatval_t * /*x*/,
+    lbfgsfloatval_t *g,
+    const int n
+) {
+    DEBUG_ERROR("No objective function defined.");
+    lbfgsfloatval_t fx = 0;
+    for(int i=0; i<n; i++) {
+        g[i] = 0;
+    }
+    return fx;
+}
+
+int LBFGS_Optimizer::static_progress(
+    void *instance,
+    const lbfgsfloatval_t *x,
+    const lbfgsfloatval_t *g,
+    const lbfgsfloatval_t fx,
+    const lbfgsfloatval_t xnorm,
+    const lbfgsfloatval_t gnorm,
+    const lbfgsfloatval_t step,
+    int n,
+    int k,
+    int ls
+    ) {
+    return ((LBFGS_Optimizer*)instance)->progress(x,g,fx,xnorm,gnorm,step,n,k,ls);
+}
+
+int LBFGS_Optimizer::progress(
+    const lbfgsfloatval_t *x,
+    const lbfgsfloatval_t * /*g*/,
+    const lbfgsfloatval_t fx,
+    const lbfgsfloatval_t xnorm,
+    const lbfgsfloatval_t /*gnorm*/,
+    const lbfgsfloatval_t /*step*/,
+    int n,
+    int k,
+    int /*ls*/
+) {
+    DEBUG_OUT(1,"Iteration " << k << " (fx = " << fx << ", xnorm = " << xnorm << ")" );
+    for(int x_idx=0; x_idx<n; ++x_idx) {
+        DEBUG_OUT(1, "    x[" << x_idx << "] = " << x[x_idx]);
+    }
+    DEBUG_OUT(1,"Iteration " << k << " (fx = " << fx << ", xnorm = " << xnorm << ")" );
+    return 0;
+}
+
+lbfgsfloatval_t LBFGS_Optimizer::optimize(int * return_code, std::string * return_code_description) {
+
+    // Initialize the parameters for the L-BFGS optimization.
+    lbfgs_parameter_t param;
+    lbfgs_parameter_init(&param);
+    param.orthantwise_c = l1_factor;
+    param.delta = lbfgs_delta;
+    param.epsilon = lbfgs_epsilon;
+    if(l1_factor!=0) {
+        param.linesearch = LBFGS_LINESEARCH_BACKTRACKING;
+    }
+    if(maximum_iterations>0) {
+        param.max_iterations = maximum_iterations;
+    }
+
+    // Start the L-BFGS optimization
+    lbfgsfloatval_t fx;
+    int ret = lbfgs(number_of_variables, lbfgs_variables, &fx, static_objective, static_progress, this, &param);
+
+    // write return code and description
+    if(return_code!=nullptr) {
+        *return_code=ret;
+    }
+    if(return_code_description!=nullptr) {
+        *return_code_description=lbfgs_code(ret);
+    }
+
+    // Report the result.
+    DEBUG_OUT(1,"L-BFGS optimization terminated with status code = " << ret << " ( " << lbfgs_code(ret) << " )");
+
+    return fx;
+}
+
+void LBFGS_Optimizer::check_derivatives(
+    const int& number_of_samples,
+    const double& range,
+    const double& delta,
+    const double& allowed_maximum_relative_deviation,
+    const bool use_current_values
+    ) {
+
+    // initialize arrays
+    lbfgsfloatval_t * x = lbfgs_malloc(number_of_variables);
+    lbfgsfloatval_t * grad = lbfgs_malloc(number_of_variables);
+    lbfgsfloatval_t * grad_dummy = lbfgs_malloc(number_of_variables);
+
+    // remember maximum relative deviation
+    lbfgsfloatval_t achieved_maximum_relative_deviation = 0;
+
+    // print inital info
+    DEBUG_OUT(0,"===============================================================================================");
+    DEBUG_OUT(0,"Checking first derivatives:");
+    DEBUG_OUT(0,"    #samples                   " << number_of_samples );
+    DEBUG_OUT(0,"    range                      " << range << (use_current_values?" (centered around current values)":" (centered around zero)") );
+    DEBUG_OUT(0,"    delta                      " << delta );
+    DEBUG_OUT(0,"    maximum allowed deviation  " << allowed_maximum_relative_deviation );
+    DEBUG_OUT(1,"    -------------------------------------------------------------------------------------------");
+
+    // draw and evaluate samples
+    for(int sample_counter : Range(1,number_of_samples)) {
+
+        // get variables
+        for(int idx : Range(0,number_of_variables-1)) {
+            x[idx] = range * (2*drand48()-1);
+            if(use_current_values) {
+                x[idx] += lbfgs_variables[idx];
+            }
+        }
+
+        // calculate objective and analytical gradient at current point
+        lbfgsfloatval_t fx = objective(x,grad,number_of_variables);
+        DEBUG_OUT(1, "Sample " << sample_counter << " (fx = " << fx << ")" );
+        DEBUG_OUT(1,"    -------------------------------------------------------------------------------------------");
+            DEBUG_OUT(1,"    " <<
+                      std::setw(5) << "idx" <<
+                      std::setw(15) << "var" <<
+                      std::setw(15) << "grad_a" <<
+                      std::setw(15) << "grad_n" <<
+                      std::setw(15) << "dev" <<
+                      std::setw(15) << "dev/|grad|"
+                );
+        DEBUG_OUT(1,"    -------------------------------------------------------------------------------------------");
+
+        // calculate numerical gradient
+        for(int idx : Range(number_of_variables)) {
+
+            // go in positive direction
+            x[idx] += delta/2.;
+            lbfgsfloatval_t fx_plus = objective(x,grad_dummy,number_of_variables);
+            // go in negative direction
+            x[idx] -= delta;
+            lbfgsfloatval_t fx_minus = objective(x,grad_dummy,number_of_variables);
+            // reset x
+            x[idx] += delta/2.;
+
+            // numerical gradient
+            lbfgsfloatval_t ngrad = (fx_plus-fx_minus)/delta;
+
+            // compute relative deviation and update maximum relative deviation
+            lbfgsfloatval_t current_deviation = fabs(ngrad-grad[idx]);
+            lbfgsfloatval_t current_relative_deviation = current_deviation/min(fabs(ngrad),fabs(grad[idx]));
+            achieved_maximum_relative_deviation = max(achieved_maximum_relative_deviation,current_relative_deviation);
+
+            // print result
+            DEBUG_OUT(1,"    " <<
+                      std::setw(5) << idx <<
+                      std::setw(15) << x[idx] <<
+                      std::setw(15) << grad[idx] <<
+                      std::setw(15) << ngrad <<
+                      std::setw(15) << current_deviation <<
+                      std::setw(15) << current_relative_deviation
+                );
+        }
+    }
+    if(achieved_maximum_relative_deviation>allowed_maximum_relative_deviation) {
+        DEBUG_OUT(0, "ERRORS in first derivative (" << achieved_maximum_relative_deviation << " > " << allowed_maximum_relative_deviation << ")" );
+    } else {
+        DEBUG_OUT(0, "No errors in first derivative (" << achieved_maximum_relative_deviation << " < " << allowed_maximum_relative_deviation << ")" );
+    }
+    DEBUG_OUT(0,"===============================================================================================");
+
+    // free arrays
+    lbfgs_free(x);
+    lbfgs_free(grad);
+    lbfgs_free(grad_dummy);
+}
+
+LBFGS_Optimizer& LBFGS_Optimizer::set_number_of_variables(unsigned int n, bool zero) {
+    number_of_variables = n;
+    lbfgs_free(lbfgs_variables);
+    lbfgs_variables = lbfgs_malloc(n);
+    if(zero) {
+        for( int i : Range(n) ) {
+            lbfgs_variables[i] = 0;
+        }
+    }
+    return *this;
+}
+
+LBFGS_Optimizer& LBFGS_Optimizer::set_maximum_iterations(unsigned int n) {
+    maximum_iterations = n;
+    return *this;
+}
+
+LBFGS_Optimizer& LBFGS_Optimizer::set_l1_factor(lbfgsfloatval_t f) {
+    l1_factor = f;
+    return *this;
+}
+
+LBFGS_Optimizer& LBFGS_Optimizer::set_lbfgs_delta(lbfgsfloatval_t f) {
+    lbfgs_delta = f;
+    return *this;
+}
+
+LBFGS_Optimizer& LBFGS_Optimizer::set_lbfgs_epsilon(lbfgsfloatval_t f) {
+    lbfgs_epsilon = f;
+    return *this;
+}
---
+++ usr/RL/TestMaze_II/optimization/LBFGS_Optimizer.h
@@ -0,0 +1,145 @@
+#ifndef LBFGS_OPTIMIZER_H_
+#define LBFGS_OPTIMIZER_H_
+
+#include <string>
+
+#include <lbfgs.h>
+
+/** \brief Abstract class for using L-BFGS optimizer.
+ *
+ * Inherit from this class and reimplement the LBFGS_Optimizer::objective
+ * function to use the L-BFGS optimizer.*/
+class LBFGS_Optimizer {
+
+public:
+
+    LBFGS_Optimizer();
+    virtual ~LBFGS_Optimizer();
+
+    /** \brief Calls instance->objective(). */
+    static lbfgsfloatval_t static_objective(
+        void *instance,             ///< LBFGS_Optimizer object use
+        const lbfgsfloatval_t *x,   ///< Current variable values
+        lbfgsfloatval_t *g,         ///< Gradient to be computed
+        const int n,                ///< Number of variables
+        const lbfgsfloatval_t step  ///< Line-search step in this iteration
+    );
+
+    /** \brief Calls instance->progress(). */
+    static int static_progress(
+        void *instance,                ///< LBFGS_Optimizer object use
+        const lbfgsfloatval_t *x,      ///< Current variable values
+        const lbfgsfloatval_t *g,      ///< Gradient to be computed
+        const lbfgsfloatval_t fx,      ///< Current value of the objective
+        const lbfgsfloatval_t xnorm,   ///< Euclidian norm of the variables
+        const lbfgsfloatval_t gnorm,   ///< Euclidian norm of the gradient
+        const lbfgsfloatval_t step,    ///< Line-search step in this iteration
+        int n,                         ///< Number of variables
+        int k,                         ///< Iteration count
+        int ls                         ///< Number of evaluation for this iteration
+        );
+
+    /** \brief Compute gradient and return value of objective. */
+    virtual lbfgsfloatval_t objective(
+        const lbfgsfloatval_t *x,       ///< Current variable values
+        lbfgsfloatval_t *g,             ///< Gradient to be computed
+        const int n                     ///< Number of variables
+    );
+
+
+    /** \brief Prints progress information during optimization. */
+    virtual int progress(
+        const lbfgsfloatval_t *x,      ///< Current variable values
+        const lbfgsfloatval_t *g,      ///< Gradient to be computed
+        const lbfgsfloatval_t fx,      ///< Current value of the objective
+        const lbfgsfloatval_t xnorm,   ///< Euclidian norm of the variables
+        const lbfgsfloatval_t gnorm,   ///< Euclidian norm of the gradient
+        const lbfgsfloatval_t step,    ///< Line-search step in this iteration
+        int n,                         ///< Number of variables
+        int k,                         ///< Iteration count
+        int ls                         ///< Number of evaluation for this iteration
+    );
+
+    /** \brief Optimize the objective and return its final value. */
+    virtual lbfgsfloatval_t optimize(
+        int * return_code = nullptr,
+        std::string * return_code_description = nullptr
+        );
+
+    /** \brief Compute numerical gradient and compare to analytically computed
+     * gradient.
+     *
+     * The function draws uniformely distributed random samples for all
+     * variables. It then computes the numerical gradient \e grad_n and compares
+     * it to the analytical gradient \e grad_a computed in the objective()
+     * function. If for any variable \e v the relative deviation |grad_n(v) -
+     * grad_a(v)|/min(|grad_a(v)|,|grad_n(v)|) is larger than the maximum
+     * allowed deviation it reports an error.
+     *
+     * @param number_of_samples The number of random samples to be drawn.
+     *
+     * @param range Samples are uniformely drawn from [-range,+range] for every
+     * variable (possibly shifted by the current variable values see \e
+     * use_current_values).
+     *
+     * @param delta The numerical gradient is computed by computing the finite
+     * difference of the objective for v+delta and v-delta.
+     *
+     * @param allowed_maximum_relative_deviation The maximum allowed relative
+     * deviation not considered as an error.
+     *
+     * @param use_current_values Whether sampling is centered around the current
+     * variable values (stored in lbfgs_variables) or around zero. */
+    void check_derivatives(const int& number_of_samples,
+                           const double& range,
+                           const double& delta = 1e-10,
+                           const double& allowed_maximum_relative_deviation = 1e-10,
+                           const bool use_current_values = false
+        );
+
+    /** \brief Get the number of variables that are optimized. */
+    virtual unsigned int    get_number_of_variables() const {return number_of_variables;}
+
+    /** \brief Get maximum number of iterations. */
+    virtual unsigned int    get_maximum_iterations() const {return maximum_iterations;}
+
+    /** \brief Get the factor for L1-regularization. */
+    virtual lbfgsfloatval_t get_l1_factor() const {return l1_factor;}
+
+    /** \brief Get lbfgs_epsilon. */
+    virtual lbfgsfloatval_t get_lbfgs_epsilon() const {return lbfgs_epsilon;}
+
+    /** \brief Get lbfgs_delta. */
+    virtual lbfgsfloatval_t get_lbfgs_delta() const {return lbfgs_delta;}
+
+
+protected:
+
+    lbfgsfloatval_t * lbfgs_variables     = nullptr; ///< Variable values.
+
+    /** \brief Set the number of variables that are optimized. */
+    virtual LBFGS_Optimizer& set_number_of_variables(unsigned int n, bool zero = false);
+
+    /** \brief Set the factor for L1-regularization. */
+    virtual LBFGS_Optimizer& set_l1_factor(lbfgsfloatval_t f );
+
+    /** \brief Set lbfgs_delta. */
+    virtual LBFGS_Optimizer& set_lbfgs_delta(lbfgsfloatval_t f );
+
+    /** \brief Set maximum number of iterations. */
+    virtual LBFGS_Optimizer& set_maximum_iterations(unsigned int n );
+
+    /** \brief Set lbfgs_epsilon. */
+    virtual LBFGS_Optimizer& set_lbfgs_epsilon(lbfgsfloatval_t f );
+
+private:
+
+    unsigned int      number_of_variables = 0;       ///< The dimension of the objective.
+    unsigned int      maximum_iterations  = 0;       ///< Maximum number of iteration (0 for infinite).
+    lbfgsfloatval_t   l1_factor           = 0;       ///< Factor for L^1 regularization (0 for no regularization).
+    lbfgsfloatval_t   lbfgs_delta         = 0;       ///< Minimum change of objective (f-f')/f as stopping criterion (default 0).
+    lbfgsfloatval_t   lbfgs_epsilon       = 1e-5;    ///< Minimum change of variables ||g||/max(1,||x||) as stopping criterion (default 1e-5).
+
+};
+
+#endif /* LBFGS_OPTIMIZER_H_ */
