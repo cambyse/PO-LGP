311902e4e4ce61240bfd952f7034ba644ca53239
Johannes Kulick <kulick@hildensia.de>
2013-11-08 16:05:04 +0100

    move old stuff out of ain ors files

--- share/src/Ors/ors.cpp
+++ share/src/Ors/ors.cpp
@@ -2183,6 +2183,32 @@ void ors::Graph::getTotals(ors::Vector& c, ors::Vector& v, ors::Vector& l, ors::
 
 #undef LEN
 
+double forceClosureFromProxies(ors::Graph& ORS, uint bodyIndex, double distanceThreshold, double mu, double torqueWeights) {
+  uint k;
+  ors::Vector c, cn;
+  arr C, Cn;
+  ors::Proxy *p;
+  for_list(k,p,ORS.proxies){
+    int body_a = ORS.shapes(p->a)->body?ORS.shapes(p->a)->body->index:-1;
+    int body_b = ORS.shapes(p->b)->body?ORS.shapes(p->b)->body->index:-1;
+    if(p->d<distanceThreshold && (body_a==(int)bodyIndex || body_b==(int)bodyIndex)) {
+      if(body_a==(int)bodyIndex) {
+        c = p->posA;
+        cn=-p->normal;
+      } else {
+        c = p->posB;
+        cn= p->normal;
+      }
+      C.append(ARRAY(c));
+      Cn.append(ARRAY(cn));
+    }
+  }
+  C .reshape(C.N/3, 3);
+  Cn.reshape(C.N/3, 3);
+  double fc=forceClosure(C, Cn, ORS.bodies(bodyIndex)->X.pos, mu, torqueWeights, NULL);
+  return fc;
+}
+
 //===========================================================================
 // Util
 /**
@@ -2240,8 +2266,5 @@ template ors::Shape* listFindByName(const MT::Array<ors::Shape*>&,const char*);
 
 #include <Core/array_t.h>
 template MT::Array<ors::Joint*>::Array();
-inline std::istream& operator>>(std::istream& is, TaskVariable*&) {NIY}
-inline std::ostream& operator<<(std::ostream& os, const TaskVariable*&) {NIY}
-template struct MT::Array<TaskVariable*>;
 #endif
 /** @} */
