544177cdc01e408e808b76f5ec3fec7b5926f045
Stefan Otte <stefan.otte@gmail.com>
2013-11-14 15:43:57 +0100

    Slit rosors into a parser and rosors.

--- share/projects/rosors/src/rosors/rosors.py
+++ share/projects/rosors/src/rosors/rosors.py
@@ -8,6 +8,7 @@ import rospy
 import rosors
 import srv
 import msg
+import parser
 
 import orspy
 
@@ -47,11 +48,11 @@ class RosOrs(object):
         # special body requested
         if req.name:
             ors_body = self.graph.getBodyByName(req.name)
-            res.bodies.append(self.ors_body_to_msg(ors_body))
+            res.bodies.append(parser.ors_body_to_msg(ors_body))
             return res
         # all bodies requested
         for ors_body in self.graph.bodies:
-            res.bodies.append(self.ors_body_to_msg(ors_body))
+            res.bodies.append(parser.ors_body_to_msg(ors_body))
         return res
 
     def handle_shapes_request(self, req):
@@ -66,45 +67,9 @@ class RosOrs(object):
             raise NotImplementedError("get by index_body is not implemented")
         elif req.name:
             ors_shape = self.graph.getShapeByName(req.name)
-            res.bodies.append(self.ors_shape_to_msg(ors_shape))
+            res.bodies.append(parser.ors_shape_to_msg(ors_shape))
             return res
         # all shapes requested
         for ors_shape in self.graph.shapes:
-            res.shapes.append(self.ors_shape_to_msg(ors_shape))
+            res.shapes.append(parser.ors_shape_to_msg(ors_shape))
         return res
-
-    #########################################################################
-    # Helpers for creating msgs
-    def ors_shape_to_msg(self, ors_shape):
-        shape_msg = msg.Shape()
-
-        shape_msg.index = ors_shape.index
-        shape_msg.index_body = ors_shape.ibody
-        shape_msg.name = ors_shape.name
-
-        shape_msg.X.translation = ors_shape.X.pos
-        shape_msg.X.rotation = ors_shape.X.rot
-        shape_msg.rel.translation = ors_shape.rel.pos
-        shape_msg.rel.rotation = ors_shape.rel.rot
-
-        return shape_msg
-
-    def ors_body_to_msg(self, ors_body):
-        body_msg = msg.Body()
-
-        body_msg.index = ors_body.index
-        body_msg.name = ors_body.name
-        body_msg.mass = ors_body.mass
-
-        # ors transformation
-        body_msg.pos = ors_body.X.pos
-        body_msg.rot = ors_body.X.rot
-        body_msg.vel = ors_body.X.vel
-        body_msg.angvel = ors_body.X.angvel
-
-        body_msg.com = ors_body.com
-
-        body_msg.force = ors_body.force
-        body_msg.torque = ors_body.torque
-
-        return body_msg
