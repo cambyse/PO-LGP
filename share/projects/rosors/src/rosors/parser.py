4423a183906ac142d01abd0b8e8d2195cbd8bcdb
Stefan Otte <stefan.otte@gmail.com>
2013-11-15 15:13:52 +0100

    Fill mesh msg.

--- share/projects/rosors/src/rosors/parser.py
+++ share/projects/rosors/src/rosors/parser.py
@@ -4,14 +4,18 @@ Helpers for creating ros msgs and ors datastructures.
 You can use the helper functions from whereever you want.
 """
 
+# ROS
 import roslib
 roslib.load_manifest("rosors")
-
-import msg
+import geometry_msgs
+import shape_msgs
+# MLR
+import ors_msgs
+import orspy
 
 
 def ors_shape_to_msg(ors_shape):
-    shape_msg = msg.Shape()
+    shape_msg = ors_msgs.msg.Shape()
 
     shape_msg.index = ors_shape.index
     shape_msg.index_body = ors_shape.ibody
@@ -22,11 +26,29 @@ def ors_shape_to_msg(ors_shape):
     shape_msg.rel.translation = ors_shape.rel.pos
     shape_msg.rel.rotation = ors_shape.rel.rot
 
+    if ors_shape.type == orspy.meshST:
+        # vertices
+        for i in range(ors_shape.mesh.V.shape[0]):
+            vertex = geometry_msgs.msg.Point(
+                ors_shape.mesh.V[i, 0],
+                ors_shape.mesh.V[i, 1],
+                ors_shape.mesh.V[i, 2]
+            )
+            shape_msg.mesh.vertices.append(vertex)
+        # triangles/faces
+        for i in range(ors_shape.mesh.T.shape[0]):
+            triangle = shape_msgs.msg.MeshTriangle([
+                ors_shape.mesh.T[i, 0],
+                ors_shape.mesh.T[i, 1],
+                ors_shape.mesh.T[i, 2]
+            ])
+            shape_msg.mesh.triangles.append(triangle)
+
     return shape_msg
 
 
 def ors_body_to_msg(ors_body):
-    body_msg = msg.Body()
+    body_msg = ors_msgs.msg.Body()
 
     body_msg.index = ors_body.index
     body_msg.name = ors_body.name
