140a7231f3cb6ddb4395809e2d730e3e41d63d7e
Johannes Kulick <kulick@hildensia.de>
2013-11-20 13:26:45 +0100

    add post-processing optimizer after rrt

--- share/examples/Motion/RRT-PR2/main.cpp
+++ share/examples/Motion/RRT-PR2/main.cpp
@@ -18,14 +18,14 @@ arr create_endpose(ors::Graph& G) {
   P.loadTransitionParameters();
   P.H_rate_diag = pr2_reasonable_W();
 
-  TaskCost *c = P.addTaskMap("position", new DefaultTaskMap(posTMT, G, "tip1", ors::Vector(0, 0, .0)));
+  // add a collision cost with threshold 0 to avoid collisions
+  uintA shapes = pr2_get_shapes(G);
+  TaskCost *c = P.addTaskMap("proxyColls", new ProxyTaskMap(allVersusListedPTMT, shapes, .01, true));
+  P.setInterpolatingCosts(c, MotionProblem::constant, {0.}, 1e-0);
+
+  c = P.addTaskMap("position", new DefaultTaskMap(posTMT, G, "tip1", ors::Vector(0, 0, .0)));
   P.setInterpolatingCosts(c, MotionProblem::finalOnly, ARRAY(P.ors->getBodyByName("target")->X.pos), 1e2);
   P.setInterpolatingVelCosts(c, MotionProblem::finalOnly, ARRAY(0.,0.,0.), 1e1);
-
-  //uintA shapes = ARRAY<uint>(P.ors->getBodyByName("l_gripper_l_finger_tip_link")->shapes(0)->index);
-  //c = P.addTaskMap("proxyColls",
-                   //new ProxyTaskMap(allVersusListedPTMT, shapes, .2, true));
-  //P.setInterpolatingCosts(c, MotionProblem::constant, ARRAY(0.), 1e1);
 
   arr x = P.x0;
   keyframeOptimizer(x, P, false, 2);
@@ -33,31 +33,15 @@ arr create_endpose(ors::Graph& G) {
   return x;
 }
 
-int main(int argc, char** argv) {
-  MT::initCmdLine(argc,argv);
-  int seed = time(NULL);
-
-  arr trajectory;  
-  arr start;
-  arr target;
-
-  ors::Graph G;
-
-  double stepsize; // RRT stepsize
-  double eps;      // eps environment size
-
-  rnd.seed(seed);
-  stepsize = .005;
-  eps = .001;
-  G.init("world.ors");
-  makeConvexHulls(G.shapes);
+arr create_rrt_trajectory(ors::Graph& G, arr& target) {
+  double stepsize = MT::getParameter<double>("rrt_stepsize", .005);
 
   // create MotionProblem
   MotionProblem P(&G);
   P.loadTransitionParameters();
 
   // add a collision cost with threshold 0 to avoid collisions
-  uintA shapes = ARRAY<uint>(P.ors->getBodyByName("l_gripper_l_finger_tip_link")->shapes(0)->index);
+  uintA shapes = pr2_get_shapes(G);
   TaskCost *c = P.addTaskMap("proxyColls", new ProxyTaskMap(allVersusListedPTMT, shapes, .01, true));
   P.setInterpolatingCosts(c, MotionProblem::constant, {0.}, 1e-0);
   c->y_threshold = 0;
@@ -66,24 +50,66 @@ int main(int argc, char** argv) {
   arr q = { 0.1, 0.999998, 0.500003, 0.999998, 1.5, -2, 0, 0.500003, 0, 0 };
   planner.joint_max = q + ones(q.N, 1);
   planner.joint_min = q - ones(q.N, 1);
-
   std::cout << "Planner initialized" <<std::endl;
-  start = G.getJointState();
+  
+  return planner.getTrajectoryTo(target);
+}
 
-  target = create_endpose(G);
-  std::cout << "q = " << start << std::endl;
-  std::cout << "target = " << target << std::endl;
+arr optimize_trajectory(ors::Graph& G, arr& init_trajectory) {
+  // create MotionProblem
+  MotionProblem P(&G);
+  P.loadTransitionParameters();
+  P.H_rate_diag = pr2_reasonable_W();
+  P.T = init_trajectory.d0-1;
 
-  trajectory = planner.getTrajectoryTo(target);
+  // add a collision cost with threshold 0 to avoid collisions
+  uintA shapes = pr2_get_shapes(G);
+  TaskCost *c = P.addTaskMap("proxyColls", new ProxyTaskMap(allVersusListedPTMT, shapes, .01, true));
+  P.setInterpolatingCosts(c, MotionProblem::constant, {0.}, 1e1);
 
-  //std::cout << "Trajectory:" << std::endl;
-  //for(uint i=0; i<trajectory.d0; ++i)
-    //cout << i << " : " << trajectory[i] << std::endl;
+  c = P.addTaskMap("position", new DefaultTaskMap(posTMT, G, "tip1", ors::Vector(0, 0, .0)));
+  P.setInterpolatingCosts(c, MotionProblem::finalOnly, ARRAY(P.ors->getBodyByName("target")->X.pos), 1e2);
+  P.setInterpolatingVelCosts(c, MotionProblem::finalOnly, ARRAY(0.,0.,0.), 1e2);
+
+  MotionProblemFunction MF(P);
+  arr x = init_trajectory;
+  optNewton(x, Convert(MF), OPT(verbose=2, stopIters=40, useAdaptiveDamping=false, damping=1e-0, maxStep=1.));
+  return x;
+}
+
+void show_trajectory(ors::Graph& G, OpenGL& gl, arr& trajectory, const char* title) {
+  arr start;
+  G.getJointState(start);
+  displayTrajectory(trajectory, trajectory.d0, G, gl, title);
+  gl.watch();
+  G.setJointState(start);
+}
+
+int main(int argc, char** argv) {
+  MT::initCmdLine(argc,argv);
+  int seed = time(NULL);
+
+  rnd.seed(seed);
+
+  ors::Graph G("world.ors");
+  makeConvexHulls(G.shapes);
 
   OpenGL gl;
   bindOrsToOpenGL(G, gl);
-  displayTrajectory(trajectory, trajectory.d0, G, gl, "RRT");
-  gl.watch();
+
+  arr start = G.getJointState();
+  std::cout << "q = " << start << std::endl;
+
+  arr target = create_endpose(G);
+  G.setJointState(start);
+
+  std::cout << "target = " << target << std::endl;
+
+  arr rrt_trajectory = create_rrt_trajectory(G, target);
+  show_trajectory(G, gl, rrt_trajectory, "RRT");
+
+  arr opt_trajectory = optimize_trajectory(G, rrt_trajectory);
+  show_trajectory(G, gl, opt_trajectory, "optimized");
 
   return 0;
 }
