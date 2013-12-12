c49a85d25cd9403ec4637cea1abdc66dfc4f2764
Ingo Lütkebohle <Ingo.Luetkebohle@ipvs.uni-stuttgart.de>
2013-12-12 10:59:03 +0100

    missing build files

---
+++ share/src/MDP/.sources.cmake
@@ -0,0 +1,13 @@
+set(SOURCES ./mdp_pomdpEM_structured.cpp
+./mdp_EMSolver.cpp
+./mdp_tony.cpp
+./mdp_solvers.cpp
+./mdp_pomdpEM_generic.cpp
+./mdp_pomdpEM_lev1.cpp
+./mdp_opengl.cpp
+./mdp_pomdpEM_lev2.cpp
+./mstep.cpp
+./mdp.h
+./mstep.h
+./mdp.cpp
+./mdp_EMSolver.h)
