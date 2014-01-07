27d15eac93d7f73c3878acc5dae3fdc2872793eb
Robert Lieck (Home) <science@robert-lieck.com>
2014-01-08 00:16:26 +0100

    sandbox example for name lookup with inheritance

--- usr/RL/TestMaze_II/GTest/SandBox.cpp
+++ usr/RL/TestMaze_II/GTest/SandBox.cpp
@@ -17,3 +17,42 @@ TEST(SandBox, SharedPtr) {
     DEBUG_OUT(0,"i1" << (i1==i3?"==":"!=") << "i3");
     DEBUG_OUT(0,"i3" << (i3==i2?"==":"!=") << "i2");
 }
+
+TEST(SandBox, Inheritance) {
+    class A {
+    public:
+        virtual void f(int) = 0;
+        virtual void f(int, int, int) = 0;
+    };
+    class AA {
+    public:
+        virtual void f(int, double) {
+            DEBUG_OUT(0,"AA::f(int, double)");
+        }
+    };
+    class B: public A, public AA {
+    public:
+        //========================//
+        // Needed for name lookup //
+        using AA::f;
+        //========================//
+        virtual void f(int) override {
+            f(0,0.);
+        }
+        virtual void f(int, int, int) override {
+            f(0);
+        }
+    };
+    class C: public B {
+    public:
+        virtual void f(int, double) {
+            DEBUG_OUT(0,"C::f(int, double)");
+        }
+        virtual void f(int) override {
+            f(0,0.);
+        }
+    };
+    A * a = new C();
+    a->f(0);
+    a->f(0,0,0);
+}
