edf6774d007bd9c724b5bee32c3d26222c7c52e1
Stefan Otte <stefan.otte@gmail.com>
2014-01-14 17:08:28 +0100

    Unittest StrategySelectEntropy.

---
+++ share/projects/the_curious_robot/src/the_curious_robot/test_selection_strategies.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from sas_pick_ooi import StrategySelectEntropy
+
+
+class TestEntropyMaxStrategy(object):
+    def test_max_entropy_single_entry(self):
+        strategy = StrategySelectEntropy("max")
+
+        entropies = {0: [-.2]}
+        selected = strategy.execute(oois=None, entropies=entropies)
+        assert selected == 0
+
+        entropies = {0: [-.2], 1: [-.4]}
+        selected = strategy.execute(oois=None, entropies=entropies)
+        assert selected == 0
+
+        entropies = {0: [-.2], 1: [-.1]}
+        selected = strategy.execute(oois=None, entropies=entropies)
+        assert selected == 1
+
+    def test_max_entropy_multiple_entries(self):
+        strategy = StrategySelectEntropy("max")
+
+        entropies = {0: [-.2, -3]}
+        selected = strategy.execute(oois=None, entropies=entropies)
+        assert selected == 0
+
+        entropies = {0: [-.2, -.2], 1: [-.4]}
+        selected = strategy.execute(oois=None, entropies=entropies)
+        assert selected == 0
+
+        entropies = {0: [-.2, -.2],
+                     1: [-.1]}
+        selected = strategy.execute(oois=None, entropies=entropies)
+        assert selected == 1
+
+        entropies = {0: [-.8, -.8, -.1],
+                     1: [-.2]}
+        selected = strategy.execute(oois=None, entropies=entropies)
+        assert selected == 0
