57883c029de6ebffdc2caf425bc319f0db8e3125
Ingo Lütkebohle <Ingo.Luetkebohle@ipvs.uni-stuttgart.de>
2014-01-24 14:15:47 +0100

    provide automatic cmakelists generator refs #3

--- share/bin/update-includes.sh
+++ share/bin/update-includes.sh
@@ -28,6 +28,14 @@ do
     if [ ! -z "$CPP_SOURCES" ]
     then
       echo "set(SOURCES ${CPP_SOURCES})" >> $tmpname
+    else
+      rm -f $tmpname      
+    fi
+    # if no output, remove existing definition, if any, and skip rest
+    if [ ! -f "$tmpname" ]
+    then
+      rm -f $incname
+      continue
     fi
     # only update include if content changed, to prevent unnecessary cmake calls
     if cmp $incname $tmpname >/dev/null 2>&1
