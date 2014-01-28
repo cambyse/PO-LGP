648c2126b346021055650fa0b17e874a427abf52
Stefan Otte <stefan.otte@gmail.com>
2014-01-28 18:19:26 +0100

    update.

--- share/projects/exploration_comparison/exploration.py
+++ share/projects/exploration_comparison/exploration.py
@@ -1,9 +1,12 @@
+from __future__ import division
 import random
 import scipy.stats as ss
 import numpy as np
 import copy
 import seaborn as sns
 import matplotlib.pyplot as plt
+
+arr = np.array
 
 
 class Door(object):
@@ -38,6 +41,21 @@ class DoorBel(object):
     def entropy(self):
         return ss.beta(self.opened, self.closed).entropy()
 
+    def exp_diff_H(self):
+        H = self.entropy()
+        # prob for outcome "a" and "b"
+        P_a = self.mean()
+        P_b = 1 - P_a
+        # entropy for outcome "a" and "b"
+        H_a = ss.beta.entropy(self.opened + 1, self.closed)
+        H_b = ss.beta.entropy(self.opened, self.closed + 1)
+        # new estimated entropy
+        H_est = (P_a * H_a + P_b * H_b)
+        # change of H
+        exp_diff = H - H_est
+
+        return exp_diff
+
     def update(self, opened):
         if opened:
             self.opened += 1
@@ -49,6 +67,9 @@ class DoorBel(object):
 
     def likelihood(self, loc):
         return ss.beta.pdf(loc, self.opened, self.closed)
+
+    def logpdf(self, loc):
+        return ss.beta.logpdf(loc, self.opened, self.closed)
 
 
 ###############################################################################
@@ -70,6 +91,12 @@ def init_all_openable_big():
 
 def init_some_openable_big(num=5):
     world = [Door(i % 2, "d" + str(i)) for i in range(num)]
+    belief = [DoorBel(door.name) for door in world]
+    return world, belief
+
+
+def init_stochastic_world(num=5):
+    world = [Door(1 / (i+1), "d" + str(i)) for i in range(num)]
     belief = [DoorBel(door.name) for door in world]
     return world, belief
 
@@ -110,6 +137,9 @@ def multi_run(f_init, strategy_name, select_strategy, num_interactions=100,
     entropy_obj = np.empty((runs, num_interactions, num_objects))
     entropy_total = np.zeros((runs, num_interactions))
 
+    expected_diff_H_obj = np.empty_like(entropy_obj)
+    expected_diff_H_total = np.ones_like(entropy_total)
+
     likelihood_obj = np.empty_like(entropy_obj)
     likelihood_total = np.ones_like(entropy_total)
 
@@ -127,6 +157,7 @@ def multi_run(f_init, strategy_name, select_strategy, num_interactions=100,
     squared_error_total = np.zeros_like(entropy_total)
 
     summary = {}
+    data = {}
 
     #========================================================================
     # RUN multiple times
@@ -137,74 +168,98 @@ def multi_run(f_init, strategy_name, select_strategy, num_interactions=100,
 
         #======================================================================
         # COLLECT AND SUMMARIZE DATA
-        for i in range(num_objects):
+        for obj in range(num_objects):
             # Entropy over time
-            entropy_obj[run, :, i] = np.array([b[i].entropy() for b in bel])
-            entropy_total[run, :] += entropy_obj[run, :, i]
+            entropy_obj[run, :, obj] = arr([b[obj].entropy() for b in bel])
+            entropy_total[run, :] += entropy_obj[run, :, obj]
 
+            # Expected change of entropy over time
+            expected_diff_H_obj[run, :, obj] = arr([b[obj].exp_diff_H()
+                                                    for b in bel])
+            expected_diff_H_total[run, :] += expected_diff_H_obj[run, :, obj]
             # Esimate over time
-            estimated_p_obj[run, :, i] = np.array([b[i].mean() for b in bel])
+            estimated_p_obj[run, :, obj] = arr([b[obj].mean() for b in bel])
 
             # Error over time: abs(p_true, p_estimated)
-            error_obj[run, :, i] = np.abs(
-                real_p_obj[i] - estimated_p_obj[run, :, i])
-            error_total[run, :] += error_obj[run, :, i]
+            error_obj[run, :, obj] = np.abs(real_p_obj[obj] -
+                                            estimated_p_obj[run, :, obj])
+            error_total[run, :] += error_obj[run, :, obj]
 
             # Squared error over time
-            squared_error_obj[run, :, i] = error_obj[run, :, i]**2
-            squared_error_total[run, :] += squared_error_obj[run, :, i]
+            squared_error_obj[run, :, obj] = error_obj[run, :, obj]**2
+            squared_error_total[run, :] += squared_error_obj[run, :, obj]
 
             # Likelihood over time
-            likelihood_obj[run, :, i] = np.array([b[i].likelihood(world[i].p)
-                                                  for b in bel])
-            likelihood_total[run, :] *= likelihood_obj[run, :, i]
+            likelihood_obj[run, :, obj] = arr([b[obj].likelihood(world[obj].p)
+                                               for b in bel])
+            likelihood_total[run, :] *= likelihood_obj[run, :, obj]
 
-            log_likelihood_obj[run, :, i] = np.log(likelihood_obj[run, :, i])
-            log_likelihood_total[run, :] *= log_likelihood_obj[run, :, i]
+            log_likelihood_obj[run, :, obj] = arr([b[obj].logpdf(world[obj].p)
+                                                   for b in bel])
+            log_likelihood_total[run, :] *= log_likelihood_obj[run, :, obj]
+
+    data["error"] = error_total
+    data["MSE"] = squared_error_total
+    data["entropy"] = entropy_total
+    data["expected_diff_H"] = expected_diff_H_total
+    data["likelihood"] = likelihood_total
+    data["log_likelihood"] = log_likelihood_total
 
     summary["error"] = error_total.mean()
     summary["MSE"] = squared_error_total.mean()
     summary["entropy"] = entropy_total.mean()
+    summary["expected_diff_H"] = expected_diff_H_total[:, -1].mean()
     summary["likelihood"] = likelihood_total[:, -1].mean()
     summary["log_likelihood"] = log_likelihood_total[:, -1].mean()
 
     #========================================================================
     # PLOT strategies seperately
     fig, (ax1, ax2, ax3, ax4, ax5, ax6) = plt.subplots(1, 6, figsize=(22, 4))
+    plt.suptitle(strategy_name, fontsize=14)
     colors = sns.color_palette()
-
-    # entropy over time
-    sns.tsplot(entropy_obj, condition=names, ax=ax1)
-    sns.tsplot(entropy_total, condition=["toatl"], color=colors[3], ax=ax1)
-    ax1.set_title("Entropy over time for " + strategy_name)
+    c3 = colors[3]
 
     # estimate over time
-    sns.tsplot(estimated_p_obj, condition=names, ax=ax2)
-    for i in range(num_objects):
-        sns.tsplot(real_p_obj[i], color="grey", label="real p", ax=ax2)
-    ax2.set_ylim((0, 1))
-    ax2.set_title("Estimate over time for " + strategy_name)
+    ax = ax1
+    sns.tsplot(estimated_p_obj, condition=names, ax=ax)
+    for obj in range(num_objects):
+        sns.tsplot(real_p_obj[obj], color="grey", label="real p", ax=ax)
+    ax.set_ylim((0, 1))
+    ax.set_title("Estimate over time")
 
     # error over time
-    sns.tsplot(error_obj, condition=names, ax=ax3)
-    sns.tsplot(error_total, condition=["total"], color=colors[3], ax=ax3)
-    ax3.set_title("Error over time for " + strategy_name)
+    # ax = axX
+    # sns.tsplot(error_obj, condition=names, ax=ax)
+    # sns.tsplot(error_total, condition=["total"], color=c3, ax=ax)
+    # ax.set_title("Error over time for " + strategy_name)
 
     # squared error over time
-    sns.tsplot(squared_error_obj, condition=names, ax=ax4)
-    sns.tsplot(squared_error_total, condition=["total"], color=colors[3],
-               ax=ax4)
-    ax4.set_title("Squared error over time for " + strategy_name)
+    ax = ax2
+    sns.tsplot(squared_error_obj, condition=names, ax=ax)
+    sns.tsplot(squared_error_total, condition=["total"], color=c3, ax=ax)
+    ax.set_title("Squared error over time")
+
+    # entropy over time
+    ax = ax3
+    sns.tsplot(entropy_obj, condition=names, ax=ax)
+    sns.tsplot(entropy_total, condition=["toatl"], color=c3, ax=ax)
+    ax.set_title("Entropy over time")
+
+    # expected change of entropy
+    ax = ax4
+    sns.tsplot(expected_diff_H_obj, condition=names, ax=ax)
+    ax.set_title("Expected change of Entropy")
 
     # likelihood over time
-    sns.tsplot(likelihood_obj, condition=names, ax=ax5)
-    sns.tsplot(likelihood_total, condition=["total"], color=colors[3], ax=ax5)
-    ax5.set_title("Likelihood over time for " + strategy_name)
+    ax = ax5
+    sns.tsplot(likelihood_obj, condition=names, ax=ax)
+    sns.tsplot(likelihood_total, condition=["total"], color=c3, ax=ax)
+    ax.set_title("Likelihood over time")
 
     # log likelihood over time
-    sns.tsplot(log_likelihood_obj, condition=names, ax=ax6)
-    sns.tsplot(log_likelihood_total, condition=["total"], color=colors[3],
-               ax=ax6)
-    ax6.set_title("Log-likelihood over time for " + strategy_name)
+    ax = ax6
+    sns.tsplot(log_likelihood_obj, condition=names, ax=ax)
+    sns.tsplot(log_likelihood_total, condition=["total"], color=c3, ax=ax)
+    ax.set_title("Log-likelihood over time")
 
-    return summary
+    return summary, data
