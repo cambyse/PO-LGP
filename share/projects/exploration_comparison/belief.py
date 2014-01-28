522c3d00375abe1c88ee9b52469afb2dad9a2e5b
Stefan Otte <stefan.otte@gmail.com>
2014-01-28 21:34:23 +0100

    expected entropy of multiple properties.

--- share/projects/exploration_comparison/belief.py
+++ share/projects/exploration_comparison/belief.py
@@ -38,13 +38,20 @@ class JointBel(probdist.CategoricalDist):
         super(JointBel, self).__init__({"nil": 1, "rot": 1, "pris": 1})
         self.name = name
 
-        self.rot = sp.stats.norm(loc=0, scale=1)
-        self.pris = sp.stats.norm(loc=0, scale=1)
+        self.rot_limit_min = sp.stats.norm(loc=-1, scale=2)
+        self.rot_limit_max = sp.stats.norm(loc=2, scale=2)
+        self.rot_damping = sp.stats.norm(loc=0, scale=.5)
+
+        self.pris_limit_min = sp.stats.norm(loc=-1, scale=2)
+        self.pris_limit_max = sp.stats.norm(loc=2, scale=2)
+        self.pris_damping = sp.stats.norm(loc=0, scale=.5)
+
         self.nil = sp.stats.norm(loc=0, scale=.1)
 
-        #
+        # TODO Scaling and Gaussians
         precision = 0.1
-        self.scaler = .25 / precision
+        zero_entropy_of_gaussian = .248
+        self.scaler = zero_entropy_of_gaussian / precision
 
     def H_tmp(self):
         """How do we want to calculate the change of entropy for the joint bel?
@@ -57,9 +64,6 @@ class JointBel(probdist.CategoricalDist):
         have to calc the entropy here.
 
         """
-        Ps = []
-        prior_entropy = []
-        expected_entropy = []
         expected_change = {}
 
         update_sigma = .9
@@ -68,34 +72,30 @@ class JointBel(probdist.CategoricalDist):
         for name in self:
             print("calc {}".format(name))
 
-            distribution = getattr(self, name)
-            if name == "nil":
-                H = distribution.entropy()
-            else:
-                # Simple Forward Model
-                # only the std determines the entropy for gaussians. therefore,
-                # only update the std
-                prior_std = distribution.std()
-                prior_sigma = math.sqrt(prior_std)
-                post_sigma = math.sqrt((prior_sigma * update_sigma) /
-                                       (prior_sigma + update_sigma))
-                post_sigma += noise
-                post_sigma *= self.scaler
-                H = ss.norm.entropy(0, post_sigma)
-                print("post sigma {}".format(post_sigma))
-
             P = self.prob(name)
-            Ps.append(P)
-            expected_entropy.append(H)
-            prior_entropy_scaled = ss.norm.entropy(0, post_sigma * self.scaler)
-            prior_entropy.append(prior_entropy_scaled)
-            expected_change[name] = P * (prior_entropy[-1]
-                                         - expected_entropy[-1])
 
-        # print("current_entropy", self.H())
-        print("Ps", Ps)
-        print("prior entropy", prior_entropy)
-        print("exp_entropy", expected_entropy)
+            if name == "nil":
+                H = self.nil.entropy()
+                expected_change[name] = (H, H, P)
+            else:
+                for subname in ["_limit_max", "_limit_min", "_damping"]:
+                    fullname = name + subname
+
+                    distribution = getattr(self, fullname)
+                    # Simple Forward Model
+                    # only the std determines the entropy for gaussians.
+                    # therefore, only update the std
+                    prior_std = distribution.std()
+                    prior_sigma = math.sqrt(prior_std)
+                    post_sigma = math.sqrt((prior_sigma * update_sigma) /
+                                           (prior_sigma + update_sigma))
+                    post_sigma += noise
+                    post_sigma *= self.scaler
+                    H_expected = ss.norm.entropy(0, post_sigma)
+                    print("post sigma {}".format(post_sigma))
+
+                    # TODO Scaling and Gaussians
+                    expected_change[fullname] = (H, H_expected, P)
 
         return expected_change
 
