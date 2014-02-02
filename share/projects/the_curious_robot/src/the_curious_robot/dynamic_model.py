e29fe4a24175bf96e6e9ef07e33849ebbf7579c5
Stefan Otte <stefan.otte@gmail.com>
2014-02-02 11:09:53 +0100

    renamed:    ../dynamic_model.py -> dynamic_model.py

--- share/projects/the_curious_robot/src/the_curious_robot/dynamic_model.py
+++ share/projects/the_curious_robot/src/the_curious_robot/dynamic_model.py
@@ -1,13 +1,17 @@
 # -*- coding: utf-8 -*-
+
+"""
+Learning the Dynamics of a System
+assume the dynamic system is a point mass which state is the position and
+velocity. The velocity is decreased by a damping factor.
+
+"""
 from __future__ import division
 import numpy as np
 import matplotlib.pyplot as plt
 import pymc as pm
 import math
 
-# Learning the Dynamics of a System
-# assume the dynamic system is a point mass which state is the position and 
-# velocity. The velocity is decreased by a damping factor.
 
 # Generative Model
 def point_mass_integrate(p0, v0, damping, N, dt, min_limit, max_limit):
@@ -21,22 +25,23 @@ def point_mass_integrate(p0, v0, damping, N, dt, min_limit, max_limit):
         direction = 1 if velocities[n] > 0 else -1
         if positions[n + 1] > max_limit or positions[n+1] < min_limit:
             direction = -direction
-        tmp_sqr_vel = max(velocities[n]**2 - \
-            abs(2 * damping * velocities[n] * dt), 0)
+        tmp_sqr_vel = max(velocities[n]**2 -
+                          abs(2 * damping * velocities[n] * dt), 0)
         velocities[n + 1] = direction * math.sqrt(tmp_sqr_vel)
-        
+
     return (positions, velocities)
 
+
 class DynamicModel:
-    def __init__(self, 
-                 prior_damping=pm.Uniform("damping", lower=0., upper=10.), 
-                 prior_init_pos=pm.Uniform("init_pos", lower=-1.0, upper=1.0), 
+    def __init__(self,
+                 prior_damping=pm.Uniform("damping", lower=0., upper=10.),
+                 prior_init_pos=pm.Uniform("init_pos", lower=-1.0, upper=1.0),
                  prior_init_vel=pm.Uniform("init_vel", lower=0.0, upper=2.0),
-                 prior_min_limit=pm.Uniform("min_limit", lower=-1.5, upper=1.5), 
+                 prior_min_limit=pm.Uniform("min_limit", lower=-1.5, upper=1.5),
                  prior_max_limit=pm.Uniform("max_limit", lower=-1.0, upper=2.0)):
 
         self.prior_damping = prior_damping
-        
+
         self.prior_init_pos = prior_init_pos
         self.prior_init_vel = prior_init_vel
 
@@ -45,43 +50,39 @@ class DynamicModel:
 
         # Standard deviation is modelled with a Uniform prior
         self.std_pos = pm.Uniform("std", lower=0.0, upper=.1)
- 
 
-    def add_observations(self, observations, dt, samples=10000, burnout=5000, 
-                        thinning=2):
-
+    def add_observations(self, observations, dt, samples=10000, burnout=5000,
+                         thinning=2):
         N = observations.shape[0]
+
         @pm.deterministic
-        def est_pos(damping=self.prior_damping, 
-                    init_pos=self.prior_init_pos, 
-                    init_vel=self.prior_init_vel, 
-                    min_limit=self.prior_min_limit, 
+        def est_pos(damping=self.prior_damping,
+                    init_pos=self.prior_init_pos,
+                    init_vel=self.prior_init_vel,
+                    min_limit=self.prior_min_limit,
                     max_limit=self.prior_max_limit):
-            """Estimate N positions of the point mass for given damping, 
-            init_pos, and init_vel."""
-            pos, vel = point_mass_integrate(init_pos,
-                                            init_vel,
-                                            damping.item(),
-                                            N,
-                                            dt,
-                                            min_limit,
-                                            max_limit)
+            """
+            Estimate N positions of the point mass for given damping,
+            init_pos, and init_vel.
+            """
+            pos, vel = point_mass_integrate(init_pos, init_vel, damping.item(),
+                                            N, dt, min_limit, max_limit)
             return pos
-         
+
         @pm.deterministic
         def est_pos_prec(std_pos=self.std_pos):
             """Transform std into precition"""
             return 1.0/std_pos**2
 
         # The observations are based on the estimated positions and precision
-        obs_pos = pm.Normal("obs", mu=est_pos, tau=est_pos_prec, 
+        obs_pos = pm.Normal("obs", mu=est_pos, tau=est_pos_prec,
                             value=observations, observed=True)
-        self.mcmc = pm.MCMC([self.prior_damping, 
+        self.mcmc = pm.MCMC([self.prior_damping,
                              self.prior_init_pos,
-                             self.prior_init_vel, 
-                             self.prior_min_limit, 
-                             self.prior_max_limit, 
-                             est_pos_prec, 
+                             self.prior_init_vel,
+                             self.prior_min_limit,
+                             self.prior_max_limit,
+                             est_pos_prec,
                              obs_pos])
 
         dist_map = pm.MAP(self.mcmc)
