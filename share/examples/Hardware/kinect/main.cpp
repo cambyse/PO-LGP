0b1485d7589cf9a1bace1dbe67c4bcbacc4b088f
Ingo Lütkebohle <Ingo.Luetkebohle@ipvs.uni-stuttgart.de>
2014-02-13 11:01:13 +0100

    use lib-provided depth-packer

--- share/examples/Hardware/kinect/main.cpp
+++ share/examples/Hardware/kinect/main.cpp
@@ -9,35 +9,6 @@
 void lib_hardware_kinect();
 void lib_Perception();
 
-struct KinectDepthPacking:Module{
-  ACCESS(MT::Array<uint16_t>, kinect_depth);
-  ACCESS(byteA, kinect_depthRgb);
-  void open(){}
-  void close(){}
-  void step(){
-    MT::Array<uint16_t> depth = kinect_depth.get();
-    if(depth.N>0){
-      byteA buffer(depth.N, 3);
-      buffer.setZero();
-#if 0
-      for(uint i=0;i<depth.N;i++) memmove(buffer.p+1+3*i, depth.p+i, 2);
-#else
-      uint16_t d;
-      byte *rgb;
-      for(uint i=0;i<depth.N;i++){
-        d=depth.p[i];
-        rgb=buffer.p+3*i;
-        rgb[0]=0xff&(d>>8);
-        rgb[1]=0xff&(d>>4);
-        rgb[2]=0xff&(d);
-      }
-#endif
-      buffer.reshape(depth.d0, depth.d1, 3);
-      kinect_depthRgb.set() = buffer;
-    }
-  }
-};
-
 void threadedRun() {
   lib_hardware_kinect();
   lib_Perception();
