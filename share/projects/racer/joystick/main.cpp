f34db81a84173522f67e7474320beb635f0bc044
Marc Toussaint <marc.toussaint@fu-berlin.de>
2014-02-20 18:42:32 +0100

    racer balances again

--- share/projects/racer/joystick/main.cpp
+++ share/projects/racer/joystick/main.cpp
@@ -2,71 +2,33 @@
 #include <System/engine.h>
 
 #include <Hardware/racer/modules.h>
+#include <Hardware/joystick.h>
 
-void testIMU(){
+void testJoystick(){
   struct MySystem:System{
-    ACCESS(arr, imuData);
-    ACCESS(arr, stateEstimate);
+    ACCESS(arr, joystickState);
     MySystem(){
-      addModule<IMU_Poller>("IMU_Poller", ModuleThread::loopFull);
-      addModule<KalmanFilter>("KalmanFilter", ModuleThread::listenFirst);
-      addModule<RacerDisplay>("RacerDisplay", ModuleThread::loopWithBeat, 0.1);
+      addModule<JoystickInterface>("JoystickInterface", ModuleThread::loopWithBeat, 0.01);
       connect();
     }
   } S;
 
-  cout <<S <<endl;
-
-  //    engine().enableAccessLog();
   engine().open(S);
 
-
-  for(;;){
-    S.stateEstimate.var->waitForNextWriteAccess();
-    arr x = S.stateEstimate.get();
-//    arr enc = S.encoderData.get();
-    cout <<"\r state = " <<x <<std::flush;
+  for(int i = 0;; ++i){
+    S.joystickState.var->waitForNextWriteAccess();
+    arr J = S.joystickState.get();
+    cout <<"\r joy=" <<J <<std::flush;
     if(engine().shutdown.getValue()) break;
-//    if(S.imuData.get()()(0)>10.) break;
   }
 
-  //    engine().shutdown.waitForSignal();
-
   engine().close(S);
 
   cout <<"bye bye" <<endl;
 }
 
-void testMotors(){
-  struct MySystem:System{
-    ACCESS(arr, controls)
-    MySystem(){
-      addModule<Motors>("Motors", ModuleThread::loopFull);
-      connect();
-    }
-  } S;
 
-  cout <<S <<endl;
-
-  engine().open(S);
-
-  S.controls.set()() = ARR(5.,5.,10.);
-  MT::wait(3);
-  S.controls.set()() = ARR(128.,128.,10.);
-  MT::wait(3);
-  S.controls.set()() = ARR(0.,0.,10.);
-  MT::wait(3);
-  S.controls.set()() = ARR(128.,128.,1.);
-  MT::wait(3);
-  S.controls.set()() = ARR(0.,0.,1.);
-  MT::wait(3);
-
-  engine().close(S);
-
-  cout <<"bye bye" <<endl;
-}
-
-void testBalance(){
+void run(){
   struct MySystem:System{
     ACCESS(arr, imuData)
     ACCESS(arr, stateEstimate);
@@ -81,9 +43,7 @@ void testBalance(){
     }
   } S;
 
-  cout <<S <<endl;
-
-  double zeroTh=MT::getParameter<double>("zeroTh", 0.);
+  //double zeroTh=MT::getParameter<double>("zeroTh", 0.);
   double k_th=MT::getParameter<double>("k_th", 0.);
   double k_thDot=MT::getParameter<double>("k_thDot", 0.);
   double k_acc=MT::getParameter<double>("k_acc", 0.);
@@ -119,13 +79,10 @@ void testBalance(){
 int main(int argc, char **argv) {
   MT::initCmdLine(argc, argv);
 
-//  testIMU();
-//  testMotors();
-  testBalance();
+//  testJoystick();
+
+  run();
 
   return 0;
 }
 
-
-// -k_th 400 -k_thDot 30
-//./x.exe -IMU_tilt 0.22 -IMU_gyroNoise .1 -IMU_accelNoise 1 -k_th 400 -k_thDot 30
