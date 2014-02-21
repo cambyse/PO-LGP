fef6f13d0a03a816b7eb69d598c4194a04475089
Peter Englert <englertpr@gmail.com>
2014-02-21 13:35:26 +0100

    added amex_controller_pkg

---
+++ share/projects/amex_controller_pkg/src/amex_controller.cpp
@@ -0,0 +1,14 @@
+#include <ros/ros.h>
+#include <Core/array.h>
+#include <tree_controller_pkg/SetVecTarget.h>
+
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "amex_controller");
+  ros::NodeHandle nh;
+
+  arr a;
+
+  return 0;
+}
