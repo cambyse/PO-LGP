387adda78d8b1be13fc24d942217ad810161e724
Johannes Kulick <kulick@hildensia.de>
2014-03-05 15:58:45 +0100

    use force for "controller"

--- share/examples/Ors/ors_forces/main.cpp
+++ share/examples/Ors/ors_forces/main.cpp
@@ -4,20 +4,22 @@
 
 int dir = 1;
 double GRAVITY = 9.81;
-ors::Vector oldPos(0, 0, 0);
 
-void move_robot(ors::KinematicWorld& G) {
+void move_robot(ors::KinematicWorld& G, int time) {
     ors::Body *b = G.getBodyByName("robot");
 
     double kd = 400;
     double kp = 1000;
   
     ors::Vector pos = G.getBodyByName("robot")->X.pos;
+    ors::Vector vel = G.getBodyByName("robot")->X.vel;
     double dPos = 1 - pos.z;
-    double dVel = oldPos.z - pos.z;
-    oldPos = G.getBodyByName("robot")->X.pos;
+    double dVel = 0 - vel.z;
+
+    double dyPos = 1 - pos.y;
+    double dyVel = 0 - vel.y;
     
-    G.addForce(ors::Vector(0, 10, b->mass * GRAVITY + kp * dPos + kd * dVel), b);
+    G.addForce(ors::Vector(0, kp * dyPos + kd * dyVel, b->mass * GRAVITY + kp * dPos + kd * dVel), b);
     //if (b->X.pos.y > 1.3) dir = -1;
     //b->X.pos.y += dir * .01;
 }
@@ -32,13 +34,12 @@ void TEST(OrsPhysx) {
   glPh.camera.focus(0,0,1.);
   glPh.watch();
   
-  oldPos = G.getBodyByName("robot")->X.pos;
   for(uint t=0; t<1500; t++) {
     cout <<"\r t=" <<t <<std::flush;
     G.physx().step();
     glPh.update();
     G.watch(false);
-    move_robot(G);
+    move_robot(G, t);
   }
 }
 
