215a8a45f75d207c796bd2a3385facb3424085b7
Andrea Baisero <andrea.baisero@ipvs.uni-stuttgart.de>
2014-03-07 15:51:59 +0100

    g4-segment application

--- share/projects/data_recording/.slicedef.cmake
+++ share/projects/data_recording/.slicedef.cmake
@@ -1,5 +1,6 @@
 set(CURRENT_SLICE 
   controllability
+  g4-segment
   g4-recorder
   tomsy-recorder
  )
---
+++ share/projects/data_recording/g4-segment/main.cpp
@@ -0,0 +1,35 @@
+#include <Core/array.h>
+#include <Core/util.h>
+
+#include <fstream>
+
+int main(int argc, char ** argv) {
+  MT::initCmdLine(argc, argv);
+
+  String in;
+  double tf, tt;
+  MT::getParameter(in, "in");
+  MT::getParameter(tf, "f", 0.);
+  MT::getParameter(tt, "t", -1.);
+
+  ifstream is;
+  MT::open(is, in);
+
+  uint fnum;
+  char tag[30];
+  double stamp;
+  arr data;
+  while(is.good()) {
+    is >> fnum >> tstamp >> data;
+
+    if(tstamp >= tf) {
+      if(tt > 0. && tstamp > tt)
+        break;
+      sprintf(tag, "%6i %13.6f", fnum, tstamp);
+      cout << tag << " " << data << endl;
+    }
+  }
+
+  is.close();
+}
+
