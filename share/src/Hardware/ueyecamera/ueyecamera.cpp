78f7abb08781fc330fd4050c445554109d08cc0f
Ingo Luetkebohle <Ingo.Luetkebohle@ipvs.uni-stuttgart.de>
2014-03-10 17:05:22 +0100

    add timeout parameter for image grabbing

--- share/src/Hardware/ueyecamera/ueyecamera.cpp
+++ share/src/Hardware/ueyecamera/ueyecamera.cpp
@@ -55,7 +55,7 @@ struct sUEyeInterface {
     void camSetup();
     void camInit();
     void camOpen();
-    void camGrab(byteA& image, double& timestamp);
+    bool camGrab(byteA& image, double& timestamp, unsigned int timeout=1<<31);
     void camClose();
     void camExit();
 
@@ -84,7 +84,7 @@ struct sUEyeInterface {
     void InitImageQueue_wr();
     void ExitImageQueue_wr();
 
-    void WaitForNextImage_wr();
+    bool WaitForNextImage_wr(unsigned int timeout=1<<31);
     void CaptureStatus_wr(UINT nCommand);
     void GetImageInfo_wr();
     void UnlockSeqBuf_wr(INT buffID, char *buff);
@@ -245,29 +245,35 @@ void sUEyeInterface::camOpen() {
   open_flag = true;
 }
 
-void sUEyeInterface::camGrab(byteA& image, double& timestamp) {
+bool sUEyeInterface::camGrab(byteA& image, double& timestamp, unsigned int timeout) {
   if(!setup_flag || !init_flag || !open_flag) {
     err_flag = true;
-    return;
+    tout(this) << "bad prep before grab: setup="
+    		<< setup_flag << ", init=" << init_flag << ", open=" << open_flag << endl;
+    return false;
   }
 
   img = NULL;
   imgBuffNum = 0;
-  WaitForNextImage_wr();
-  updateTimestamp();
-  // make sure receiver is large enough
-  image.resize(c_ueye_height, c_ueye_width, c_ueye_bypp);
-  memcpy(image.p, img, c_ueye_size);
-  timestamp = tstamp;
-  
-  UnlockSeqBuf_wr(imgBuffNum, img);
+  if(WaitForNextImage_wr(timeout)) {
+	  updateTimestamp();
+	  // make sure receiver is large enough
+	  image.resize(c_ueye_height, c_ueye_width, c_ueye_bypp);
+	  memcpy(image.p, img, c_ueye_size);
+	  timestamp = tstamp;
 
-  // TODO do we even want this?
-  frame_count++;
-  if(frame_count % 1000 == 0) {
-      GetFramesPerSecond_wr();
-      tout(this) << "current fps: " << live_fps << endl;
-      //module->ueye_fps.set() = live_fps;
+	  UnlockSeqBuf_wr(imgBuffNum, img);
+
+	  // TODO do we even want this?
+	  frame_count++;
+	  if(frame_count % 1000 == 0) {
+		  GetFramesPerSecond_wr();
+		  tout(this) << "current fps: " << live_fps << endl;
+		  //module->ueye_fps.set() = live_fps;
+	  }
+	  return true;
+  } else {
+	  return false;
   }
 }
 
@@ -465,19 +471,26 @@ void sUEyeInterface::ExitImageQueue_wr() {
   handleCamStatus();
 }
 
-void sUEyeInterface::WaitForNextImage_wr() {
-  camStatus = is_WaitForNextImage(camID, 1<<31, &img, &imgBuffNum);
-  switch(camStatus) {
-    case IS_CAPTURE_STATUS:
-      CaptureStatus_wr(IS_CAPTURE_STATUS_INFO_CMD_GET);
-      handleCaptStatus();
-      CaptureStatus_wr(IS_CAPTURE_STATUS_INFO_CMD_RESET);
-      WaitForNextImage_wr();
-    case IS_SUCCESS:
-      return;
-  }
+bool sUEyeInterface::WaitForNextImage_wr(unsigned int timeout) {
+	unsigned int attempt = 0;
+	while(attempt++ < 3) {
+	  camStatus = is_WaitForNextImage(camID, timeout, &img, &imgBuffNum);
+	  switch(camStatus) {
+		case IS_CAPTURE_STATUS:
+		  CaptureStatus_wr(IS_CAPTURE_STATUS_INFO_CMD_GET);
+		  handleCaptStatus();
+		  CaptureStatus_wr(IS_CAPTURE_STATUS_INFO_CMD_RESET);
+		  break;
+		case IS_TIMED_OUT:
+			tout(this) << "timeout waiting for image" << endl;
+			return false;
+		case IS_SUCCESS:
+		  return true;
+	  }
+	}
   tout(this) << "WaitForNextImage() failed" << endl;
   handleCamStatus();
+  return false;
 }
 
 void sUEyeInterface::CaptureStatus_wr(UINT nCommand) {
