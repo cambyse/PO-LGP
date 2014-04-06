35629ba41d95f3cd0adb9dd74ef1251cacc8e030
Robert Lieck (Home) <science@robert-lieck.com>
2014-04-07 01:14:31 +0200

    unfinished Commander stuff

--- usr/RL/TestMaze_II/GTest/CommanderTest.cpp
+++ usr/RL/TestMaze_II/GTest/CommanderTest.cpp
@@ -5,36 +5,120 @@
 #define DEBUG_STRING ""
 #include "../util/debug.h"
 
+
+//------------------------------------------------------------
+
+template<class ... T>
+class A {
+public:
+    void print() {
+        DEBUG_OUT(0,"A: " << sizeof...(T));
+    }
+};
+
+template<class ... FTYPE,template<class ...> class T, class ... TARG>
+void f(T<TARG...> t) {
+    //static_assert(std::is_same<FTYPE..., TARG...>::value,"Not the same type.");
+    DEBUG_OUT(0,sizeof...(FTYPE));
+    t.print();
+}
+
+//------------------------------------------------------------
+
+template<class ... A1REST,
+         class ... A2REST>
+constexpr bool all_same(A<A1REST...>, A<A2REST...>) {
+    return sizeof...(A1REST)==0 && sizeof...(A2REST)==0;
+}
+
+template<class A1First,
+         class A2First>
+constexpr bool all_same(A<A1First>, A<A2First>) {
+    return std::is_same<A1First,A2First>::value;
+}
+
+template<class A1First, class ... A1REST,
+         class A2First, class ... A2REST>
+constexpr bool all_same(A<A1First, A1REST...>, A<A2First, A2REST...>) {
+    return sizeof...(A1REST)==sizeof...(A2REST) &&
+        std::is_same<A1First,A2First>::value && all_same(A<A1REST...>(),A<A2REST...>());
+}
+
+// static_assert(all_same(A<int,double,int>(),A<int,double,int>()), "error 1"); // OK
+// static_assert(all_same(A<int,double,int>(),A<int,int>()), "error 2");        // different number
+// static_assert(all_same(A<int,double,int>(),A<int,bool,int>()), "error 3");   // different type
+
+//------------------------------------------------------------
+
+template<template<class...> class T>
+void f(T t) {
+    std::function<void(int)> ff(t);
+    ff(3);
+}
+
 TEST(Commander, TMP) {
 
-    typedef Commander::CommandList command_list_t;
-    typedef Commander::Description description_t;
+    f([](int){});
 
-    int i = 5;
+    using namespace Commander;
 
-    Commander c;
-    c.add(command_list_t("do_nothing"),
-          Commander::Arguments<>(),
-          Commander::Arguments<>(),
-          description_t("Prints \"I don't do anything!\""),
-          [](){DEBUG_OUT(0,"I don't do anything!");}
-        );
-    c.add(command_list_t("print_i"),
-          Commander::Arguments<>(),
-          Commander::Arguments<>(),
-          description_t("Prints variable i from CommanderTest body."),
-          [&](){DEBUG_OUT(0,"i = " << i);}
-        );
+    //----------------------------------//
+    // Want to be able to do this
 
-    auto com = c.print_commands();
-    DEBUG_OUT(0,"");
-    for(auto line : com) {
-        DEBUG_OUT(0,line);
-    }
-    DEBUG_OUT(0,"");
+    // int ext = 5;
+    // CommandCenter c;
+    // c.add_command(
+    //     {"first-alias","second-alias"},
+    //     [=](int i, double d = 3.3) -> ReturnType {
+    //         if(i<0) {
+    //             return {false, "First argument must be a positive integer"};
+    //         } else {
+    //             DEBUG_OUT(0,"This is a lambda closure using extern variable ext=" << ext <<
+    //                       " plus two arguments i=" << i << " and d=" << d);
+    //             return {true, "I successfully printed a message"};
+    //         }
+    //     },
+    //     "This is a description of the command"
+    //     );
+    // DEBUG_OUT(0,c.get_help());
+    // DEBUG_OUT(0,c.execute("first-alias -2 3.4")); // error message
+    // DEBUG_OUT(0,c.execute("second-alias 5")); // success
 
-    c.execute("do_nothing");
-    c.execute("print_i");
+    //----------------------------------//
+
+    //static_assert(std::is_same<bool,int>::value,"test failed");
+
+    //f<int,double>(A<int,double>());
+
+    //g(A<int,int,int>(),A<int>());
+
+    return;
+
+    // int i = 5;
+
+    // CommandCenter c;
+    // c.add(CommandList("do_nothing"),
+    //       Arguments<>(),
+    //       Arguments<>(),
+    //       Description("Prints \"I don't do anything!\""),
+    //       [](){DEBUG_OUT(0,"I don't do anything!");}
+    //     );
+    // c.add(CommandList("print_i"),
+    //       Arguments<>(),
+    //       Arguments<>(),
+    //       Description("Prints variable i from CommanderTest body."),
+    //       [&](){DEBUG_OUT(0,"i = " << i);}
+    //     );
+
+    // auto com = c.print_commands();
+    // DEBUG_OUT(0,"");
+    // for(auto line : com) {
+    //     DEBUG_OUT(0,line);
+    // }
+    // DEBUG_OUT(0,"");
+
+    // c.execute("do_nothing");
+    // c.execute("print_i");
 
     // auto func1 = [] () { DEBUG_OUT(0,"empty lambda"); };
     // auto func2 = [=] () { DEBUG_OUT(0,"print i: " << i); };
--- usr/RL/TestMaze_II/util/Commander.h
+++ usr/RL/TestMaze_II/util/Commander.h
@@ -14,10 +14,7 @@
 
 #include "debug.h"
 
-/** \brief This is an evil class: it uses variadic templates and lambda closures. */
-class Commander {
-    //----typedefs/classes----//
-public:
+namespace Commander {
 
     /** \brief Represents a list of command aliases. */
     class CommandList {
@@ -56,124 +53,132 @@ public:
         }
     };
 
+    /** \brief Type representing alternatives. */
+    /* class Or {}; */
+
     /** \brief Abstract base class for arguments. */
-    class AbstractArguments {
-    public:
-        class Or {};
-        virtual QString get_string() const = 0;
-    };
+    /* class AbstractArguments { */
+    /* public: */
+    /*     virtual QString get_string() const = 0; */
+    /* }; */
 
     /** \brief Represent a variable number of arguments. */
-    template<class ... Args>
-        class Arguments: public AbstractArguments {
-        //----typedefs/classes----//
-    public:
-        typedef std::function<void(Args...)> function_t;
-    private:
-        class NoType {};
-        //----members----//
-    private:
-        std::vector<std::type_index> type_descriptors;
-        //----methods----//
-    public:
-        Arguments() {
-            if(sizeof...(Args)>0) {
-                type_descriptors = get_type_descriptors<Args...>();
-            }
-        }
-        virtual ~Arguments() = default;
-        virtual QString get_string() const override {
-            QString ret;
-            bool first = true;
-            for(auto t : type_descriptors) {
-                if(first) {
-                    first = false;
-                } else {
-                    ret += " ";
-                }
-                if(t==std::type_index(typeid(int))) {
-                    ret += "<int>";
-                } else if(t==std::type_index(typeid(double))) {
-                    ret += "<double>";
-                } else if(t==std::type_index(typeid(float))) {
-                    ret += "<float>";
-                } else if(t==std::type_index(typeid(bool))) {
-                    ret += "<bool>";
-                } else if(t==std::type_index(typeid(QString))) {
-                    ret += "<QString>";
-                } else if(t==std::type_index(typeid(Or))) {
-                    ret += "|";
-                } else {
-                    ret += "<unknown>";
-                }
-            }
-            return ret;
-        }
-    private:
-        template<typename First = NoType>
-            std::vector<std::type_index> get_type_descriptors()
-        {
-            if(typeid(First)==typeid(NoType)) {
-                return {};
-            } else {
-                return {std::type_index(typeid(First))};
-            }
-        }
+    /* template<class ... Args> */
+    /*     class Arguments: public AbstractArguments { */
+    /*     //----typedefs/classes----// */
+    /* public: */
+    /*     typedef std::function<void(Args...)> function_t; */
+    /* private: */
+    /*     class NoType {}; */
+    /*     //----members----// */
+    /* private: */
+    /*     std::vector<std::type_index> type_descriptors; */
+    /*     //----methods----// */
+    /* public: */
+    /*     Arguments() { */
+    /*         if(sizeof...(Args)>0) { */
+    /*             type_descriptors = get_type_descriptors<Args...>(); */
+    /*         } */
+    /*     } */
+    /*     virtual ~Arguments() = default; */
+    /*     virtual QString get_string() const override { */
+    /*         QString ret; */
+    /*         bool first = true; */
+    /*         for(auto t : type_descriptors) { */
+    /*             if(first) { */
+    /*                 first = false; */
+    /*             } else { */
+    /*                 ret += " "; */
+    /*             } */
+    /*             if(t==std::type_index(typeid(int))) { */
+    /*                 ret += "<int>"; */
+    /*             } else if(t==std::type_index(typeid(double))) { */
+    /*                 ret += "<double>"; */
+    /*             } else if(t==std::type_index(typeid(float))) { */
+    /*                 ret += "<float>"; */
+    /*             } else if(t==std::type_index(typeid(bool))) { */
+    /*                 ret += "<bool>"; */
+    /*             } else if(t==std::type_index(typeid(QString))) { */
+    /*                 ret += "<QString>"; */
+    /*             } else if(t==std::type_index(typeid(Or))) { */
+    /*                 ret += "|"; */
+    /*             } else { */
+    /*                 ret += "<unknown>"; */
+    /*             } */
+    /*         } */
+    /*         return ret; */
+    /*     } */
+    /* private: */
+    /*     template<typename First = NoType> */
+    /*         std::vector<std::type_index> get_type_descriptors() */
+    /*         { */
+    /*             if(typeid(First)==typeid(NoType)) { */
+    /*                 return {}; */
+    /*             } else { */
+    /*                 return {std::type_index(typeid(First))}; */
+    /*             } */
+    /*         } */
 
-        template<typename First, typename Second, typename ... More>
-            std::vector<std::type_index> get_type_descriptors()
-        {
-            auto v1 = get_type_descriptors<First>();
-            auto v2 = get_type_descriptors<Second,More...>();
-            v1.insert(v1.end(),v2.begin(),v2.end());
-            return v1;
-        }
-    };
+    /*     template<typename First, typename Second, typename ... More> */
+    /*         std::vector<std::type_index> get_type_descriptors() */
+    /*     { */
+    /*         auto v1 = get_type_descriptors<First>(); */
+    /*         auto v2 = get_type_descriptors<Second,More...>(); */
+    /*         v1.insert(v1.end(),v2.begin(),v2.end()); */
+    /*         return v1; */
+    /*     } */
+    /* }; */
 
     /** \brief Represents a description of a command. */
     class Description: public QString {
     public:
-    template<class T>
-        Description(const T& t): QString(t) {}
+        template<class T>
+            Description(const T& t): QString(t) {}
         virtual ~Description() = default;
         QString get_string() const {
             return *this;
         }
     };
 
-    //----members----//
-    std::vector<std::tuple<
-        CommandList,
-        std::shared_ptr<const AbstractArguments>,
-        std::shared_ptr<const AbstractArguments>,
-        Description,
-        std::function<void()>
-        > > commands;
+    typedef std::pair<bool,QString> ReturnType;
 
-    //----methods----//
-public:
-    Commander() = default;
-    virtual ~Commander() = default;
-    template<class MandatArgs , class OptionArgs, class Func>
-        void add(const CommandList& com,
-                 const MandatArgs& man,
-                 const OptionArgs& opt,
-                 const Description& des,
-                 const Func& func) {
-        static_assert(1==1,"unequal");
-        typename MandatArgs::function_t f = func;
-        commands.push_back(
-            std::make_tuple(com,
-                            std::shared_ptr<const AbstractArguments>(new MandatArgs(man)),
-                            std::shared_ptr<const AbstractArguments>(new OptionArgs(opt)),
-                            des,
-                            std::function<void()>(func))
-            );
-    }
-    std::vector<QString> print_commands(int space = 4) const;
-    QString add_space(int from, int to) const;
-    void execute(QString command_name) const;
-};
+    template<class ... Args>
+        class Function : public std::function<ReturnType(Args...)> {};
+
+    /** \brief This is an evil class: it uses variadic templates and lambda closures. */
+    class CommandCenter {
+        //----typedefs/classes----//
+    public:
+
+        //----members----//
+        std::vector<std::tuple<
+            CommandList,
+            //std::shared_ptr<const AbstractArguments>,
+            //std::shared_ptr<const AbstractArguments>,
+            std::function<void()>,
+            Description
+            > > commands;
+
+        //----methods----//
+    public:
+        CommandCenter() = default;
+        virtual ~CommandCenter() = default;
+        template<template<class...> class Func, class ... Args>
+            void add_command(//const CommandList& com,
+                const Func<Args...>& func//,
+                             /* const Description& des */) {
+            /* commands.push_back( */
+            /*     std::make_tuple(com, */
+            /*                     std::function<ReturnType(Args...)>(func), */
+            /*                     des) */
+            /*     ); */
+        }
+        /* std::vector<QString> print_commands(int space = 4) const; */
+        /* QString add_space(int from, int to) const; */
+        /* void execute(QString command_name) const; */
+    };
+
+}
 
 #include "debug_exclude.h"
 
