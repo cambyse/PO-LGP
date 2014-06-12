36140b0e73c645e2e64d860e647501334acf6d6c
Ingo Luetkebohle <Ingo.Luetkebohle@ipvs.uni-stuttgart.de>
2014-06-12 16:12:02 +0200

    catch exceptions

--- share/examples/Hardware/flycap/main.cpp
+++ share/examples/Hardware/flycap/main.cpp
@@ -10,17 +10,20 @@ using namespace MLR;
 
 int main(int argc, char* argv[])
 {
-	OpenGL gl;
-	FlycapInterface flycap(0);
-	flycap.startStreaming();
+	try {
+		OpenGL gl;
+		FlycapInterface flycap(0);
+		flycap.startStreaming();
 	
-	byteA rgb;
-	while(true) {
-		double timestamp;
-		flycap.grab(rgb, timestamp);
-		gl.update();
+		byteA rgb;
+		while(true) {
+			double timestamp;
+			flycap.grab(rgb, timestamp);
+			gl.update();
+		}
+	} catch(const std::exception& ex) {
+		cerr << ex.what() << endl;
 	}
-
 
 	return 0;
 }
