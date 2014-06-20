b7643a4a43fcd40ff9d54dd8e068f8739c3c58ee
Ingo Lütkebohle <Ingo.Luetkebohle@ipvs.uni-stuttgart.de>
2014-06-20 10:07:31 +0200

    add graphview.cpp back

--- share/src/Gui/.sources.cmake
+++ share/src/Gui/.sources.cmake
@@ -1,1 +1,1 @@
-set(SOURCES ./gtk.cpp ./opengl.cpp ./mesh_lewiner.cpp ./geo_qhull.cpp ./plot.h ./graphview.h ./mesh.h ./opengl.h  ./gtk.h ./plot.cpp ./mesh.cpp ./color.h ./opengl_qt.h ./ors_opengl.cpp)
+set(SOURCES ./gtk.cpp ./opengl.cpp ./mesh_lewiner.cpp ./geo_qhull.cpp ./plot.h ./graphview.h ./mesh.h ./opengl.h  ./gtk.h ./plot.cpp ./mesh.cpp ./color.h ./opengl_qt.h ./ors_opengl.cpp graphview.cpp)
