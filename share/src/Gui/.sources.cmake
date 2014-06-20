216b73e185887eb45fccb45759d876f1db5579ef
Ingo Lütkebohle <Ingo.Luetkebohle@ipvs.uni-stuttgart.de>
2014-06-20 11:42:38 +0200

    fix name of test executables in test

--- share/src/Gui/.sources.cmake
+++ share/src/Gui/.sources.cmake
@@ -1,1 +1,3 @@
-set(SOURCES ./gtk.cpp ./opengl.cpp ./mesh_lewiner.cpp ./geo_qhull.cpp ./plot.h ./graphview.h ./mesh.h ./opengl.h  ./gtk.h ./plot.cpp ./mesh.cpp ./color.h ./opengl_qt.h ./ors_opengl.cpp graphview.cpp)
+#set(SOURCES ./gtk.cpp ./opengl.cpp ./mesh_lewiner.cpp ./geo_qhull.cpp ./plot.h ./graphview.h ./mesh.h ./opengl.h  ./gtk.h ./plot.cpp ./mesh.cpp ./color.h ./opengl_qt.h ./ors_opengl.cpp graphview.cpp)
+file(GLOB SOURCES *.cpp)
+
