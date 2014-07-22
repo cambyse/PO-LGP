32069222ae0e938522e7d9a90da8bf8a8591f75d
Dmitry <dmitry.zarubin@fu-berlin.de>
2014-07-22 18:59:00 +0200

    extract planes

--- share/projects/pcl_sprint/plane.cpp
+++ share/projects/pcl_sprint/plane.cpp
@@ -1,5 +1,22 @@
 #include "plane.h"
 
+void extractPlanes(pcl::PointCloud<PoinT2>::Ptr inCloud, pcl::PointCloud<PoinT2>::Ptr outCloud, uint nplanes)
+{
+  pcl::PointCloud<pcl::Normal>::Ptr normal_cloud (new pcl::PointCloud<pcl::Normal>);
+  normalEstimator(inCloud,normal_cloud,50);
+
+  pcl::ModelCoefficients::Ptr coefficients_plane (new pcl::ModelCoefficients);
+  for (uint i;i< nplanes; i++)
+  {
+  pcl::PointIndices::Ptr inliers_plane (new pcl::PointIndices);
+  planeDetector(inCloud,normal_cloud,coefficients_plane,inliers_plane);
+
+  pcl::PointCloud<PointT>::Ptr cloud_substracted_plane(new pcl::PointCloud<PointT>);
+  substractPlane(inCloud,inliers_plane,cloud_substracted_plane);
+  }
+
+}
+
 void passthroughFilter(pcl::PointCloud<PoinT2>::Ptr inCloud, pcl::PointCloud<PoinT2>::Ptr outCloud, double minLimit, double maxLimit)
 {
   pcl::PassThrough<PoinT2> pass;
