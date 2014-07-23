2b2e43457bbda4d35f73fb15b7dfedabdf2f0b94
Peter Englert <englertpr@gmail.com>
2014-07-23 11:06:18 +0200

    changed pointcloud type to consistent value and added object detector functions

--- share/projects/pcl_sprint/plane.h
+++ share/projects/pcl_sprint/plane.h
@@ -11,15 +11,16 @@
 #include <pcl/sample_consensus/method_types.h>
 #include <pcl/sample_consensus/model_types.h>
 #include <pcl/segmentation/sac_segmentation.h>
+#include "dataStructures.h"
 
-typedef pcl::PointXYZRGB PoinT2;
+//typedef pcl::PointXYZRGB PointT;
 
 
-void passthroughFilter(pcl::PointCloud<PoinT2>::Ptr inCloud,pcl::PointCloud<PoinT2>::Ptr outCloud,double minLimit, double maxLimit);
+void passthroughFilter(pcl::PointCloud<PointT>::Ptr inCloud,pcl::PointCloud<PointT>::Ptr outCloud,double minLimit, double maxLimit);
 
-void normalEstimator(pcl::PointCloud<PoinT2>::Ptr inCloud,pcl::PointCloud<pcl::Normal>::Ptr outCloud,int knn);
+void normalEstimator(pcl::PointCloud<PointT>::Ptr inCloud,pcl::PointCloud<pcl::Normal>::Ptr outCloud,int knn);
 
-void planeDetector(pcl::PointCloud<PoinT2>::Ptr inCloud,pcl::PointCloud<pcl::Normal>::Ptr inCloudNormal, pcl::ModelCoefficients::Ptr outCoefficients, pcl::PointIndices::Ptr outInliersPlane);
+void planeDetector(pcl::PointCloud<PointT>::Ptr inCloud,pcl::PointCloud<pcl::Normal>::Ptr inCloudNormal, pcl::ModelCoefficients::Ptr outCoefficients, pcl::PointIndices::Ptr outInliersPlane);
 
-void substractPlane(pcl::PointCloud<PoinT2>::Ptr inCloud,pcl::PointIndices::Ptr inInliersPlane, pcl::PointCloud<PoinT2>::Ptr outCloud);
+void substractPlane(pcl::PointCloud<PointT>::Ptr inCloud,pcl::PointIndices::Ptr inInliersPlane, pcl::PointCloud<PointT>::Ptr outCloud);
 
