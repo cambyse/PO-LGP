cambyse/PO-LGP

commit 66f1e86ac34b3b33888891dc77ae4754428ee963

adapt to snuff

usr/RL/TestMaze_II/Learner/TemporallyExtendedFeatureLearner.cpp
@@ -152,7 +152,7 @@ double TEFL::optimize_weights_LBFGS() {
     }
 
     // set weights
-    weights = values;
+    weights = arma::conv_to<col_vec_t>::from(values);
 
     // return objective value
     return obj_value;

usr/RL/TestMaze_II/Learner/TemporallyExtendedLinearQ.cpp
@@ -495,7 +495,7 @@ int TELQ::LBFGS_progress(const lbfgsfloatval_t */*x*/,
 }
 
 void TELQ::LBFGS_final_message(double obj_val) const {
-    double xnorm = arma::sum(arma::abs(weights));
+    double xnorm = arma::as_scalar(arma::sum(arma::abs(weights)));
     IF_DEBUG(1) { cout <<
             QString("    TD-error + L1 = %1 + %2")
             .arg(obj_val-xnorm*l1_factor)

usr/RL/TestMaze_II/BatchWorker_quiet/BatchWorker_quiet.pro
@@ -118,6 +118,7 @@ SOURCES += \
 LIBS += -llbfgs \
     -lemon \
     -larmadillo \
+    -llapack \
     -lgomp
 INCLUDEPATH +=
 LIBPATH += /usr/share/lib

usr/RL/TestMaze_II/BatchWorker_verbose/BatchWorker_verbose.pro
@@ -117,6 +117,7 @@ SOURCES += \
 LIBS += -llbfgs \
     -lemon \
     -larmadillo \
+    -llapack \
     -lgomp
 INCLUDEPATH +=
 LIBPATH += /usr/share/lib

usr/RL/TestMaze_II/Learner/TemporallyExtendedFeatureLearner.h
@@ -19,7 +19,7 @@ class TemporallyExtendedFeatureLearner: public HistoryObserver, public virtual S
     //----typedefs/classes----//
 public:
     DISAMBIGUATE_CONFIG_TYPEDEFS(HistoryObserver);
-    typedef arma::SpMat<double> f_mat_t; // using double for compatibility to vec_t
+    typedef arma::Mat<double> f_mat_t; // using double for compatibility to vec_t
     typedef arma::Col<double> col_vec_t;
     typedef arma::Row<double> row_vec_t;
     typedef std::map<f_ptr_t,double> weight_map_t;

usr/RL/TestMaze_II/MazeGUI/MazeGUI.pro
@@ -68,7 +68,8 @@ HEADERS += \
     ../Learner/ConjunctiveAdjacency.h \
     \
     ../BatchMaze.h \
-    ../testmaze_ii.h
+    ../testmaze_ii.h \
+    ../ui_testmaze_ii.h
 SOURCES += \
     main.cpp \
     \

commit ec86846345a67c91aa21887dc3de433b4d5349dc

fix initilaization bug introduced in commit 66f1e86ac34b3b33888891dc77ae4754428ee963 by switching from SpMat type to Mat

usr/RL/TestMaze_II/Learner/TemporallyExtendedFeatureLearner.cpp
@@ -25,6 +25,8 @@ using std::dynamic_pointer_cast;
 using std::cout;
 using std::endl;
 
+using arma::zeros;
+
 typedef TemporallyExtendedFeatureLearner TEFL;
 
 TEFL::TemporallyExtendedFeatureLearner(std::shared_ptr<ConjunctiveAdjacency> N):
@@ -565,7 +567,7 @@ void TEFL::update_F_matrices() {
 
     #warning need to clear for correct dimensions -- why? --> write minimal example
     F_matrices.clear();
-    F_matrices.resize(data_n,f_mat_t(feature_n,outcome_n));
+    F_matrices.resize(data_n,zeros<f_mat_t>(feature_n,outcome_n));
 
     // for all data points
 #ifdef USE_OMP

usr/RL/TestMaze_II/Learner/TemporallyExtendedLinearQ.cpp
@@ -61,7 +61,7 @@ TELQ::row_vec_t TELQ::get_action_values(const_instance_ptr_t ins) const {
         return zeros<row_vec_t>(action_n);
     }
     // get feature matrix
-    f_mat_t feature_matrix(feature_set.size(),action_n);
+    f_mat_t feature_matrix = zeros<f_mat_t>(feature_set.size(),action_n);
     int feature_idx = 0;
     for(f_ptr_t feature : feature_set) {
         int action_idx = 0;

usr/RL/TestMaze_II/Learner/TemporallyExtendedModel.cpp
@@ -26,6 +26,8 @@ using std::map;
 using std::make_tuple;
 using std::dynamic_pointer_cast;
 
+using arma::zeros;
+
 typedef TemporallyExtendedModel TEM;
 
 TEM::TemporallyExtendedModel(std::shared_ptr<ConjunctiveAdjacency> N):
@@ -43,8 +45,7 @@ TEM::probability_t TEM::get_prediction(const_instance_ptr_t ins,
                                        const reward_ptr_t& reward) const {
     int outcome_idx = 0;
     int matching_outcome_idx = -1;
-    f_mat_t F_matrix;
-    F_matrix.zeros(feature_set.size(),observation_space->space_size()*reward_space->space_size());
+    f_mat_t F_matrix = zeros<f_mat_t>(feature_set.size(),observation_space->space_size()*reward_space->space_size());
     for(observation_ptr_t obs : observation_space) {
         for(reward_ptr_t rew : reward_space) {
             int feature_idx = 0;
@@ -73,8 +74,7 @@ TEM::probability_t TEM::get_prediction(const_instance_ptr_t ins,
 TEM::probability_map_t TEM::get_prediction_map(const_instance_ptr_t ins,
                                                const action_ptr_t& action) const {
     // compute feature matrix
-    f_mat_t F_matrix;
-    F_matrix.zeros(feature_set.size(),observation_space->space_size()*reward_space->space_size());
+    f_mat_t F_matrix = zeros<f_mat_t>(feature_set.size(),observation_space->space_size()*reward_space->space_size());
     {
         int outcome_idx = 0;
         for(observation_ptr_t obs : observation_space) {