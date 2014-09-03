a5274e25f8d78dffe9922fcc370265a35fdd666b
bigblindbais <andrea.baisero@gmail.com>
2014-09-03 16:19:55 +0200

    better display for g4-recorder

--- share/projects/data_recording/g4-recorder/main.cpp
+++ share/projects/data_recording/g4-recorder/main.cpp
@@ -5,17 +5,18 @@
 #include <Hardware/G4/module_G4Display.h>
 #include <Core/module_FloatA_Recorder.h>
 #include <Hardware/G4/module_G4Recorder.h>
+#include <Hardware/G4/module_G4Printer.h>
 
 struct G4System:System {
   ACCESS(floatA, poses);
   G4System(){
     addModule<G4Poller>("POLLER", Module_Thread::loopWithBeat, .001);//8333); // 120Hz
     addModule<G4Display>("DISPLAY", Module_Thread::loopWithBeat, .033); // 30Hz
-    addModule<G4Recorder>("RECORDER", Module_Thread::listenFirst, .1);
+    // addModule<G4Recorder>("RECORDER", Module_Thread::listenFirst, .1);
+    // addModule<G4Printer>("PRINTER", Module_Thread::loopWithBeat, 1);
     connect();
   }
 };
-
 
 void threadedRun(){
   G4System S;
