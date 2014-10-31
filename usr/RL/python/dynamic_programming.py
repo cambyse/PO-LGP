56a24076b6217ed5cb5e313f358aae2eb0fc3505
Robert Lieck (@ThinkPet) <science@robert-lieck.com>
2014-10-31 17:12:12 +0100

    Maze

--- usr/RL/python/dynamic_programming.py
+++ usr/RL/python/dynamic_programming.py
@@ -1,15 +1,12 @@
 from decorator import decorator
 
-def _dynamic_programming(f, *args, **kwargs):
-    try:
-        f.cache[args]
-    except KeyError:
-        f.cache[args] = f(*args, **kwargs)
-    return f.cache[args]
-
 def dynamic_programming(f):
+    def dp(f, *args, **kwargs):
+        try:
+            f.cache[args]
+        except KeyError:
+            f.cache[args] = f(*args, **kwargs)
+        return f.cache[args]
     f.cache = {}
-    def ec():
-        f.cache.clear()
-    f.empty_cache = ec
-    return decorator(_dynamic_programming, f)
+    f.clear = f.cache.clear
+    return decorator(dp, f)
