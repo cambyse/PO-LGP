f79b1ee6523e27510ad94064e8d4c1bc48a78d4e
Robert Lieck (Home) <science@robert-lieck.com>
2015-03-12 14:22:46 +0100

    add comments and also include top-level

--- usr/RL/src/doxygen_generate.sh
+++ usr/RL/src/doxygen_generate.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
+## get list as arguments or use all subfolders and ./
 if [ $# -gt 0 ]; then
     list=$1
 else
     list=./*
+    list="$list ."
 fi
 
+## print list and ask for OK
 echo "Executing doxygen in:"
 for f in $list; do
     echo "    $f"
 done
-
-
 read -p "OK? (Y/n)" ok
+
+## exit if not OK or proceed...
 if [ "$ok" = "n" -o "$ok" = "N" ]; then
     exit
 else
