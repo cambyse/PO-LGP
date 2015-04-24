f304a038a22c116b4fa05579bc18994eaf017a0e
Stefan Otte <stefan.otte@gmail.com>
2015-04-24 13:08:52 +0200

    Turn class into a macro

--- share/examples/pr2/generic_ros_sync/main.cpp
+++ share/examples/pr2/generic_ros_sync/main.cpp
@@ -5,72 +5,46 @@
 #include <std_msgs/String.h>
 
 //===========================================================================
-// #define BEGIN_ROSMODULE(name, topic, msg_type) \
-//   struct ROS_#name : Module { \
-//     ros::NodeHandle _nh; \
-//     ros::Subscriber _sub; \
-//     \
-//     virtual void open() { \
-//       rosCheckInit(); \
-//       this->_sub  = s->nh.subscribe(#TOPIC, 1, &RosSub_#TOPIC::callback, this); \
-//     }; \
-//     virtual void step() {}; \
-//     virtual void close() { \
-//       this->_nh.shutdown(); \
-//     }; \
-//     void callback(const #type::ConstPtr& msg) { \
-//       this->#TOPIC.set() = msg; \
-//     } \
-//     TODO add ACCESS
+#define ROSSUB(topic_name, msg_type, var_name) \
+  class ROSSUB_##var_name : public Module { \
+  private: \
+    ros::NodeHandle* _nh; \
+    ros::Subscriber _sub; \
+    \
+  public: \
+    ACCESS(msg_type, var_name) \
+    ROSSUB_##var_name() : Module(#var_name) {} \
+    void open() { \
+      rosCheckInit(); \
+      this->_nh = new ros::NodeHandle; \
+      this->_sub  = this->_nh->subscribe( \
+        topic_name, 1, &ROSSUB_##var_name::callback, this); \
+    } \
+    void step() {} \
+    void close() { \
+      this->_nh->shutdown(); \
+      delete _nh; \
+    } \
+    void callback(const msg_type::ConstPtr& msg) { \
+      this->var_name.set() = *msg; \
+    } \
+  };
 
+//===========================================================================
+ROSSUB("/hello_world", std_msgs::String, hello_world);
+// --> ROSSUB_hello_world
 
-// #define END_MODULE() };
-
-const char* topic_name{"tmp_test"};
-
-class ROSSub_XXX : public Module {
-public:
-  ROSSub_XXX() : Module(topic_name) { cout << "ctor" <<endl; }
-
-  ACCESS(std_msgs::String, data)
-  // struct __XXX__Access:Access_typed<std_msgs::String> {
-  //   __XXX__Access():Access_typed<std_msgs::String>(){}
-  // } _data;
-
-  void open() {
-    cout << "in open" << endl;
-    rosCheckInit();
-    this->_nh = new ros::NodeHandle;
-    this->_sub  = this->_nh->subscribe(
-        topic_name, 1,
-        &ROSSub_XXX::callback, this);
-  }
-  void step() { cout << "step" << endl; }
-  void close() {
-    this->_nh->shutdown();
-  }
-  void callback(const std_msgs::String::ConstPtr& msg) {
-    cout << *msg << endl;
-    // this->_data.set() = msg;
-  }
-
-private:
-  ros::NodeHandle* _nh;
-  ros::Subscriber _sub;
-};
 
 // =================================================================================================
 struct MySystem:System {
   // Access Variables
-  // ACCESS(CtrlMsg, ctrl_ref);
-  ACCESS(CtrlMsg, ctrl_obs)
+  ACCESS(std_msgs::String, hello_world)
 
   MySystem() {
     if (MT::getParameter<bool>("useRos", true)){
       cout << "using ROS" << endl;
       addModule<RosCom_Spinner>(NULL, Module::loopWithBeat, .001);
-      // addModule<RosCom_ControllerSync>(NULL, Module::listenFirst);
-      addModule<ROSSub_XXX>(NULL, Module::listenFirst, .1);
+      addModule<ROSSUB_hello_world>(NULL, Module::listenFirst, .1);
     }
     connect();
   }
@@ -83,11 +57,12 @@ int main(int argc, char** argv){
 
   MySystem system;
   engine().open(system);
-  cout << "open " << endl;
+  cout << "Engine open " << endl;
 
   for (int i = 0; true; i++) {
-    MT::wait(0.1);
-  cout << "in loop" << endl;
+    system.hello_world.waitForNextRevision();
+    std_msgs::String d = system.hello_world.get();
+    cout << d << endl;
   }
 
   engine().close(system);
