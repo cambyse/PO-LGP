3b1e189c820c1b1492acda4842a4628651ac2eea
Stefan Otte <stefan.otte@gmail.com>
2015-04-28 11:26:49 +0200

    Extract "ROSSUB macro" and alvar into their own headers

--- share/src/pr2/roscom.h
+++ share/src/pr2/roscom.h
@@ -5,7 +5,6 @@
 #include <Core/array.h>
 #include <Core/geo.h>
 #include <Ors/ors.h>
-#include <ar_track_alvar_msgs/AlvarMarkers.h>
 
 
 //===========================================================================
@@ -15,48 +14,6 @@
 
 bool rosOk();
 void rosCheckInit();
-
-//===========================================================================
-/** MACRO to create a MLR module that integrates data from ROS into the MLR
- *  module system.
- *
- *  This macro creates a class "ROS_#var_name" that subscribes the given
- *  topic_name with the given msg_type.  The ros msg is basically copied into
- *  the MLR module system.
- *
- *  You have to add the created class and the ACCESS variable to your System
- *  and you're good to go.
- *
- *  See ../../examples/pr2/generic_ros_sync/main.cpp for more.
- *
- *  TODO Refactor to avoid includes in header files. But we need the type of
- *  the msg. Is it even possible?
- */
-#define ROSSUB(topic_name, msg_type, var_name) \
-  class ROSSUB_##var_name : public Module { \
-  public: \
-    ACCESS(msg_type, var_name) \
-    ROSSUB_##var_name() : Module(#var_name) {} \
-    void open() { \
-      rosCheckInit(); \
-      this->_nh = new ros::NodeHandle; \
-      this->_sub  = this->_nh->subscribe( \
-        topic_name, 1, &ROSSUB_##var_name::callback, this); \
-    } \
-    void step() {} \
-    void close() { \
-      this->_nh->shutdown(); \
-      delete _nh; \
-    } \
-    void callback(const msg_type::ConstPtr& msg) { \
-      this->var_name.set() = *msg; \
-    } \
-  private: \
-    ros::NodeHandle* _nh; \
-    ros::Subscriber _sub; \
-    \
-  };
-
 
 //===========================================================================
 //
@@ -162,9 +119,5 @@ BEGIN_MODULE(RosCom_ForceSensorSync)
   ACCESS(arr, wrenchL)
   ACCESS(arr, wrenchR)
 END_MODULE()
-
-//===========================================================================
-/// Sync the AR maker alvar
-ROSSUB("/ar_pose_marker", ar_track_alvar_msgs::AlvarMarkers, ar_pose_marker)
 
 //===========================================================================
