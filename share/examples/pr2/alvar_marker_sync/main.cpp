b325f295aebcd1967db4e62f9890edac7e99fa40
Stefan Otte <stefan.otte@gmail.com>
2015-04-28 11:01:03 +0200

    Properly sync alvar with ors

--- share/examples/pr2/alvar_marker_sync/main.cpp
+++ share/examples/pr2/alvar_marker_sync/main.cpp
@@ -3,8 +3,6 @@
 #include <Gui/opengl.h>
 #include <pr2/roscom.h>
 
-using namespace ar_track_alvar_msgs;
-
 // =================================================================================================
 struct MySystem:System {
   // Access Variables
@@ -22,45 +20,53 @@ struct MySystem:System {
 
 
 // =================================================================================================
-void update_marker(ors::Body& body, ar_track_alvar_msgs::AlvarMarker& marker) {
-// TODO transform: torso_lift_link is the reference frame_id
+void setBody(ors::Body& body, ar_track_alvar_msgs::AlvarMarker& marker) {
   body.X.pos.x = marker.pose.pose.position.x;
   body.X.pos.y = marker.pose.pose.position.y;
-  body.X.pos.z = marker.pose.pose.position.z + 0.75;
+  body.X.pos.z = marker.pose.pose.position.z;
   body.X.rot.w = marker.pose.pose.orientation.w;
   body.X.rot.x = marker.pose.pose.orientation.x;
   body.X.rot.y = marker.pose.pose.orientation.y;
   body.X.rot.z = marker.pose.pose.orientation.z;
 }
 
+/**
+ * Sync all markers from the msg with the ors world.
+ *
+ * Note: this never deletes old markers.
+ */
 void syncMarkers(ors::KinematicWorld& world, ar_track_alvar_msgs::AlvarMarkers& markers) {
-  for (AlvarMarker& marker : markers.markers) {
-    cout << marker.id << " " << marker.pose.pose.position << endl;
+  // transform: torso_lift_link is the reference frame_id
+  ors::Vector refFrame = world.getBodyByName("torso_lift_link")->X.pos;
 
+  for (ar_track_alvar_msgs::AlvarMarker& marker : markers.markers) {
     MT::String marker_name = STRING("marker" << marker.id);
     ors::Body *body = world.getBodyByName(marker_name);
     if (not body) {
+      cout << marker_name << " does not exist yet; adding it..." << endl;
+
       body = new ors::Body(world);
       body->name = marker_name;
       ors::Shape *shape = new ors::Shape(world, *body);
       shape->type = ors::boxST;
       shape->size[0] = .1; shape->size[1] = .1; shape->size[2] = .03; shape->size[3] = .1;
     }
-    update_marker(*body, marker);
+    setBody(*body, marker);
+    // transform: torso_lift_link is the reference frame_id
+    body->X.pos += refFrame;  // TODO is this the proper way to do it?
   }
-  cout << "=============" << endl;
 }
+
 
 // =================================================================================================
 int main(int argc, char** argv){
   MT::initCmdLine(argc, argv);
 
-  MySystem system;
-  engine().open(system);
+  bool useRos = true;
 
   ors::KinematicWorld world("model.kvg");
-
-  bool useRos = true;
+  MySystem system;
+  engine().open(system);
 
   initialSyncJointStateWithROS(world, system.ctrl_obs, useRos);
   for (int i = 0; true; i++) {
@@ -68,6 +74,9 @@ int main(int argc, char** argv){
 
     ar_track_alvar_msgs::AlvarMarkers markers = system.ar_pose_marker.get();
     syncMarkers(world, markers);
+
+    // world.calc_fwdPropagateFrames();
+    // world.calc_fwdPropagateShapeFrames();
 
     world.gl().update(STRING("frame " << i), false, false, false);
     MT::wait(0.01);
