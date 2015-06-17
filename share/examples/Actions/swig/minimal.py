7db359862111db564c084842a7908a8111fea0df
Stefan Otte <stefan.otte@gmail.com>
2015-06-17 09:23:52 +0200

    easy acces to shapes, bodies, and joints

--- share/examples/Actions/swig/minimal.py
+++ share/examples/Actions/swig/minimal.py
@@ -1,4 +1,5 @@
 from __future__ import print_function
+from collections import namedtuple
 import swig
 
 
@@ -9,22 +10,26 @@ import swig
 # s.waitForCondition("(conv GripperOpen)")
 # s.setFact("(conv GripperOpen)!")
 
-s = swig.ActionSwigInterface(1)
-print(dir(s))
+interface = swig.ActionSwigInterface(1)
+print(dir(interface))
+
+
+LEFT = 256
+RIGHT = 255
 
 
 ###############################################################################
 # Helper functions
 def shapes(name=None):
-    return s.getShapeByName(name) if name else s.getShapeList()
+    return interface.getShapeByName(name) if name else interface.getShapeList()
 
 
 def joints(name=None):
-    return s.getJointByName(name) if name else s.getJointList()
+    return interface.getJointByName(name) if name else interface.getJointList()
 
 
 def bodies(name=None):
-    return s.getBodyByName(name) if name else s.getBodyList()
+    return interface.getBodyByName(name) if name else interface.getBodyList()
 
 
 def facts():
@@ -32,11 +37,25 @@ def facts():
 
 
 ###############################################################################
-LEFT = 0
-RIGHT = 1
+# Convenient access and autocompletion to shapes, joints, and bodies
+# Just type `s.<tab>` to get a list of all shapes
+Shapes = namedtuple("Shapes", " ".join(shapes()))
+s = Shapes(*shapes())
 
 
+Bodies = namedtuple("Bodies", " ".join(bodies()))
+b = Bodies(*bodies())
+
+
+Joints = namedtuple("Joints", " ".join(joints()))
+j = Joints(*joints())
+
+
+###############################################################################
 def _run(fact):
+    """Run the given fact, wait for its completion and remove the fact
+    afterwards.
+    """
     symbols = fact[: fact.find(")")] + ")"
     symbols_conv = '(conv ' + symbols[1:]
 
