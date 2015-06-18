38c5b862f208b33d247aea20fd048229a5ccb288
Stefan Otte <stefan.otte@gmail.com>
2015-06-18 16:56:50 +0200

    Use "coordinate frames" for alvar markers.

--- share/src/pr2/rosalvar.cpp
+++ share/src/pr2/rosalvar.cpp
@@ -38,8 +38,8 @@ void syncMarkers(ors::KinematicWorld& world, AlvarMarkers& markers) {
       body->name = marker_name;
       ors::Shape *shape = new ors::Shape(world, *body);
       shape->name = marker_name;
-      shape->type = ors::boxST;
-      shape->size[0] = .1; shape->size[1] = .1; shape->size[2] = .03; shape->size[3] = .1;
+      shape->type = ors::markerST;
+      shape->size[0] = .3; shape->size[1] = .0; shape->size[2] = .0; shape->size[3] = .0;
     }
     setBody(*body, marker);
     // transform: torso_lift_link is the reference frame_id
