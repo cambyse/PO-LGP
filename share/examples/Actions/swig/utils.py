8e7c3f5ebc5c6490301fd9ea0f2b7f3c62ed054d
Stefan Otte <stefan.otte@gmail.com>
2015-06-24 19:18:31 +0200

    Simplify and unittest side selection

--- share/examples/Actions/swig/utils.py
+++ share/examples/Actions/swig/utils.py
@@ -1,15 +1,12 @@
 import numpy as np
 
+
 ###############################################################################
 # Sides
-LEFT = 256
-RIGHT = 255
-
-DEFAULT_SIDE = LEFT
-
-print("=" * 70)
-print("DEFAULT_SIDE is set to {}".format("LEFT"))
-print("  You can always change it: `DEFAULT_SIDE = RIGHT`")
+class SIDE:
+    LEFT = "left"
+    RIGHT = "right"
+    DEFAULT = "left"
 
 
 ###############################################################################
@@ -22,33 +19,71 @@ def assert_valid_shapes(name, shapes):
 ###############################################################################
 # Helper: symbols...
 def strip_specs(fact):
+    """Remove the specifications/parameters from the given symbol and return
+    it.
+
+    >>> fact = "(fact1 fact2){ value=1 pos=[2, 2, 2]}"
+    >>> strip_specs(fact)
+    '(fact1 fact2)'
+
+    """
     return fact[: fact.find(")")] + ")"
 
 
 def conv_symbol(symbol):
+    """
+    Return 'converged' symbol of the given symbol by prepending `conv`.
+
+    >>> conv_symbol("(action)")
+    '(conv action)'
+
+    """
     return '(conv ' + symbol[1:]
 
 
 def pos_str2arr(pos_str):
+    """
+    Turn string pos into a numpy array.
+
+    >>> pos_str2arr("[(3 4 5)]")
+    array([ 3.,  4.,  5.])
+
+    """
     return np.array([float(i) for i in pos_str[2:-2].split(" ")])
 
 
 def side2endeff(side=None):
-    if side is None:
-        side = DEFAULT_SIDE
-    eff = {LEFT: "endeffL", RIGHT: "endeffR"}
-    return eff[side]
+    """
+    Use the SIDE.DEFAULT if side is None.
+
+    >>> SIDE.DEFAULT = SIDE.RIGHT
+    >>> side2endeff()
+    'endeffR'
+    """
+    source = {SIDE.LEFT: "endeffL", SIDE.RIGHT: "endeffR"}
+    return source.get(side, source[SIDE.DEFAULT])
 
 
 def side2gripper_joint(side=None):
-    if side is None:
-        side = DEFAULT_SIDE
-    gripper_joint = {LEFT: "l_gripper_joint", RIGHT: "r_gripper_joint"}
-    return gripper_joint[side]
+    """
+    Use the SIDE.DEFAULT if side is None.
+
+    >>> SIDE.DEFAULT = SIDE.RIGHT
+    >>> side2gripper_joint()
+    'r_gripper_joint'
+    """
+    source = {SIDE.LEFT: "l_gripper_joint", SIDE.RIGHT: "r_gripper_joint"}
+    return source.get(side, source[SIDE.DEFAULT])
 
 
 def side2wrist_joint(side=None):
-    if side is None:
-        side = DEFAULT_SIDE
-    wrist_joint = {LEFT: "l_wrist_roll_joint", RIGHT: "r_wrist_roll_joint"}
-    return wrist_joint[side]
+    """
+    Use the SIDE.DEFAULT if side is None.
+
+    >>> SIDE.DEFAULT = SIDE.RIGHT
+    >>> side2wrist_joint()
+    'r_wrist_roll_joint'
+    """
+    source = {SIDE.LEFT: "l_wrist_roll_joint",
+              SIDE.RIGHT: "r_wrist_roll_joint"}
+    return source.get(side, source[SIDE.DEFAULT])
