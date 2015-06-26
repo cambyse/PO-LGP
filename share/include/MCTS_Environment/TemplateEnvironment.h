15c119977c7d0615cd6c182e8d72df06cbed035b
Robert Lieck (Home) <science@robert-lieck.com>
2015-06-26 02:27:18 +0200

    template for environments with integer actions, observations, and states

--- share/include/MCTS_Environment/TemplateEnvironment.h
+++ share/include/MCTS_Environment/TemplateEnvironment.h
@@ -36,4 +36,47 @@ public:
     virtual bool is_markov() const override;
 };
 
+class IntegerEnvironment: public AbstractEnvironment {
+public:
+    struct IntegerAction: public Action {
+        IntegerAction(int action): action(action) {}
+        virtual ~IntegerAction() = default;
+        virtual bool operator==(const Action & other) const override {
+            auto integer_action = dynamic_cast<const IntegerAction*>(&other);
+            return integer_action!=nullptr && integer_action->action==action;
+        }
+        virtual size_t get_hash() const override {
+            return std::hash<int>()(action);
+        }
+        virtual void write(std::ostream & out) const override {
+            out << action;
+        }
+        int action;
+    };
+    struct IntegerObservation: public Observation {
+        IntegerObservation(int observation): observation(observation) {}
+        virtual ~IntegerObservation() = default;
+        virtual bool operator==(const Observation & other) const override {
+            auto integer_observation = dynamic_cast<const IntegerObservation*>(&other);
+            return integer_observation!=nullptr && integer_observation->observation==observation;
+        }
+        virtual size_t get_hash() const override {
+            return std::hash<int>()(observation);
+        }
+        virtual void write(std::ostream & out) const override {
+            out << observation;
+        }
+        int observation;
+    };
+public:
+    IntegerEnvironment() = default;
+    virtual ~IntegerEnvironment() = default;
+    virtual void make_current_state_default() override final {default_state = state;}
+    virtual void reset_state() override final {state = default_state;}
+    virtual bool is_markov() const override {return true;}
+protected:
+    int state = 0;
+    int default_state = 0;
+};
+
 #endif /* TEMPLATEENVIRONMENT_H_ */
