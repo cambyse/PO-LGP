f7abe0f586c33f93459cc6a0512762739e2519c7
Johannes Kulick <kulick@hildensia.de>
2015-07-01 13:45:07 +0200

    sphinx docu

--- share/examples/Actions/swig/actions.py
+++ share/examples/Actions/swig/actions.py
@@ -75,23 +75,10 @@ def _run_with(with_construct):
 
 def run(plan):
     """
-    Runs a plan in the following format.
+    Runs a plan in :ref:`section-plan-format`
 
-    A plan is a lightwight datastructure to store sequential and simultaneous
-    activities. A single Activity is already a plan. To create more complex
-    plans, the following construxts are possible:
 
-    * A list of activities or plans is run sequentially. Each one must be
-      converged for the next one to start
-    * A tuple of activities or plans is run simultaneously. Everything is
-      flatten, i.e. (a, [b, c], d) is equally treated as (a, b, c, d). All
-      activities have to be converged for the tuple to be considered converged
-    * A dict with two entries. First "with" contains a list of activities,
-      second "plan" contains a plan. The list of activities in the "with" list
-      are run simultaneous to the plan. However, when the last activity of the
-      plan is converged they are stopped regardless of the convergence status.
-
-    :param plan:
+    :param plan: A plan in the plan format
     :return:
     """
     if not isinstance(plan, list):
