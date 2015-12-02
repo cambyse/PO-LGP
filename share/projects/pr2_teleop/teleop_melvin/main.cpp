bc67d84f79e5c23ac084951b600c7bf03be20f96
Marc Toussaint <marc.toussaint@uni-stuttgart.de>
2015-12-02 18:49:41 +0100

    auto

--- share/projects/pr2_teleop/teleop_melvin/main.cpp
+++ share/projects/pr2_teleop/teleop_melvin/main.cpp
@@ -1,61 +1,38 @@
-//#include <pr2/actionMachine.h>
-//#include <pr2/actions.h>
-#include <Motion/feedbackControl.h>
-#include <Hardware/gamepad/gamepad.h>
 #include <Hardware/G4/G4.h>
-#include <Hardware/G4/module_G4Publisher.h>
-#include <Hardware/G4/module_G4Recorder.h>
 #include <Hardware/G4/module_G4Display.h>
-// #include <Hardware/G4/module_G4Debugger.h>
-
-#include "g4mapper/calibrator_module.h"
-#include "g4taprecon/taprecon.h"
-#include "pd_executor/pd_executor_module.h"
-
 #include <Actions/teleopControl.h>
 #include <Actions/TaskControllerModule.h>
-
 #include <pr2/roscom.h>
 #include <Perception/perception.h>
+#include <Hardware/gamepad/gamepad.h>
+#include "calibrator_module.h"
 
 // ============================================================================
 
-struct PR2G4Control {
-  GamepadInterface gpi;
-  G4Poller g4poller;
-  G4HutoRoMap hutoro;
-//  PDExecutor exec;
-
-//  RosCom_Spinner spinner;
-  TaskControllerModule tcm;
-  TeleopControlActivity teleop;
-  OrsViewer orsviewer;
-
-//  GamepadInterface gamepad;
-//  GamepadControlActivity gpc;
-//  SubscriberConvNoHeader<marc_controller_pkg::JointState, CtrlMsg, &conv_JointState2CtrlMsg> sub_ctrl_obs("/marc_rt_controller/jointState", ctrl_obs);
-//  PublisherConv<marc_controller_pkg::JointState, CtrlMsg, &conv_CtrlMsg2JointState>          pub_ctrl_ref("/marc_rt_controller/jointReference", ctrl_ref);
-//  SubscriberConv<geometry_msgs::PoseWithCovarianceStamped, arr, &conv_pose2transXYPhi>       sub_pr2_odom("/robot_pose_ekf/odom_combined", pr2_odom);
-
-
-  PR2G4Control() {
-
-    // ROS
-    if(mlr::getParameter<bool>("useRos", false)) {
-      //new RosSpinner;
-      //addModule<RosCom_Spinner>(NULL, .001);
-      //addModule<RosCom_ControllerSync>(NULL, .001);
-    }
-    //connect();
-  };
-};
-
-
-// ============================================================================
 int main(int argc, char** argv) {
   mlr::initCmdLine(argc, argv);
 
-  PR2G4Control system;
+  rosCheckInit("teleopNode");
+
+  GamepadInterface gpi;
+  G4Poller g4poller;
+  G4HutoRoMap hutoro;
+
+  TaskControllerModule tcm;
+  TeleopControlActivity teleop;
+  OrsViewer orsviewer;
+  G4Display g4disp;
+
+  ACCESSname(CtrlMsg, ctrl_ref)
+  ACCESSname(CtrlMsg, ctrl_obs)
+  ACCESSname(arr, pr2_odom)
+  if(mlr::getParameter<bool>("useRos", false)){
+    new RosCom_Spinner;
+    new SubscriberConvNoHeader<marc_controller_pkg::JointState, CtrlMsg, &conv_JointState2CtrlMsg> ("/marc_rt_controller/jointState", ctrl_obs);
+    new PublisherConv<marc_controller_pkg::JointState, CtrlMsg, &conv_CtrlMsg2JointState>          ("/marc_rt_controller/jointReference", ctrl_ref);
+    new SubscriberConv<geometry_msgs::PoseWithCovarianceStamped, arr, &conv_pose2transXYPhi>       ("/robot_pose_ekf/odom_combined", pr2_odom);
+  }
+
   threadOpenModules(true);
 
   moduleShutdown().waitForValueGreaterThan(0);
--- share/projects/pr2_teleop/teleop_melvin/vimstuff/.vim/filetype.vim
+++
@@ -1,9 +0,0 @@
-augroup filetypedetect
-  au BufNewFile,BufRead *.tex               setf tex
-augroup END
-augroup filetypedetect
-  au BufNewFile,BufRead *.tjp,*.tji               setf tjp
-augroup END
-augroup filetype
-  au! BufRead,BufNewFile *.dis set filetype=mixed
-augroup END
