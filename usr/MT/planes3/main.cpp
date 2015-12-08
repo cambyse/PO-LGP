288b1a9ab533790de4babca4b7bf4a20486e8774
Marc Toussaint <marc.toussaint@uni-stuttgart.de>
2015-12-08 14:42:26 +0100

    quaternion general derivatives... ;-(

---
+++ usr/MT/planes3/main.cpp
@@ -0,0 +1,20 @@
+#include <Perception/plane.h>
+
+void testGradients(){
+  uint ndata = 100;
+  arr n = randn(3);
+  arr m = randn(3);
+  arr X = randn(ndata,3);
+  arr transform = randn(7);
+  transform.subRange(3,6)() /= length(transform.sub(3,6));
+
+  CostFct_PlanePoints f(n, m, X, transform);
+
+  checkGradient(f.f_transform(), transform, 1e-4);
+
+}
+
+int main(int argc,char **argv){
+  rnd.clockSeed();
+  testGradients();
+}
