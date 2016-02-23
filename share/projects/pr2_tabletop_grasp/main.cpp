09cc4727a603500fe81c9c4212084f5306a3a138
bigblindbais <andrea.baisero@gmail.com>
2016-02-23 15:07:09 +0100

    point-cloud integrated and grasping is correct

--- share/projects/pr2_tabletop_grasp/main.cpp
+++ share/projects/pr2_tabletop_grasp/main.cpp
@@ -120,7 +120,7 @@ struct PR2Grasp {
   ros::Subscriber cluster_sub;
   ros::Publisher cluster_pub;
   ros::Publisher obj_id_pub;
-  ros::ServiceServer eyespy_grasp_service;
+  ros::ServiceServer grasp_service;
 
   bool service_trigger;
 
@@ -140,7 +140,9 @@ struct PR2Grasp {
   void eyespy_cluster_callback(const visualization_msgs::MarkerArray &msg);
   // void eyespy_grasp_callback(const object_recognition_msgs::TableArrayConstPtr &msg_ta, const obj_id_pkg::MarkerArrayConstPtr &msg_ma, const obj_id_pkg::ObjIdConstPtr &msg_oid);
   void eyespy_grasp_callback(const obj_id_pkg::MarkerArrayConstPtr &msg_ma, const obj_id_pkg::ObjIdConstPtr &msg_oid);
-  bool eyespy_grasp_service_callback(std_srvs::Empty::Request &request, std_srvs::Empty::Response &response);
+  bool eyespy_grasp_service(std_srvs::Empty::Request &request, std_srvs::Empty::Response &response);
+
+  void graspObject(ors::Shape *object);
 
   void run(void);
 };
@@ -150,7 +152,7 @@ PR2Grasp::PR2Grasp(ros::NodeHandle &nh_): nh(nh_), service_trigger(false), world
   cluster_pub = nh.advertise<obj_id_pkg::MarkerArray>("/eyespy/clusters", 10);
   obj_id_pub = nh.advertise<obj_id_pkg::ObjId>("/eyespy/obj_id", 10);
 
-  eyespy_grasp_service = nh.advertiseService("/eyespy/grasp", &PR2Grasp::eyespy_grasp_service_callback, this);
+  grasp_service = nh.advertiseService("/eyespy/grasp", &PR2Grasp::eyespy_grasp_service, this);
 
   // table_array_sub = new message_filters::Subscriber<object_recognition_msgs::TableArray>(nh, "/table_array", 10);
   eyespy_cluster_sub = new message_filters::Subscriber<obj_id_pkg::MarkerArray>(nh, "/eyespy/clusters", 10);
@@ -160,7 +162,7 @@ PR2Grasp::PR2Grasp(ros::NodeHandle &nh_): nh(nh_), service_trigger(false), world
   // eyespy_sync->registerCallback(boost::bind(&PR2Grasp::eyespy_grasp_callback, this, _1, _2, _3));
   eyespy_sync->registerCallback(boost::bind(&PR2Grasp::eyespy_grasp_callback, this, _1, _2));
 
-  ti = new TrajectoryInterface(world,world_pr2);
+  ti = new TrajectoryInterface(world, world_pr2);
 }
 
 void PR2Grasp::eyespy_cluster_callback(const visualization_msgs::MarkerArray &msg) {
@@ -174,7 +176,7 @@ void PR2Grasp::eyespy_cluster_callback(const visualization_msgs::MarkerArray &ms
     service_trigger = false;
     obj_id_pkg::ObjId oid;
     oid.header = obj_id_ma.header;
-    oid.obj_id = 1;
+    oid.obj_id = 0;
     obj_id_pub.publish(oid);
   }
 }
@@ -204,6 +206,8 @@ void PR2Grasp::eyespy_grasp_callback(const obj_id_pkg::MarkerArrayConstPtr &msg_
     {1, 1, 1},
   };
 
+  ors::Shape *object=nullptr;
+
   uint nobj=0;
   uint nmarkers = msg_ma->markers.size();
   for(uint marki = 0; marki < nmarkers; marki++) {
@@ -213,13 +217,13 @@ void PR2Grasp::eyespy_grasp_callback(const obj_id_pkg::MarkerArrayConstPtr &msg_
       auto &p = msg_ma->markers[marki].points[i];
       points[i] = ARR(p.x, p.y, p.z, 1);
     }
-    points = (points * ~ti->world_pr2->getShapeByName("endeffKinect_real")->X.getAffineMatrix()).cols(0, 3);
-    // cout << "points.shape: " << points.d0 << " " << points.d1 << endl;
-    // cout << "points.z.min: " << points.col(2).min() << endl;
+    points = (points * ~ti->world_plan->getShapeByName("endeffKinect_real")->X.getAffineMatrix()).cols(0, 3);
 
     double zmin = points.col(2).min();
-    if(zmin > .5) {
-      ors::Shape *pcShape = new ors::Shape(*ti->world_pr2,NoBody);
+    // if(true) {
+    // if(zmin > .5) {
+    if(zmin > .25) {
+      ors::Shape *pcShape = new ors::Shape(*ti->world_plan, NoBody);
       pcShape->type = ors::pointCloudST;
       pcShape->name = STRING("pcShape_"<<nobj);
 
@@ -228,38 +232,83 @@ void PR2Grasp::eyespy_grasp_callback(const obj_id_pkg::MarkerArrayConstPtr &msg_
       pcShape->color[2] = colors[nobj][2];
       pcShape->mesh.V = points;
       pcShape->mesh.computeNormals();
+
+      arr center = sum(points,0)/double(points.d0);
+      center.flatten();
+      center(2) = (points.col(2).max() + points.col(2).min()) / 2.;
+
+      arr Y,v,W;
+      pca(Y,v,W,points);
+      arr dir = W.col(0); dir.flatten();
+      dir(2) = 0;
+
+      ors::Body *boxBody = new ors::Body(*ti->world_plan);
+      boxBody->name = STRING("boxBody_"<<nobj);
+      boxBody->type = ors::BodyType::dynamicBT;
+      boxBody->X.pos = center;
+      boxBody->X.rot.setDiff(ors::Vector(1.,0.,0.),dir);
+
+      ors::Shape *boxShape = new ors::Shape(*ti->world_plan, *boxBody);
+      boxShape->type = ors::boxST;
+      boxShape->name = STRING("boxShape_"<<nobj);
+      arr size = ARRAY(Y.col(0).max()-Y.col(0).min(),Y.col(1).max()-Y.col(1).min(),points.col(2).max()-points.col(2).min(), 0.);
+      memmove(boxShape->size, size.p, 4*sizeof(double));
+      arr color = ARRAY(0.1,0.5,0.1);
+      memmove(boxShape->color, color.p, 3*sizeof(double));
+
+      if(nobj == msg_oid->obj_id)
+        object = boxShape;
       nobj++;
     }
   }
+  ti->world_plan->calc_fwdPropagateFrames();
+  ti->world_plan->watch(true);
 
-  ti->world_pr2->calc_fwdPropagateFrames();
-  ti->world_pr2->watch(true);
-
-  // /// fit box to pointcloud
-  // arr center = sum(points,0)/double(points.d0);
-  // center.flatten();
-
-  // arr Y,v,W;
-  // pca(Y,v,W,points);
-  // arr dir = W.col(0); dir.flatten();
-
-  // ors::Body *boxBody = new ors::Body(world);
-  // boxBody->name = "boxBody";
-  // boxBody->type = ors::BodyType::dynamicBT;
-  // boxBody->X.pos = center;
-  // boxBody->X.rot.setDiff(ors::Vector(1.,0.,0.),dir);
-  // ors::Shape *boxShape = new ors::Shape(world,*boxBody);
-  // boxShape->type = ors::boxST;
-  // boxShape->name = "boxShape";
-  // arr size = ARRAY(Y.col(0).max()-Y.col(0).min(),Y.col(1).max()-Y.col(1).min(),Y.col(2).max()-Y.col(2).min(), 0.);
-  // memmove(boxShape->size, size.p, 4*sizeof(double));
-  // arr color = ARRAY(0.1,0.5,0.1);
-  // memmove(boxShape->color, color.p, 3*sizeof(double));
-  // world.calc_fwdPropagateFrames();
-  // world.watch(true);
+  graspObject(object);
 }
 
-bool PR2Grasp::eyespy_grasp_service_callback(std_srvs::Empty::Request &request, std_srvs::Empty::Response &response) {
+void PR2Grasp::graspObject(ors::Shape *object) {
+  if(object) {
+    arr X;
+    MotionProblem MP(*ti->world_plan);
+    Task *t;
+    t = MP.addTask("transitions", new TransitionTaskMap(*ti->world_plan));
+    t->map.order=2; //make this an acceleration task!
+    t->setCostSpecs(0, MP.T, {0.}, 1e-1);
+
+    t = MP.addTask("pos1", new DefaultTaskMap(posTMT, *ti->world_plan, "endeffL", NoVector, object->name,ors::Vector(0.,0.,0.1)));
+    t->setCostSpecs(70, 80, {0.}, 1e2);
+    t = MP.addTask("rot1", new DefaultTaskMap(vecAlignTMT, *ti->world_plan, "endeffL", ors::Vector(1.,0.,0.), "base_link_0",ors::Vector(0.,0.,-1.)));
+    t->setCostSpecs(70, MP.T, {1.}, 1e1);
+    t = MP.addTask("rot2", new DefaultTaskMap(vecAlignTMT, *ti->world_plan, "endeffL", ors::Vector(0.,0.,1.), object->name,ors::Vector(1.,0.,0.)));
+    t->setCostSpecs(70, MP.T, {1.}, 1e1);
+    t = MP.addTask("pos2", new DefaultTaskMap(posTMT, *ti->world_plan, "endeffL", NoVector, object->name,ors::Vector(0.,0.,0.)));
+    t->setCostSpecs(MP.T-5, MP.T, {0.}, 1e2);
+    t = MP.addTask("limit", new LimitsConstraint());
+    t->setCostSpecs(0, MP.T, ARR(0.), 1e2);
+    // ShapeL shaps = {
+    //   ti->world_plan->getShapeByName("l_forearm_roll_link_0"), ti->world_plan->getShapeByName("table"),
+    //   ti->world_plan->getShapeByName("l_elbow_flex_link_0"), ti->world_plan->getShapeByName("table")
+    // };
+    // t = MP.addTask("collision", new ProxyConstraint(pairsPTMT, shapesToShapeIndices(shaps), 0.1));
+    // t->setCostSpecs(0., MP.T, {0.}, 1.);
+
+    MotionProblemFunction MPF(MP);
+    X = MP.getInitialization();
+
+    optConstrained(X, NoArr, Convert(MPF), OPT(verbose=2, stopIters=100, maxStep=1., stepInc=2., aulaMuInc=2.,stopTolerance = 1e-2));
+
+    MP.costReport(true);
+    for (;;)
+      displayTrajectory(X, 1, *ti->world_plan, "planned trajectory");
+
+    ti->gotoPositionPlan(X[0]);
+    ti->executeTrajectoryPlan(X,10.,true,true);
+    ti->~TrajectoryInterface();
+  }
+}
+
+bool PR2Grasp::eyespy_grasp_service(std_srvs::Empty::Request &request, std_srvs::Empty::Response &response) {
   cout << "grasp triggered manually" << endl;
   service_trigger = true;
   return true;
@@ -324,11 +373,10 @@ int main(int argc, char** argv){
   // graspBox();
   // return 0;
 
-//  ros::init(argc, argv, "pr2_tabletop_grasp");
-
-//  ros::NodeHandle nh;
-//  PR2Grasp pr2grasp(nh);
-//  pr2grasp.run();
+  // ros::init(argc, argv, "pr2_tabletop_grasp");
+  // ros::NodeHandle nh;
+  // PR2Grasp pr2grasp(nh);
+  // pr2grasp.run();
 
   // testPointCloud();
    graspBox();
