dac5a1b72fa5540bacf749eb337cb2edbba7177d
Marc Toussaint <marc.toussaint@uni-stuttgart.de>
2016-03-07 17:12:47 +0100

    make compilable on pr2

--- share/src/Ors/orsviewer.h
+++ share/src/Ors/orsviewer.h
@@ -57,14 +57,15 @@ struct OrsPathViewer : Module{
 //===========================================================================
 
 struct ComputeCameraView:Module{
-  ACCESSlisten(ors::KinematicWorld, modelWorld)
+  Access_typed<ors::KinematicWorld> modelWorld;
   Access_typed<byteA> cameraView;
   OpenGL gl;
   uint skipFrames, frame;
   ComputeCameraView(uint skipFrames=0)
-    : Module("OrsViewer"),
-      cameraView(this, "cameraView"),
-      skipFrames(skipFrames), frame(0){}
+    : Module("OrsViewer")
+    , modelWorld(this, "modelWorld", true)
+    , cameraView(this, "cameraView")
+    , skipFrames(skipFrames), frame(0){}
   void open();
   void step();
   void close() {}
