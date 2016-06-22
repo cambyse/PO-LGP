da685a8f2afb5ff2a0b2b6dafd3868b9626affbd
maiersn <maiersn@studi.informatik.uni-stuttgart.de>
2016-06-22 18:18:55 +0200

    alvar_marker that updates a dict EXTREMELY often; will be fixed later by Heiko.

--- share/teaching/RoboticsPractical/42-ballThrowing/alvar_marker_test.py
+++ share/teaching/RoboticsPractical/42-ballThrowing/alvar_marker_test.py
@@ -2,9 +2,13 @@
 import rospy
 import baxter_interface
 from visualization_msgs.msg import Marker
+from geometry_msgs.msg import Point, Pose
+
+markers = dict()
 
 def callback(data):
-    rospy.loginfo(rospy.get_caller_id() + 'msg: %s', data.data)
+    markers[data.id] = data.pose.position
+    #rospy.loginfo(rospy.get_caller_id() + 'msg: %s', data.points)
 
 def listener():
     rospy.init_node('listener', anonymous=True)
