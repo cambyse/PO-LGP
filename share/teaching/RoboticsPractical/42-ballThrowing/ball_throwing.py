13720b6acfc332a77e230a5a7251f9a7688dd2a6
Heiko Zimmermann <mail@zimmermannhe.de>
2016-06-23 19:12:05 +0200

    My commit messages are really lame...

--- share/teaching/RoboticsPractical/42-ballThrowing/ball_throwing.py
+++ share/teaching/RoboticsPractical/42-ballThrowing/ball_throwing.py
@@ -10,20 +10,6 @@ import alvar_marker_test
 from baxter_interface import Gripper
 import numpy as np
 
-# Initialise such that it registers somehow or something, I don't know...
-rp.init_node('Hello_Baxter')
-limb = bax.Limb('left')
-left_gripper = Gripper('left')
-
-# Get joint startpos_1
-startpos = dict()
-startpos['left_w0'] = -0.7834806874124751
-startpos['left_w1'] = -0.3857961681531816
-startpos['left_w2'] = -2.6349954983901696
-startpos['left_e0'] = -2.5713352956929247
-startpos['left_e1'] =  0.25732527716777814
-startpos['left_s0'] = -0.45674277959288195
-startpos['left_s1'] =  1.04272344056511
 
 #def disable_gravity():
 #    os.execv('/opt/ros/indigo/bin/rostopic', ['rostopic', 'pub', '-r', '10',
@@ -38,7 +24,7 @@ def dict2npa(u):
                    u['left_s1']])
     return na
 
-def npa2dict(u)
+def npa2dict(u):
     return {'left_w0': 0,
             'left_w1': u[0],
             'left_w2': 0,
@@ -61,6 +47,62 @@ def control(features, W):
     v = np.dot(W, features) + np.random.randn(3)
     v_dict = npa2dict(v)
 
+    for key, value in limits.items:
+        if not limb.joint_angle[key] in value:
+            v_dict[key] = 0
+
+    return v_dict
+
+def expected_policy_reward(samples, T, W):
+    episod_rewards = []
+    for e in range(samples):
+        for t in range(T):
+            v = control(features(t), W)
+            limb.set_joint_velocities(v)
+            time.sleep(0.01)
+
+
+
+
+        episod_reward.append(1000000)
+
+    episod_rewards = np.array(episod_reward)
+    return np.sum(episod_rewards)/episod_rewards.size
+
+def start(E, T, W):
+    er_prev = 0
+
+    while True:
+        noise = np.random.randn(W.shape[0], W.shape[1])
+        er = expected_policy_reward(1, T, W + noise)
+
+        if er > er_prev:
+            er_prev = er
+            W += noise
+
+
+def reset():
+    limb.move_to_joint_positions(startpos)
+
+def init_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--simulate', help="Don't use any ros\
+            connections, just simulate.", action='store_true', dest='simulate')
+    parser.set_defaults(simulate=False)
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    # Get joint startpos_1
+    startpos = dict()
+    startpos['left_w0'] = -0.7834806874124751
+    startpos['left_w1'] = -0.3857961681531816
+    startpos['left_w2'] = -2.6349954983901696
+    startpos['left_e0'] = -2.5713352956929247
+    startpos['left_e1'] =  0.25732527716777814
+    startpos['left_s0'] = -0.45674277959288195
+    startpos['left_s1'] =  1.04272344056511
+    
     limits = {'left_w0': range(-3.059 * lb, 3.059 * ub),
             'left_w1': range(-1.5708 * lb, 2.094 * ub),
             'left_w2': range( -3.059 * lb, 3.059 * ub),
@@ -69,33 +111,20 @@ def control(features, W):
             'left_s0': range(-1.70168 * lb, 1.70168 * ub),
             'left_s1': range(-2.147 * lb, 1.047 * ub)}
 
-    for key, value in limits.items:
-        if not limb.joint_angle[key] in value:
-            v_dict[key] = 0
-
-    return v_dict
-
-
-def start(E, T): 
-    for e in range(E):
-        for t in range(T):
-            v = control(features(t), W)
-            limb.set_joint_velocities(v)
-            time.sleep(0.01)
-
-def reset():
-    limb.move_to_joint_positions(startpos)
-
-def init_parser():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-s', '--simulate', help="Don't use any ros connections, just simulate.", type=bool, default=0)
-    return parser.parse_args()
-
-
-if __name__ == "__main__":
     args = init_parser()
 
     # Start alvar listener thread thingy
-    if (args.simulate == 0):
+    print("Simulate value", args.simulate)
+    if (args.simulate):
+
+    else:
         thread.start_new_thread(alvar_marker_test.listener, ())
+        # Initialise such that it registers somehow or something, I don't know...
+        rp.init_node('Hello_Baxter')
+        limb = bax.Limb('left')
+        left_gripper = Gripper('left')
+
     print('All done')
+
+
+
