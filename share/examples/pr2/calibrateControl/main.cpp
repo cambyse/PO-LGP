7c845345a2a27bffc8e41c3894ba98e9915e8c0e
Marc Toussaint <marc.toussaint@uni-stuttgart.de>
2016-07-05 16:09:52 +0200

    ctrlCalib

---
+++ share/examples/pr2/calibrateControl/main.cpp
@@ -0,0 +1,65 @@
+#include <Core/module.h>
+#include <RosCom/roscom.h>
+#include <RosCom/spinner.h>
+#include <Actions/gamepadControl.h>
+#include <Control/TaskControllerModule.h>
+#include <Hardware/gamepad/gamepad.h>
+#include <Ors/orsviewer.h>
+
+#include <sensor_msgs/JointState.h>
+//#include <pr2/baxter.h>
+
+
+
+// =================================================================================================
+int main(int argc, char** argv){
+  mlr::initCmdLine(argc, argv);
+
+  rosCheckInit("gamepadControl");
+
+  Access_typed<CtrlMsg> ctrl_ref(NULL, "ctrl_ref");
+  Access_typed<CtrlMsg> ctrl_obs(NULL, "ctrl_obs");
+  Access_typed<arr>     pr2_odom(NULL, "pr2_odom");
+
+  Access_typed<arr> q_ref(NULL, "q_ref");
+  Access_typed<sensor_msgs::JointState> jointState(NULL, "jointState");
+
+  TaskControllerModule tcm;
+  OrsViewer view;
+  RosCom_Spinner spinner; //the spinner MUST come last: otherwise, during closing of all, it is closed before others that need messages
+
+  if(mlr::getParameter<bool>("useRos")){
+    mlr::String robot = mlr::getParameter<mlr::String>("robot", "pr2");
+    if(robot=="pr2"){
+      new SubscriberConvNoHeader<marc_controller_pkg::JointState, CtrlMsg, &conv_JointState2CtrlMsg> ("/marc_rt_controller/jointState", ctrl_obs);
+      new PublisherConv<marc_controller_pkg::JointState, CtrlMsg, &conv_CtrlMsg2JointState>          ("/marc_rt_controller/jointReference", ctrl_ref);
+      new SubscriberConv<geometry_msgs::PoseWithCovarianceStamped, arr, &conv_pose2transXYPhi>       ("/robot_pose_ekf/odom_combined", pr2_odom);
+    }
+    if(robot=="baxter"){
+      new Subscriber<sensor_msgs::JointState> ("/robot/joint_states", jointState);
+    }
+  }
+
+  threadOpenModules(true);
+
+  mlr::wait(1.);
+  cout <<"NOW" <<endl;
+
+  arr q0 = tcm.modelWorld.get()->q;
+  TaskMap_qItself map;
+  CtrlTask task("qItself", &map, 1., 1., 1., 10.);
+
+  q0 += .2;
+  task.setTarget(q0);
+
+  tcm.ctrlTasks.set() = { &task };
+
+  mlr::wait(3.);
+
+//  moduleShutdown().waitForValueGreaterThan(0);
+
+  threadCloseModules();
+
+  cout <<"bye bye" <<endl;
+  return 0;
+}
