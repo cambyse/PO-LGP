67baa6d381fca534bff51505eb2a801f6167d2ca
Heiko Zimmermann <mail@zimmermannhe.de>
2016-07-07 18:56:39 +0200

    .

--- share/teaching/RoboticsPractical/42-ballThrowing/offline_learning.py
+++ share/teaching/RoboticsPractical/42-ballThrowing/offline_learning.py
@@ -2,6 +2,8 @@
 import numpy as np
 import sys
 import csv
+from sklearn import linear_model
+from sklearn.neighbors import NearestNeighbors 
 
 D = np.array([]).reshape(0, 13)
 
@@ -13,5 +15,28 @@ for arg in sys.argv[1:]:
                     continue
                 D  = np.vstack([D, np.fromstring(line, dtype=np.float, sep=",")])
                 #print(D)
-            
-X, y = np.hsplit(D, [-1])
+
+def discard_far_points(x, D, eps):
+    D_close = np.array([]).reshape(0, 13)
+    
+    for i in range(D.shape[0]):
+        if np.linalg.norm(D[i, :-1] - x) < eps:
+            D_close = np.vstack([D_close, D[i,:]])
+
+    return D_close
+
+def kNN(x, D, k):
+    nbrs = NearestNeighbors(n_neighbors=k, algorithm='ball_tree').fit(D[:,:-1])
+    distances, indices = nbrs.kneighbors(x)
+    return D[indices][0]
+
+
+def estimate_gradient(x, D, k): 
+    k = min(D.shape[0], k)
+
+    X, y = np.hsplit(kNN(x, D, k), [-1])
+    rreg = linear_model.LinearRegression()
+    print(X)
+    rreg.fit (X, y)
+
+    return rreg.coef_
