4d25301c3434092a1ca98291f36a2af752b41208
Marc Toussaint <marc.toussaint@uni-stuttgart.de>
2016-08-12 11:21:16 +0200

    auto

--- share/data/pr2_model/HOWTO.sh
+++ share/data/pr2_model/HOWTO.sh
@@ -2,6 +2,6 @@ cp $1 z.urdf
 ../../bin/urdf2ors.py > z1-raw.ors
 sed 's/package:\/\/pr2_description\/meshes\///g' z1-raw.ors > z2-paths.ors
 cat pr2_before.ors z2-paths.ors pr2_after.ors > z3-augmented.ors
-ors_editor -file z3-augmented.ors -cleanOnly
+../../bin/ors_editor -file z3-augmented.ors -cleanOnly
 mv z.ors z4-clean.ors
 
