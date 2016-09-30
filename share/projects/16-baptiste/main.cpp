396bd122f76ac2c805ffb35d05e2f18b50715b7e
buschbapti <baptiste.busch@inria.fr>
2016-09-30 16:24:38 +0200

    Create the REBA task Map
    - REBA task is created and correctly lead to the resting posture.
    - Computation time is a bit high
    - cost seems to be too high

--- share/projects/16-baptiste/main.cpp
+++ share/projects/16-baptiste/main.cpp
@@ -1,4 +1,8 @@
 #include <Motion/komo.h>
+#include <string>
+#include <map>
+
+using namespace std;
 
 struct HandPositionMap:TaskMap{
   virtual void phi(arr& y, arr& J, const ors::KinematicWorld& G, int t=-1){
@@ -23,22 +27,61 @@ struct HandPositionMap:TaskMap{
 //===========================================================================
 
 struct RebaMap:TaskMap{
+	map<const string, arr> coeffs_map;
+	const int nb_joints = 18;
+	const string joint_names[18] = {"neck_0", "neck_1", "neck_2",
+	                                "spine_0", "spine_1", "spine_2",
+	                                "right_knee", "left_knee",
+	                                "right_shoulder_0", "right_shoulder_1",
+	                                "left_shoulder_0", "left_shoulder_1",
+	                                "right_elbow_0", "left_elbow_0",
+	                                "right_wrist_0", "right_wrist_1",
+	                                "left_wrist_0", "left_wrist_1"};
+
+	RebaMap(){
+		initCoefficientMap(coeffs_map);
+	}
+
+	void initCoefficientMap( map<const string, arr>& theMap ){
+		theMap["neck_0"] = ARR(0.45594512, -0.07958066,  1.00000056);
+		theMap["neck_1"] = ARR(0.45594512, -0.07958066,  1.00000056);
+		theMap["neck_2"] = ARR(0.45594512, -0.07958066,  1.00000056);
+		theMap["spine_0"] = ARR(1.82377278,  0.        ,  1.        );          
+		theMap["spine_1"] = ARR(1.82377278,  0.        ,  1.        );   
+		theMap["spine_2"] = ARR(1.82377278,  0.        ,  1.        );
+		theMap["right_knee"] = ARR(1.82377278,  0.        ,  1.        );       
+		theMap["left_knee"] = ARR(1.82377278,  0.        ,  1.        ); 
+		theMap["right_shoulder_0"] = ARR(1.21587174, -3.81974618,  4.        ); 
+		theMap["right_shoulder_1"] = ARR(1.21584852,  0.        ,  1.        );
+		theMap["left_shoulder_0"] = ARR(1.21587174, -3.81974618,  4.        );  
+		theMap["left_shoulder_1"] = ARR(1.21584852,  0.        ,  1.        );
+		theMap["right_elbow_0"] = ARR(1.36783611, -2.3873254 ,  2.        );    
+		theMap["left_elbow_0"] = ARR(1.36783611,  2.3873254 ,  2.        ); 
+		theMap["right_wrist_0"] = ARR(1.62113894,  0.        ,  1.        );    
+		theMap["right_wrist_1"] = ARR(1.62113894,  0.        ,  1.        ); 
+		theMap["left_wrist_0"] = ARR(1.62113894,  0.        ,  1.        );     
+		theMap["left_wrist_1"] = ARR(1.62113894,  0.        ,  1.        );
+	}
+
   virtual void phi(arr& y, arr& J, const ors::KinematicWorld& G, int t=-1){
-    ors::Joint *j1 = G.getJointByName("right_shoulder_1");
-    ors::Joint *j2 = G.getJointByName("left_shoulder_1");
-
-    y = ARR(G.q(j1->qIndex), G.q(j2->qIndex));
-
+    y = ARR();
     if(&J){
-      J = zeros(2, G.q.N);
-      J(0, j1->qIndex)=1.;
-      J(1, j2->qIndex)=1.;
+      J = zeros(nb_joints, G.q.N);
+    }
+
+    for (int i=0; i<nb_joints; ++i){
+    	const string name = joint_names[i];
+    	arr coeffs = coeffs_map[name];
+    	ors::Joint *joint = G.getJointByName(name.c_str());
+    	float joint_value = G.q(joint->qIndex);
+    	y.append(coeffs(0) * joint_value * joint_value + coeffs(1) * joint_value + coeffs(2));
+    	J(i, joint->qIndex) = 2 * coeffs(0) * joint_value + coeffs(1);
     }
   }
 
 
   virtual uint dim_phi(const ors::KinematicWorld& G){
-    return 2;
+    return nb_joints;
   }
 
   virtual mlr::String shortTag(const ors::KinematicWorld& G){ return mlr::String("RebaMap"); }
@@ -48,6 +91,7 @@ struct RebaMap:TaskMap{
 //===========================================================================
 
 void moveReba(){
+  
   KOMO komo;
   komo.setConfigFromFile();
 
@@ -57,13 +101,13 @@ void moveReba(){
 #if 0
   komo.setPosition(1., 1., "endeffL", "target", sumOfSqrTT, NoArr, 1e2);
 #else
-  komo.setTask(1., 1., new RebaMap(), sumOfSqrTT, {.5, -.5}, 1e2);
+  komo.setTask(0., 1., new RebaMap(), sumOfSqrTT, NoArr, 1e2);
 #endif
   komo.setSlowAround(1., .1, 1e3);
 
   komo.reset();
   komo.run();
-//  komo.checkGradients();
+  komo.checkGradients();
 
   Graph result = komo.getReport(true);
 
@@ -79,7 +123,3 @@ int main(int argc,char** argv){
 
   return 0;
 }
-
-
-
-
