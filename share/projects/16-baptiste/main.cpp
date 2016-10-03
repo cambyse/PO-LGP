71df46b6098030529a209200a8a447a44f956e8b
Baptiste Busch <baptiste.busch@gmail.com>
2016-10-03 17:22:28 +0200

    Tune the cost + add the HandPositionMap
    REBA Map is validated in collision with the HandPositionMap. For it to
    work a tuning factor for the score function was nocesseray.
    TODO: evaluate this tuning.

--- share/projects/16-baptiste/main.cpp
+++ share/projects/16-baptiste/main.cpp
@@ -20,7 +20,7 @@ struct HandPositionMap:TaskMap{
     return 3;
   }
 
-  virtual mlr::String shortTag(const ors::KinematicWorld& G){ return mlr::String("RebaMap"); }
+  virtual mlr::String shortTag(const ors::KinematicWorld& G){ return mlr::String("HandPositionMap"); }
 
 };
 
@@ -43,24 +43,24 @@ struct RebaMap:TaskMap{
 	}
 
 	void initCoefficientMap( map<const string, arr>& theMap ){
-		theMap["neck_0"] = ARR(0.45594512, -0.07958066,  1.00000056);
-		theMap["neck_1"] = ARR(0.45594512, -0.07958066,  1.00000056);
-		theMap["neck_2"] = ARR(0.45594512, -0.07958066,  1.00000056);
-		theMap["spine_0"] = ARR(1.82377278,  0.        ,  1.        );          
-		theMap["spine_1"] = ARR(1.82377278,  0.        ,  1.        );   
-		theMap["spine_2"] = ARR(1.82377278,  0.        ,  1.        );
-		theMap["right_knee"] = ARR(1.82377278,  0.        ,  1.        );       
-		theMap["left_knee"] = ARR(1.82377278,  0.        ,  1.        ); 
-		theMap["right_shoulder_0"] = ARR(1.21587174, -3.81974618,  4.        ); 
-		theMap["right_shoulder_1"] = ARR(1.21584852,  0.        ,  1.        );
-		theMap["left_shoulder_0"] = ARR(1.21587174, -3.81974618,  4.        );  
-		theMap["left_shoulder_1"] = ARR(1.21584852,  0.        ,  1.        );
-		theMap["right_elbow_0"] = ARR(1.36783611, -2.3873254 ,  2.        );    
-		theMap["left_elbow_0"] = ARR(1.36783611,  2.3873254 ,  2.        ); 
-		theMap["right_wrist_0"] = ARR(1.62113894,  0.        ,  1.        );    
-		theMap["right_wrist_1"] = ARR(1.62113894,  0.        ,  1.        ); 
-		theMap["left_wrist_0"] = ARR(1.62113894,  0.        ,  1.        );     
-		theMap["left_wrist_1"] = ARR(1.62113894,  0.        ,  1.        );
+		theMap["neck_0"] = ARR(0.45594512, -0.07958066,  1.00000056, 0.01);
+		theMap["neck_1"] = ARR(0.45594512, -0.07958066,  1.00000056, 0.01);
+		theMap["neck_2"] = ARR(0.45594512, -0.07958066,  1.00000056, 0.01);
+		theMap["spine_0"] = ARR(1.82377278,  0.        ,  1.        , 0.01);          
+		theMap["spine_1"] = ARR(1.82377278,  0.        ,  1.        , 0.01);   
+		theMap["spine_2"] = ARR(1.82377278,  0.        ,  1.        , 0.01);
+		theMap["right_knee"] = ARR(1.82377278,  0.        ,  1.        , 0.01);       
+		theMap["left_knee"] = ARR(1.82377278,  0.        ,  1.        , 0.01); 
+		theMap["right_shoulder_0"] = ARR(1.21587174, -3.81974618,  4.        , 0.01); 
+		theMap["right_shoulder_1"] = ARR(1.21584852,  0.        ,  1.        , 0.01);
+		theMap["left_shoulder_0"] = ARR(1.21587174, -3.81974618,  4.        , 0.01);  
+		theMap["left_shoulder_1"] = ARR(1.21584852,  0.        ,  1.        , 0.01);
+		theMap["right_elbow_0"] = ARR(1.36783611, -2.3873254 ,  2.        , 0.01);    
+		theMap["left_elbow_0"] = ARR(1.36783611,  2.3873254 ,  2.        , 0.01); 
+		theMap["right_wrist_0"] = ARR(1.62113894,  0.        ,  1.        , 0.01);    
+		theMap["right_wrist_1"] = ARR(1.62113894,  0.        ,  1.        , 0.01); 
+		theMap["left_wrist_0"] = ARR(1.62113894,  0.        ,  1.        , 0.01);     
+		theMap["left_wrist_1"] = ARR(1.62113894,  0.        ,  1.        , 0.01);
 	}
 
   virtual void phi(arr& y, arr& J, const ors::KinematicWorld& G, int t=-1){
@@ -74,8 +74,8 @@ struct RebaMap:TaskMap{
     	arr coeffs = coeffs_map[name];
     	ors::Joint *joint = G.getJointByName(name.c_str());
     	float joint_value = G.q(joint->qIndex);
-    	y.append(coeffs(0) * joint_value * joint_value + coeffs(1) * joint_value + coeffs(2));
-    	J(i, joint->qIndex) = 2 * coeffs(0) * joint_value + coeffs(1);
+    	y.append(coeffs(3) * (coeffs(0) * joint_value * joint_value + coeffs(1) * joint_value + coeffs(2)));
+    	J(i, joint->qIndex) = coeffs(3) * (2 * coeffs(0) * joint_value + coeffs(1));
     }
   }
 
@@ -101,6 +101,7 @@ void moveReba(){
 #if 0
   komo.setPosition(1., 1., "endeffL", "target", sumOfSqrTT, NoArr, 1e2);
 #else
+  komo.setTask(.5, 1., new HandPositionMap(), sumOfSqrTT, NoArr, 1e2);
   komo.setTask(0., 1., new RebaMap(), sumOfSqrTT, NoArr, 1e2);
 #endif
   komo.setSlowAround(1., .1, 1e3);
