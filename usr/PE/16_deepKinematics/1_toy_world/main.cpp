76450fbe8371af575a2c26d9bf08f6e8a3cd3175
Peter Englert <peter.englert@ipvs.uni-stuttgart.de>
2016-11-18 15:05:05 +0100

    changed depth image in OpenGL from byteA to floatA

--- usr/PE/16_deepKinematics/1_toy_world/main.cpp
+++ usr/PE/16_deepKinematics/1_toy_world/main.cpp
@@ -11,47 +11,67 @@
 #include <Perception/kinect2pointCloud.h>
 
 int main(int argc,char **argv){
-  double width = 640;
-  double height = 480;
-
-  // initialize world and set camera
+  /// initialize world
+  double w = 640;
+  double h = 480;
   ors::KinematicWorld world("toy.ors");
-  world.gl().resize(width,height);
+  OpenGL gl("background", w,h);
+  gl.add(glStandardScene, 0);
+  gl.add(world);
 
-  world.gl().camera.setKinect();
-  world.gl().camera.setPosition(0., -.5, .5);
-  world.gl().camera.focus(0.,0.,0.);
+  /// set camera position
+  gl.camera.setKinect();
+  gl.camera.setPosition(0., -.5, .5);
+  gl.camera.focus(0.,0.,0.);
 
-  world.getBodyByName("box")->X.addRelativeRotationDeg(30,0.,0.,1.);
+  world.getBodyByName("box")->X.addRelativeRotationDeg(15,0.,0.,1.);
   world.calc_fwdPropagateFrames();
-  world.gl().update("simulator",true,true);
 
-  world.gl().renderInBack(true, true, width,height);
+  /// render scene
+  gl.update("", true, true, true);
+//  gl.renderInBack(true, true, w, h);
 
-  // get depth image and compute pointcloud
+  /// get image and depth image
   ors::Mesh mesh;
-  arr& pts = mesh.V;
-  arr& cols = mesh.C;
-  byteA captureDepth = world.gl().captureDepth;
-  byteA captureImage = world.gl().captureImage;
+  arr pts, cols;
+  floatA captureDepth = gl.captureDepth;
+  byteA captureImage = gl.captureImage;
 
-  // convert byteA to uint16A
-  uint16A depth_image((const uint16_t*)captureDepth.p, captureDepth.N);
-  depth_image.reshape(captureDepth.d0,captureDepth.d1);
+  /// convert byteA to uint16A
+  uint16A depth_image;
+  depth_image.resize(captureDepth.d0,captureDepth.d1);
+  for (uint i=0;i<depth_image.N;i++) {
+    double d = (double) captureDepth.elem(i);
+    gl.camera.glConvertToTrueDepth(d);
+    depth_image.elem(i) = (uint16_t) (d*1000.); // conv. from [m] -> [mm]
+  }
+  if (captureDepth.min()==0) {
+    cout <<"max D=" <<captureDepth.max() <<" min d=" <<captureDepth.min() <<endl;
+    MLR_MSG("image not completely rendered!"); return 0;
+  }
+  cout <<"max D=" <<depth_image.max() <<" min d=" <<depth_image.min() <<endl;
 
+  //  flip_image(captureImage);
+
+  /// convert depth image to pointcloud
   images2pointcloud(pts,cols,captureImage,depth_image);
-//  depthData2pointCloud(pts,depth_image);
+  //depthData2pointCloud(pts,depth_image);
 
-//  ors::Transformation T;
-//  T = world.gl().camera.X;
-//  T.setInverse(world.gl().camera.X);
-//  T.applyOnPointArray(pts);
+  for(uint i=0;i<pts.d0;i++) pts(i,2) *= -1.; // switch z dimension of points
+  ors::Transformation T = gl.camera.X;
+  T.applyOnPointArray(pts);
+  cout << T.rot.getArr() << endl;
 
-  world.gl().add(mesh);
-  world.watch(true);
+  /// visualize pointcloud
+  mesh.V = pts;
+  mesh.C = cols;
+  gl.add(mesh);
+  gl.watch();
 
-  write_ppm(world.gl().captureImage, "z.ppm", true);
-  write_ppm(world.gl().captureDepth, "z.d.ppm", true);
+  /// save image and depth_image to file
+  write_ppm(gl.captureImage, "z.ppm", true);
+  byteA D = convert<byte, float>(captureDepth*255.f);
+  write_ppm(D, "z.d.ppm", true);
 
   return 0;
 }
