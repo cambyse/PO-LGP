4a666754e4ed5179955473d02fb32046e4c16c37
Marc Toussaint <marc.toussaint@uni-stuttgart.de>
2017-01-22 11:29:01 +0100

    docs updates

--- share/examples/Optim/bayesOpt/main.cpp
+++ share/examples/Optim/bayesOpt/main.cpp
@@ -8,15 +8,17 @@ void TEST(BayesOpt){
   arr bounds_lo = consts<double>(-2., d);
   arr bounds_hi = consts<double>(+2., d);
   BayesOpt opt(f, bounds_lo, bounds_hi);
-  for(uint i=0;i<10;i++){
+  for(uint i=0;i<50;i++){
     opt.step();
 //    cout <<opt.data_X <<opt.data_y <<endl;
     opt.report();
+    mlr::wait(.1);
     mlr::wait();
   }
 }
 
 int main(int argn,char** argv){
+  rnd.clockSeed();
   testBayesOpt();
   return 0;
 }
