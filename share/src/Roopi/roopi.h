3f681f6a1082c13ab7dd62d46ff3b68d0f01d1e7
Marc Toussaint <marc.toussaint@uni-stuttgart.de>
2017-03-24 11:55:14 +0100

    delete registry fix

--- share/src/Roopi/roopi.h
+++ share/src/Roopi/roopi.h
@@ -37,7 +37,7 @@ struct Roopi {
   ~Roopi();
 
   //==============================================================================
-
+  //
   //-- CONTROL FLOW: This is robotics independent; generic methods to schedule, trigger scripts, etc
 
   void report();
@@ -66,7 +66,7 @@ struct Roopi {
   Act::Ptr whenever(const Act_Event::Ptr& event, const std::function<int()>& script);
 
   //==============================================================================
-
+  //
   //-- INITIALIZATION
 
   void setKinematics(const char* filename, bool controlView=true);          ///< set kinematics by hand (done in 'autoStartup')
@@ -84,7 +84,6 @@ struct Roopi {
   Act_TaskController& getTaskController();           ///< get taskController (to call verbose, or lock..)
   Act_ComPR2& getComPR2();                           ///< to call 'stopSendingMotion'
 
-
   //-- direct access to variables and threads
   template<class T> Access<T> variable(const char* var_name){    ///< get a handle to a typed variable: you can R/W-access the data or read/wait for the revision
     return Access<T>(NULL, var_name, false); }
@@ -94,14 +93,19 @@ struct Roopi {
   template<class T> T* thread(const char* thread_name){ return getThread<T>(thread_name); }
   void reportCycleTimes();
 
-  //-- kinematic editing (to be done more..)
+  //==============================================================================
+  //
+  //-- KINEMATIC EDITING (to be done more...)
+
   mlr::Shape* newMarker(const char* name, const arr& pos);        ///< adds a shape to the model world
   void kinematicSwitch(const char* object, const char* attachTo, bool placing); ///< switches kinematics in the model world
   WToken<mlr::KinematicWorld> setK();                             ///< get write access to the model world
   RToken<mlr::KinematicWorld> getK();                             ///< get read access to the model world
-  void resyncView();
 
-  //-- control
+  //==============================================================================
+  //
+  //-- MOTION CONTROL
+
   Act_CtrlTask::Ptr newCtrlTask(){ return Act_CtrlTask::Ptr(new Act_CtrlTask(this)); }  ///< set the CtrlTask yourself (see newHoldingTask as example)
   Act_CtrlTask::Ptr newCtrlTask(TaskMap *map, const arr& PD={1.,.9}, const arr& target={0.}, const arr& prec={1.});
   Act_CtrlTask::Ptr newCtrlTask(const char* specs);
@@ -118,9 +122,10 @@ struct Roopi {
   Act_CtrlTask::Ptr collisions(bool on);
   void deactivateCollisions(const char* s1, const char* s2);
 
+  //==============================================================================
+  //
+  //-- VARIOUS ACTIVITIES
 
-
-  //-- some activities
   Act_Thread::Ptr  newThread(Thread* th)  { return Act_Thread::Ptr(new Act_Thread(this, th)); } ///< a trivial wrapper to make a thread (create it with new YourThreadClass) an activity
   Act_ComPR2::Ptr  newComPR2()            { return Act_ComPR2::Ptr(new Act_ComPR2(this)); } ///< subscribers/publishers that communicate with PR2
   Act_PathOpt::Ptr newPathOpt()           { return Act_PathOpt::Ptr(new Act_PathOpt(this)); } ///< a path optimization activity, access komo yourself to define the problem
@@ -129,15 +134,12 @@ struct Roopi {
   Act_Thread::Ptr PhysX();           ///< run PhysX (nvidia physical simulator)
   Act_Thread::Ptr GamepadControl();  ///< activate gamepad to set controls
   Act_Thread::Ptr CameraView(const char* modelWorld_name="modelWorld");      ///< compute and display the camera view
-//  Act_Thread newKinect2Pcl(bool view=true);
   Act_PclPipeline PclPipeline(bool view=false);
   Act_PerceptionFilter PerceptionFilter(bool view=false);
 
-
   //==============================================================================
   //
-  // MACROS, which call scripts
-  //
+  // MACROS which call scripts
 
   Act::Ptr graspBox(const char* objName, LeftOrRight lr){
     return run( [this, objName, lr](){ return Script_graspBox(*this, objName, lr); } );
@@ -159,110 +161,3 @@ struct Roopi {
   }
 
 };
-
-//==============================================================================
-
-
-#if 0
-  //-- control tasks
-
-  /// creates a new CtrlTask; pass a 'newed' map as argument, it will be deleted later; after creation it is inactive
-  CtrlTask *createCtrlTask(const char* name, TaskMap* map, bool active=false);
-
-  /// activate a control task after you've set parameters, etc
-  void activateCtrlTask(CtrlTask* t, bool reinitializeReferences = false);
-  /// deactivate a control task
-  void deactivateCtrlTask(CtrlTask* t);
-
-  /// removes a CtrlTask and deletes also the task map
-  void destroyCtrlTask(CtrlTask* t);
-
-  /// modifies CtrlTasks
-  void modifyCtrlTaskReference(CtrlTask* ct, const arr& yRef, const arr& yDotRef = NoArr);
-
-  /// holds all joints in position. Desactivates all other tasks
-  void holdPosition();
-  /// release hold position task. Has no effect on other tasks
-  void releasePosition();
-
-  /// returns whether a CtrlTask has converged
-  bool converged(CtrlTask* ct, double tolerance = 1e-2);
-
-  /// wait for all given CtrlTasks to be converged
-  bool waitForConv(CtrlTask* ct, double maxTime = -1, double tolerance = 1e-2);
-  bool waitForConv(const CtrlTaskL& cts, double maxTime = -1, double tolerance = 1e-2);
-
-  /// wait for a CtrlTask to be at a specific state;
-  bool waitForConvTo(CtrlTask* ct, const arr& desState, double maxTime = -1, double tolerance = 1e-2);
-
-  // low-level ctr - use is discouraged!!
-  struct TaskControlThread* tcm(); //low-level access of the tcm - really necessary? Danny: yes
-//  void addCtrlTask(CtrlTask* ct); ///< adds CtrlTask directly to the taskController
-//  void addCtrlTasks(CtrlTaskL cts); ///< adds multiple CtrlTasks directly to the taskController
-
-  // shortcuts to crate standard CtrlTasks (mt) I'd remove this for now
-  //  /// adds qItself task to the taskController
-  //  CtrlTask* _addQItselfCtrlTask(const arr& qRef, const arr& Kp = ARR(30.0), const arr& Kd = ARR(5.0));
-
-  //  /// creates inactive default task with zero gains and adds it to the taskController
-  //  CtrlTask* _addDefaultCtrlTask(const char* name,
-  //                               const TaskMap_DefaultType type,
-  //                               const char* iShapeName, const mlr::Vector& iVec = NoVector,
-  //                               const char* jShapeName = NULL, const mlr::Vector& jVec = NoVector);
-
-  //-- trajectory tasks
-
-  void interpolateToReferenceThread(CtrlTask *task, double executionTime, const arr &reference, const arr &start);
-
-  TaskReferenceInterpolAct* createTaskReferenceInterpolAct(const char* name, CtrlTask* task);
-  void interpolateToReference(TaskReferenceInterpolAct* t, double executionTime, const arr& reference, const arr& start = NoArr);
-  bool waitForFinishedTaskReferenceInterpolAct(TaskReferenceInterpolAct* t, double maxTime = -1);
-
-  void interpolateToReference(CtrlTask* task, double executionTime, const arr& reference, const arr& start = NoArr);
-
-  void followTaskTrajectory(CtrlTask* task, double executionTime, const arr& trajectory);
-  void followTaskTrajectories(const CtrlTaskL& tasks, double executionTime, const arrA& trajY, const arrA& trajYDot = {}, const arrA& trajYDDot = {});
-  void followQTrajectory(const Roopi_Path* path);
-
-  //-- planning tasks
-
-  Roopi_Path* createPathInJointSpace(CtrlTask* task, double executionTime, bool verbose = false);
-  Roopi_Path* createPathInJointSpace(const CtrlTaskL& tasks, double executionTime, bool verbose = false);
-
-  //-- macros
-
-  /// move "shape" to "pos" using a motion planner.
-  bool goToPosition(const arr& pos, const char* shape, double executionTime, bool verbose = false);
-
-  /// move to joint configuration using a motion planner
-  bool gotToJointConfiguration(const arr& jointConfig, double executionTime, bool verbose = false);
-
-  //-- low-level access
-
-  arr getJointState();
-  arr getJointSign();
-  arr getTorques();
-  arr getFTLeft();
-  arr getFTRight();
-
-  /// get current value of the underlying task map
-  arr getTaskValue(CtrlTask* task);
-
-  /// sync the joint configuration of the model world into the planWorld
-  void syncWorldWithReal(mlr::KinematicWorld& K);
-  mlr::KinematicWorld& getPlanWorld();
-  void copyModelWorld(mlr::KinematicWorld& K);
-
-  double getLimitConstraint(double margin = 0.05);
-  double getCollisionConstraint(double margin = 0.1);
-
-  //-- TODO
-
-  //getModelWorld() what return type?
-
-//  //-- wait for & stop tasks
-  void stop(const CtrlTaskL& tasks);
-  void waitConv(const CtrlTaskL& tasks);
-#endif
-
-
