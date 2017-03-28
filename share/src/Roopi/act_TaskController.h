e5b57dc6c40edfe98d8bbfb23c5f614330f4a737
Matt Bernstein <matthew.bernstein@ipvs.uni-stuttgart.de>
2017-03-28 16:43:49 +0200

    Lockbox cleanup

--- share/src/Roopi/act_TaskController.h
+++ share/src/Roopi/act_TaskController.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "act.h"
+#include "Kin/kin.h"
 
 struct Act_TaskController : Act{
   struct TaskControlThread *tcm = NULL;
@@ -10,4 +11,7 @@ struct Act_TaskController : Act{
 
   void verbose(int verbose=1);
   void lockJointGroupControl(const char *groupname, bool lockThem=true);
+  void setRealWorldJoint(mlr::Joint* jt, const arr& q);
+  TaskControlThread* getTCM();
+
 };
