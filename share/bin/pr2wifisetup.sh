11620c1a369a2668122308fec34d095327a373de
Matt Bernstein <matthew.bernstein@ipvs.uni-stuttgart.de>
2017-05-23 11:06:41 +0200

    Updated ROS scripts for 16.04

--- share/bin/pr2wifisetup.sh
+++ share/bin/pr2wifisetup.sh
@@ -1,7 +1,18 @@
 echo "Setting ENV..."
 
 export ROS_MASTER_URI=http://bigbirdc1.informatik.uni-stuttgart.de:11311
-export ROS_IP=`ifconfig wlan0 | grep 'inet addr:'| grep -v '127.0.0.1' | cut -d: -f2 | awk '{ print $1}'`
+
+UB=`lsb_release -rs`
+
+if [ UB = "16.04" ]; then
+	DEV=`udevadm info -e | grep -oE -m 1 "/wl[[:alnum:]]*" | grep -oE "wl[[:alnum:]]*"`
+elif [ UB = "14.04" ]; then
+	DEV="wlan0"
+fi
+
+DEV='udevadm info -e | grep -oE -m 1 "/wl[[:alnum:]]*" | grep -oE "wl[[:alnum:]]*"'
+
+export ROS_IP=`ifconfig eth0 | grep 'inet addr:'| grep -v '127.0.0.1' | cut -d: -f2 | awk '{ print $1}'`
 # export ROS_HOSTNAME=`hostname`
 export ROS_HOSTNAME=$ROS_IP
 
