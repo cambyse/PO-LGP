3db646fa898315572290e5178052127a5b0c4f76
Marc Toussaint <marc.toussaint@uni-stuttgart.de>
2017-08-29 14:05:07 -0400

    auto

--- share/projects/17-LGP-belief/main.cpp
+++ share/projects/17-LGP-belief/main.cpp
@@ -25,15 +25,20 @@ void solve(){
   komo.world.report();
   komo.setPathOpt(6., 10, 10.);
 
+  //initialization task...
   Task *t = komo.setTask(1.5, 1.5, new TaskMap_Default(posTMT, K, "base_footprint"), OT_sumOfSqr, {1., -2., 0.}, 1e1);
 
+  //gaze at landmark
 //  komo.setTask(1., -1., new TaskMap_Default(gazeAtTMT, K, "endeffEyes", NoVector, "landmark"), OT_sumOfSqr, {}, 1e1);
 
+  //3D target pose
   komo.setTask(3., -1., new TaskMap_qItself(QIP_byJointNames, {"worldTranslationRotation"}, K), OT_sumOfSqr, q_target);
 
-  komo.setTask(-1., -1., new TaskMap_BeliefTransition(new TaskMap_Default(gazeAtTMT, K, "endeffEyes", NoVector, "landmark")), OT_sumOfSqr, {}, 1e5);
+  //belief dynamics - modulated by gaze at landmark
+  komo.setTask(-1., -1., new TaskMap_BeliefTransition(new TaskMap_Default(gazeAtTMT, K, "endeffEyes", NoVector, "landmark")), OT_sumOfSqr, {}, 1e3);
 //  komo.setTask(-1., -1., new TaskMap_BeliefTransition(), OT_sumOfSqr, {}, 1e5);
 
+  //belief target
   komo.setTask(4., -1., new TaskMap_qUncertainties(), OT_sumOfSqr);
 
   komo.reset();
@@ -45,12 +50,12 @@ void solve(){
   //-----------------------------
   // add collision and belief dynamics
 
-#if 0
+#if 1
   t->prec.clear();
-  komo.setCollisions(true, .05, 1e1);
+  komo.setCollisions(false, .05, 1e2);
   komo.run();
   cout <<komo.getReport(true) <<endl;
-  komo.checkGradients();
+//  komo.checkGradients();
 #endif
 
   komo.plotTrajectory();
