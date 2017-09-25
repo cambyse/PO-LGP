56bb3014af3acb66bceb46368d039942faa29bef
Marc Toussaint <marc.toussaint@uni-stuttgart.de>
2017-09-25 14:13:14 -0400

    CORRECT penetration gradients - in test GJK only. TODO: properly deal with radius; integrate with Proxy

--- share/projects/17-LGP-worlds/main.cpp
+++ share/projects/17-LGP-worlds/main.cpp
@@ -218,13 +218,13 @@ void solve7(){
 int MAIN(int argc,char **argv){
   mlr::initCmdLine(argc, argv);
 
-  illustrate();
+//  illustrate();
 //  solve1();
 //  solve3();
 //  solve4();
 //  solve5();
 //  solve6();
-//  solve7();
+  solve7();
 
   return 0;
 }
