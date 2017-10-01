7ead6e639d00b78868371e0ecdd65a6060f13a8d
Marc Toussaint <marc.toussaint@uni-stuttgart.de>
2017-10-01 18:58:26 -0400

    better characterization of pair contacts

--- share/projects/17-josh/supportAnalysis/main.cpp
+++ share/projects/17-josh/supportAnalysis/main.cpp
@@ -1,48 +1,103 @@
-#include <KOMO/komo.h>
-#include <string>
-#include <map>
+#include <Kin/kin.h>
 #include <Gui/opengl.h>
+#include <Core/graph.h>
+#include <stdio.h>
+
+#include <Kin/frame.h>
 #include <Kin/kin_swift.h>
-#include <Kin/taskMaps.h>
-#include <Kin/kinViewer.h>
-#include <Kin/TM_StaticStability.h>
 
-using namespace std;
+#include <Geo/pairCollide.h>
+#include <Kin/proxy.h>
 
-//===========================================================================
+void illustrate(){
 
-int main(int argc,char** argv){
-  mlr::initCmdLine(argc,argv);
+  OpenGL gl("Red Ball Scenes", 1600, 800);
 
-  mlr::KinematicWorld K("kin.g");
+  uint N=11;
+  mlr::Array<mlr::KinematicWorld> K(N);
+  for(uint i=0;i<N;i++){
+    mlr::String str;
+    str.resize(10, false);
+    snprintf(str.p, 10, "%02i", i+1);
+    K(i).init(STRING("p"<<str<<".g"));
+    K(i).orsDrawMarkers=false;
+    gl.addView(i, glStandardScene, NULL);
+    gl.addSubView(i, K(i));
+    gl.views(i).camera.setDefault();
+    gl.views(i).camera.setPosition(3., 0., 3.);
+    gl.views(i).camera.focus(0., 0., 1.);
+    gl.views(i).camera.upright();
+    gl.views(i).text <<"problem " <<i+1;
+  }
+  gl.setSubViewTiles(4,3);
 
-  KOMO komo;
-  komo.setModel(K, false);
-  komo.world.report();
-  komo.setPathOpt(4., 20, 10.);
+  gl.watch();
+}
 
-  komo.setGrasp(1., "baxterL", "red");
-  komo.setPlace(2., "baxterL", "red", "plate");
+void analyzeSupport(){
+  uint N=11;
+  mlr::Array<mlr::KinematicWorld> K(N);
+  OpenGL gl;
 
-//  komo.setGrasp(1., "baxterR", "green");
-//  komo.setPlace(2., "baxterR", "green", "red");
+  ofstream fil("z.supportAna");
 
-  komo.setGrasp(2.1, "baxterR", "yellow");
-  komo.setPlace(3.1, "baxterR", "yellow", "blue");
+  StringA contactName = {"vertex", "edge", "plane"};
 
-//  komo.setTask(3.5, -1., new TM_StaticStability(komo.world, "blue", .01), OT_ineq);
-//  komo.setTask(3.5, -1., new TM_StaticStability(komo.world, "plate", .01), OT_ineq);
-//  komo.setTask(3.5, -1., new TM_StaticStability(komo.world, "yellow", NULL, .01), OT_ineq);
+  for(uint i=0;i<N;i++){
+    fil <<"\n### PROBLEM " <<i <<endl;
+    mlr::String str;
+    str.resize(10, false);
+    snprintf(str.p, 10, "%02i", i+1);
+    mlr::KinematicWorld K(STRING("p"<<str<<".g"));
 
-  komo.reset();
-  komo.reportProblem();
-  komo.run();
-//  komo.checkGradients();
-  cout <<komo.getReport(true) <<endl;
+    for(mlr::Frame *f:K.frames) if(f->shape){
+      f->shape->cont=true;
+      f->shape->mesh().C.append(.3);
+    }
+    K.swift().setCutoff(.05);
+    K.swift().initActivations(K, 0);
 
-  while(komo.displayTrajectory(.1, true));
-//  renderConfigurations(komo.configurations, "z.vid/z.path.", -2, 600, 600, &komo.displayCamera());
+    K.stepSwift();
+    K.reportProxies();
+
+    for(mlr::Proxy* p:K.proxies){
+      mlr::Frame *a = K.frames(p->a);
+      mlr::Frame *b = K.frames(p->b);
+      if(a->shape->type()!=mlr::ST_ssBox || b->shape->type()!=mlr::ST_ssBox) continue;
+      PairCollision coll(a->shape->sscCore(),
+                         b->shape->sscCore(),
+                         a->X,
+                         b->X);
+
+      coll.marginAnalysis(.001);
+
+      cout <<"PROXY " <<a->name <<"--" <<b->name
+             <<"\n  " <<coll;
+
+      if(coll.distance<a->shape->size(3)+b->shape->size(3)+.001){
+        fil <<"  " <<a->name <<'-' <<b->name <<" : " <<contactName(coll.eig1.d0) <<'-' <<contactName(coll.eig2.d0) <<endl;
+      }
+
+      gl.add(glStandardLight);
+      gl.add(coll);
+      gl.add(K);
+//      gl.watch();
+      gl.update();
+      gl.clear();
+
+    }
+
+//    K.watch(true);
+  }
+}
+
+int main(int argc,char **argv){
+  mlr::initCmdLine(argc, argv);
+
+//  illustrate();
+  analyzeSupport();
 
   return 0;
 }
+
 
