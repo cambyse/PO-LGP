10580da96d9a05804aa4efb749f7be81cd91f4ea
cambyse <camille.phiquepal@gmail.com>
2019-01-21 21:40:48 +0100

    merge skeleton files along with training data

--- share/projects/17-camille-obsTask/apps/lgp-learn-self-driving/merge-run-results.py
+++ share/projects/17-camille-obsTask/apps/lgp-learn-self-driving/merge-run-results.py
@@ -2,8 +2,10 @@
 
 import sys, os
 import fnmatch
+import shutil
 
 RESULT_DATA_FILE = 'result-data.csv'
+SKELETON_FORMATS = ["*.png", "*.gv", "*.po"]
 
 def merge(datasets_dir, output_dir):
   #print ("datasets_dir {}, output_dir {}".format(datasets_dir, output_dir))
@@ -27,6 +29,13 @@ def merge(datasets_dir, output_dir):
       with open(filename) as file:
         for line in file:
           outfile.write(line)
+
+  # move skeletons
+  for root, dirnames, filenames in os.walk(datasets_dir):
+    for ext in SKELETON_FORMATS:
+      for filename in fnmatch.filter(filenames, ext):
+        if not os.path.exists( output_dir + "/" + filename ):
+          shutil.copy(root + "/" + filename, output_dir)
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
