ae09367560966f591bf0da233d344784ee168fb3
Control tree developper <control.tree@outlook.com>
2020-05-07 19:58:56 +0200

    added simple qp solver (tested in 1d)

--- share/projects/17-camille-obsTask/libs/PyKomo/optimization/qp_solver.py
+++ share/projects/17-camille-obsTask/libs/PyKomo/optimization/qp_solver.py
@@ -1,4 +1,7 @@
 import numpy as np
+from augmented_lagrangian_solver import AugmentedLagrangianSolverIneq
+from newton import NewtonFunction
+from optimization_problems import UnconstrainedQP, ConstrainedQP, ConstrainedProblem
 
 class UnconstrainedQPSolver:
     def __init__(self, qp):
@@ -9,5 +12,48 @@ class UnconstrainedQPSolver:
         B = -self.qp.c
 
         x = np.linalg.solve(A, B)
+
+        return x
+
+class ConstrainedQPSolver:
+    def __init__(self, qp):
+        self.qp = qp
+
+    @staticmethod
+    def to_f(qp):
+        class F(NewtonFunction):
+            def value(self, x):
+                return 0.5 * np.dot(x.T, np.dot(qp.Q, x)) + np.dot(qp.c.T, x)
+
+            def gradient(self, x):
+                return np.dot(qp.Q, x) + qp.c
+
+            def hessian(self, x):
+                return qp.Q
+
+        return F()
+
+    @staticmethod
+    def to_g(qp):
+        class G(NewtonFunction):
+            def value(self, x):
+                return np.dot(qp.A, x) - qp.u
+
+            def gradient(self, x):
+                return qp.A
+
+            def hessian(self, x):
+                return np.zeros(qp.Q.shape)
+
+        return G()
+
+    def run(self, x):
+        f = self.to_f(self.qp)
+        g = self.to_g(self.qp)
+
+        pb = ConstrainedProblem(f=f, g=g)
+
+        al = AugmentedLagrangianSolverIneq(pb)
+        x =  al.run(x)
 
         return x
--- share/projects/17-camille-obsTask/libs/PyKomo/optimization/test/test_qp_solver.py
+++ share/projects/17-camille-obsTask/libs/PyKomo/optimization/test/test_qp_solver.py
@@ -4,15 +4,26 @@ import numpy.testing as npt
 import numpy as np
 from pathlib import Path
 sys.path.append(str(Path('.').absolute().parent))
-from optimization_problems import UnconstrainedQP
-from qp_solver import UnconstrainedQPSolver
+from optimization_problems import UnconstrainedQP, ConstrainedQP, ConstrainedProblem
+from qp_solver import UnconstrainedQPSolver, ConstrainedQPSolver
 
-def test_dummy_qp():
-    qp = UnconstrainedQP(Q=np.array([[2.0]]), c=np.array([[1.0]]))
+def test_unconstrained_qp():
+    # min x^2 - x
+    qp = UnconstrainedQP(Q=np.array([[2.0]]), c=np.array([-1.0]))
     solver = UnconstrainedQPSolver(qp)
     x = solver.run()
 
-    nt.assert_almost_equals(x, -0.5, delta=0.001)
+    nt.assert_almost_equals(x, 0.5, delta=0.001)
+
+def test_constrained_qp():
+    # min x^2 - x
+    # s.t. x <= 0
+    qp = ConstrainedQP(Q=np.array([[2.0]]), c=np.array([-1.0]), A=np.array([[1.0]]), u=np.array([0.2]))
+    solver = ConstrainedQPSolver(qp)
+    x = solver.run(np.array([-1.0]))
+
+    nt.assert_almost_equals(x, 0.2, delta=0.001)
+
 
 if __name__ == "__main__":
-     test_dummy_qp()
+     test_unconstrained_qp()
