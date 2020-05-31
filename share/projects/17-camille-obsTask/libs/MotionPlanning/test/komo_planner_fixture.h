a093382120463567f50a55116fd5d72f72a409e8
Camille Phiquepal <camille.phiquepal@gmail.com>
2020-05-31 15:37:42 +0200

    refactored groundings

--- share/projects/17-camille-obsTask/libs/MotionPlanning/test/komo_planner_fixture.h
+++ share/projects/17-camille-obsTask/libs/MotionPlanning/test/komo_planner_fixture.h
@@ -3,8 +3,17 @@
 #include "komo_planner.h"
 #include <komo_wrapper.h>
 
+#include <Kin/switch.h>
+#include <Kin/TM_angVel.h>
+#include <Kin/TM_default.h>
+#include <Kin/TM_gravity.h>
+#include <Kin/TM_InsideBox.h>
+#include <Kin/TM_AboveBox.h>
+#include <Kin/TM_qLimits.h>
+
 #include <gtest/gtest.h>
 #include <functional>
+
 
 using namespace std;
 using namespace mp;
@@ -15,7 +24,30 @@ using namespace mp;
  *
  */
 
-using W = mp::KomoWrapper;
+struct CarKOMOWrapper : public mp::KomoWrapper
+{
+  CarKOMOWrapper(KOMO_ext * komo)
+    : mp::KomoWrapper(komo)
+  {
+
+  }
+
+  void setVelocity(const Interval& it, const TreeBuilder& tree, const char* shape, ObjectiveType type, const arr& target_velocity, double scale)
+  {
+    addObjective( it, tree, new TM_Default(TMT_pos, komo_->world, shape), type, target_velocity, scale, 1);
+  }
+
+  //  void addObjective(const Interval& it, const TreeBuilder& tb, Feature* map, ObjectiveType type=OT_sos, const arr& target=NoArr, double scale=-1., int order=-1, int deltaFromStep=0, int deltaToStep=0);
+
+  void setPosition(const Interval& it, const TreeBuilder& tree, const char* shape, const char* shapeRel, ObjectiveType type, const arr& target_position, double scale)
+  {
+    addObjective( it, tree, new TM_Default(TMT_pos, komo_->world, shape, NoVector, shapeRel, NoVector), type, target_position, scale, 0);
+  }
+
+  // komo->setPosition( t_start+1, -1, "car_ego", car_next.c_str(), OT_sos, {-0.7, 0, 0} );
+};
+
+using W = CarKOMOWrapper;
 
 /////////////////Tasks////////////////////////
 struct AxisBound:Feature{
@@ -87,25 +119,28 @@ private:
 class InitialGrounder
 {
 public:
-  void init( KOMO_ext* komo, int verbose )
+  void init( const TreeBuilder& tree, KOMO_ext* komo, int verbose )
   {
     mp::Interval always{{0, -1}, {0, 1}};
 
     // road bounds
-    komo->addObjective( 0.0, -1, new AxisBound( "car_ego", -0.15, AxisBound::Y, AxisBound::MIN ), OT_ineq );
-    komo->addObjective( 0.0, -1, new AxisBound( "car_ego",  0.15, AxisBound::Y, AxisBound::MAX ), OT_ineq );
+    W(komo).addObjective( always, tree, new AxisBound( "car_ego", -0.15, AxisBound::Y, AxisBound::MIN ), OT_ineq, NoArr, 1e2, 0 );
+    W(komo).addObjective( always, tree, new AxisBound( "car_ego",  0.15, AxisBound::Y, AxisBound::MAX ), OT_ineq, NoArr, 1e2, 0 );
 
     // min speed
-    komo->addObjective( 0.0, -1, new AxisBound( "car_ego",  0.00, AxisBound::X, AxisBound::MIN ), OT_ineq, - arr{ 0.03 }, 1e2, 1 );
+    W(komo).addObjective( always, tree, new AxisBound( "car_ego",  0.00, AxisBound::X, AxisBound::MIN ), OT_ineq, - arr{ 0.03 }, 1e2, 1 );
 
     // truck speed
     arr truck_speed{ 0.03, 0, 0 };
     truck_speed( 0 ) = 0.03;
-    komo->setVelocity( 0.0, -1, "truck", NULL, OT_eq, truck_speed );
+    //komo->setVelocity( 0.0, -1, "truck", NULL, OT_eq, truck_speed );
+    //W(komo).addObjective( always, tree, new TM_Default(TMT_pos, komo->world, "truck"), OT_eq, truck_speed, 1e2, 1);
+    W(komo).setVelocity(always, tree, "truck", OT_eq, truck_speed, 1e2);
 
+    // preference ego lane?
+    //W(komo).addObjective( always, tree, new AxisBound( "car_ego", -0.1, AxisBound::Y, AxisBound::MAX ), OT_sos, NoArr, 1e0, 0 );
     // min speed
-    komo->addObjective( 0.0, 1.0, new AxisBound( "car_ego", -0.1, AxisBound::Y, AxisBound::MAX ), OT_sos );
-    komo->addObjective( 0.0, -1, new AxisBound( "car_ego",  0.00, AxisBound::X, AxisBound::MIN ), OT_ineq, - arr{ 0.03 }, 1e2, 1 );
+    W(komo).addObjective( always, tree, new AxisBound( "car_ego",  0.00, AxisBound::X, AxisBound::MIN ), OT_ineq, - arr{ 0.03 }, 1e2, 1 );
 
     // collision
     komo->activateCollisions( "car_ego", "truck" );
@@ -115,19 +150,27 @@ public:
 
   virtual void groundInitSingleAgent( const TreeBuilder& tree, KOMO_ext* komo, int verbose )
   {
-    init( komo, verbose );
+    mp::Interval always{{0, -1}, {0, 1}};
+
+    init( tree, komo, verbose );
 
     // opposite car speed
     arr op_speed{ -0.03, 0, 0 };
-    komo->setVelocity( 0.0, -1, "car_op", NULL, OT_eq, op_speed );
+    //komo->setVelocity( 0.0, -1, "car_op", NULL, OT_eq, op_speed );
+    //W(komo).addObjective( always, tree, new TM_Default(TMT_pos, komo->world, "car_op"), OT_eq, op_speed, 1e2, 1);
+    W(komo).setVelocity( always, tree, "car_op", OT_eq, op_speed, 1e2);
   }
 
   virtual void groundInitDoubleAgent( const TreeBuilder& tree, KOMO_ext* komo, int verbose )
   {
-    init( komo, verbose );
+    mp::Interval always{{0, -1}, {0, 1}};
+
+    init( tree, komo, verbose );
 
     arr op_speed{ -0.04, 0, 0 };
-    komo->setVelocity( 0, 1.5, "car_op", NULL, OT_eq, op_speed );
+    //komo->setVelocity( 0, 1.5, "car_op", NULL, OT_eq, op_speed );
+    //W(komo).addObjective( always, tree, new TM_Default(TMT_pos, komo->world, "car_op"), OT_eq, op_speed, 1e2, 1);
+    W(komo).setVelocity( always, tree, "car_op", OT_eq, op_speed, 1e2);
   }
 };
 
@@ -154,100 +197,80 @@ public:
 
 void groundLook( const mp::Interval& phase, const mp::TreeBuilder& tree, const std::vector< std::string > & args, KOMO_ext* komo, int verbose )
 {
-  //
-  const double t_start = phase.time.from;
-  const double t_end =   phase.time.to;
-  //
-
-  // look
-  komo->addObjective( t_start + 0.9, t_end, new AxisBound( "car_ego", 0.0, AxisBound::Y, AxisBound::MIN ), OT_sos );
+  //komo->addObjective( t_start + 0.9, t_end, new AxisBound( "car_ego", 0.0, AxisBound::Y, AxisBound::MIN ), OT_sos );
+  mp::Interval it{{phase.time.from + 0.9, phase.time.to}, phase.edge};
+  W(komo).addObjective(it, tree, new AxisBound( "car_ego", 0.0, AxisBound::Y, AxisBound::MIN ), OT_sos, NoArr, 1e2, 0);
 
   if( verbose > 0 )
   {
-    std::cout << t_start << "->" << t_end << ": " << " look " << " at " << args[0] << std::endl;
+    std::cout << phase.time.from << "->" << phase.time.from << ": " << " look " << " at " << args[0] << std::endl;
   }
 }
 
 void groundOvertake( const mp::Interval& phase, const mp::TreeBuilder& tree, const std::vector< std::string > & args, KOMO_ext* komo, int verbose )
 {
-  //
-  const double t_start = phase.time.from;
-  const double t_end =   phase.time.to;
-  //
-
   // overtake
-  //komo->addObjective( t_start -0.5, t_start + 0.5, new AxisBound( "car_ego", 0.05, AxisBound::Y, AxisBound::MIN ), OT_sos );
-  komo->setPosition( t_end, -1, "car_ego", args[0].c_str(), OT_sos, { 0.45, 0, 0 } );
+  //komo->setPosition( t_end, -1, "car_ego", args[0].c_str(), OT_sos, { 0.45, 0, 0 } );
+  mp::Interval future{{phase.time.to, -1}, phase.edge};
+  W(komo).setPosition(future, tree, "car_ego", args[0].c_str(), OT_sos, { 0.45, 0, 0 }, 1e2);
 
   if( verbose > 0 )
   {
-    std::cout << t_start << "->" << t_end << ": " << " overtake " << args[0] << std::endl;
+    std::cout << phase.time.from << "->" << phase.time.from << ": " << " overtake " << args[0] << std::endl;
   }
 }
 
 void groundFollow( const mp::Interval& phase, const mp::TreeBuilder& tree, const std::vector< std::string > & args, KOMO_ext* komo, int verbose )
 {
-  //
-  const double t_start = phase.time.from;
-  const double t_end =   phase.time.to;
-  //
-
   // overtake
-  komo->setPosition( t_end, -1, "car_ego", "truck", OT_sos, { -0.7, 0, 0 } ); // -0.55
+  //komo->setPosition( t_end, -1, "car_ego", "truck", OT_sos, { -0.7, 0, 0 } ); // -0.55
+  mp::Interval future{{phase.time.to, -1}, phase.edge};
+  W(komo).setPosition(future, tree, "car_ego", args[0].c_str(), OT_sos, { -0.7, 0, 0 }, 1e2);
 
   if( verbose > 0 )
   {
-    std::cout << t_start << "->" << t_end << ": " << " follow " << args[0] << std::endl;
+    std::cout << phase.time.from << "->" << phase.time.from << ": " << " follow " << args[0] << std::endl;
   }
 }
 
 void groundAccelerate( const mp::Interval& phase, const mp::TreeBuilder& tree, const std::vector< std::string > & args, KOMO_ext* komo, int verbose )
 {
-  //
-  const double t_start = phase.time.from;
-  const double t_end =   phase.time.to;
-  //
-
   // opposite car speed
   arr op_speed{ -0.07, 0, 0 };
-  komo->setVelocity( t_start, -1, "car_op", NULL, OT_eq, op_speed );
+  //komo->setVelocity( t_start, -1, "car_op", NULL, OT_eq, op_speed );
+  mp::Interval always{{phase.time.from, -1}, phase.edge};
+  W(komo).setVelocity( always, tree, "car_op", OT_eq, op_speed, 1e2);
 }
 
 void groundContinue( const mp::Interval& phase, const mp::TreeBuilder& tree, const std::vector< std::string > & args, KOMO_ext* komo, int verbose )
 {
-  //
-  const double t_start = phase.time.from;
-  const double t_end =   phase.time.to;
-  //
-
   // opposite car speed
   arr op_speed{ -0.04, 0, 0 };
-  komo->setVelocity( t_start, -1, "car_op", NULL, OT_eq, op_speed );
+  //komo->setVelocity( t_start, -1, "car_op", NULL, OT_eq, op_speed );
+  mp::Interval always{{phase.time.from, -1}, phase.edge};
+  W(komo).setVelocity( always, tree, "car_op", OT_eq, op_speed, 1e2);
 }
 
 void groundSlowDown( const mp::Interval& phase, const mp::TreeBuilder& tree, const std::vector< std::string > & args, KOMO_ext* komo, int verbose )
 {
-  //
-  const double t_start = phase.time.from;
-  const double t_end =   phase.time.to;
-  //
-
   // opposite car speed
   arr op_speed{ -0.015, 0, 0 };
-  komo->setVelocity( t_start, -1, "car_op", NULL, OT_eq, op_speed );
+  //komo->setVelocity( t_start, -1, "car_op", NULL, OT_eq, op_speed );
+  mp::Interval always{{phase.time.from, -1}, phase.edge};
+  W(komo).setVelocity( always, tree, "car_op", OT_eq, op_speed, 1e2);
 }
 
 void groundMergeBetween( const mp::Interval& phase, const mp::TreeBuilder& tree, const std::vector< std::string >& facts, KOMO_ext* komo, int verbose )
 {
-  //
-  const double t_start = phase.time.from;
-  const double t_end =   phase.time.to;
-  //
   auto car_before = facts[0];
   auto car_next = facts[1];
 
-  komo->setPosition( t_start+1, -1, "car_ego", car_next.c_str(), OT_sos, {-0.7, 0, 0} );
-  komo->setPosition( t_start+1, -1, car_before.c_str(), "car_ego", OT_sos, {-0.7, 0, 0} );
+//  komo->setPosition( t_start+1, -1, "car_ego", car_next.c_str(), OT_sos, {-0.7, 0, 0} );
+//  komo->setPosition( t_start+1, -1, car_before.c_str(), "car_ego", OT_sos, {-0.7, 0, 0} );
+
+  mp::Interval future{{phase.time.to, -1}, phase.edge};
+  W(komo).setPosition(future, tree, "car_ego", car_next.c_str(), OT_sos, { -0.7, 0, 0 }, 1e2);
+  W(komo).setPosition(future, tree, car_before.c_str(), "car_ego", OT_sos, { -0.7, 0, 0 }, 1e2);
 
   //setKeepDistanceTask( phase+1, -1, komo, car_successors );
   //std::cout << "merge between " << car_before << " and " << car_next << std::endl;
